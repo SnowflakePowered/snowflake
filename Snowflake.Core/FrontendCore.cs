24e9915c62047348bd7e596aaff68e0f3381190e
Ronny Chan <ronny6993@gmail.com>
2014-07-09 21:30:36 -0400

    Refactor plugin loading to PluginManager

--- Snowflake.Core/FrontendCore.cs
+++ Snowflake.Core/FrontendCore.cs
@@ -18,42 +18,23 @@ namespace Snowflake.Core
     {
         public Dictionary<string, Platform> LoadedPlatforms { get; private set; }
         public string AppDataDirectory { get; private set; }
-
-        [ImportMany(typeof(IIdentifier))]
-        IEnumerable<Lazy<IIdentifier>> identifiers;
-        [ImportMany(typeof(IEmulator))]
-        IEnumerable<Lazy<IEmulator>> emulators;
-        [ImportMany(typeof(IScraper))]
-        IEnumerable<Lazy<IScraper>> scrapers;
-        [ImportMany(typeof(IGenericPlugin))]
-        IEnumerable<Lazy<IGenericPlugin>> plugins;
-
-
-        public IDictionary<string, IIdentifier> LoadedIdentifiers { get; private set; }
-        public IDictionary<string, IEmulator> LoadedEmulators { get; private set; }
-        public IDictionary<string, IScraper> LoadedScrapers { get; private set; }
-        public IDictionary<string, IGenericPlugin> LoadedPlugins { get; private set; }
+        public PluginManager PluginManager { get; private set; }
         public GameDatabase GameDatabase { get; private set; }
      
-        [Import(typeof(IIdentifier))]
-        public IIdentifier datIdentifier;
-
         public FrontendCore() : this(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Snowflake")) { }
         public FrontendCore(string appDataDirectory)
         {
             this.AppDataDirectory = appDataDirectory;
             this.LoadedPlatforms = this.LoadPlatforms(Path.Combine(this.AppDataDirectory, "platforms"));
-            this.ComposeImports();
-            this.LoadedIdentifiers = this.LoadPlugin<IIdentifier>(this.identifiers);
-            this.LoadedEmulators = this.LoadPlugin<IEmulator>(this.emulators);
-            this.LoadedScrapers = this.LoadPlugin<IScraper>(this.scrapers);
-            this.LoadedPlugins = this.LoadPlugin<IGenericPlugin>(this.plugins);
+         
             this.GameDatabase = new GameDatabase(Path.Combine(this.AppDataDirectory, "games.db"));
-            
+            this.PluginManager = new PluginManager(this.AppDataDirectory);
+            this.PluginManager.LoadAllPlugins();
+
             ThemeServer server = new ThemeServer(Path.Combine(this.AppDataDirectory, "theme"));
             server.StartServer();
 
-            Console.WriteLine(this.LoadedIdentifiers["Snowflake-IdentifierDat"].PluginName);
+            Console.WriteLine(this.PluginManager.LoadedIdentifiers["Snowflake-IdentifierDat"].PluginName);
 
         }
 
@@ -82,25 +63,6 @@ namespace Snowflake.Core
 
         }
 
-        private void ComposeImports()
-        {
-            DirectoryCatalog catalog = new DirectoryCatalog(Path.Combine(this.AppDataDirectory, "plugins"));
-            CompositionContainer container = new CompositionContainer(catalog);
-            container.SatisfyImportsOnce(this);
-        }
 
-        private Dictionary<string, T> LoadPlugin<T>(IEnumerable<Lazy<T>> plugins)
-        {
-            if(!(typeof(IPlugin).IsAssignableFrom(typeof(T)))){
-                throw new ArgumentException("Attemped to load plugin that is not inherited from IPlugin");
-            }
-            var loadedPlugins = new Dictionary<string, T>();
-            foreach (var plugin in plugins)
-            {
-                var instance = (IPlugin) plugin.Value;
-                loadedPlugins.Add(instance.PluginName, plugin.Value);
-            }
-            return loadedPlugins;
-        }
     }
 }
---
+++ Snowflake.Core/PluginManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using Snowflake.API.Interface;
+using System.ComponentModel.Composition.Hosting;
+using System.IO;
+
+namespace Snowflake.Core
+{
+    public class PluginManager
+    {
+        private string AppDataDirectory { get; private set; }
+
+        [ImportMany(typeof(IIdentifier))]
+        IEnumerable<Lazy<IIdentifier>> identifiers;
+        [ImportMany(typeof(IEmulator))]
+        IEnumerable<Lazy<IEmulator>> emulators;
+        [ImportMany(typeof(IScraper))]
+        IEnumerable<Lazy<IScraper>> scrapers;
+        [ImportMany(typeof(IGenericPlugin))]
+        IEnumerable<Lazy<IGenericPlugin>> plugins;
+
+
+        public IDictionary<string, IIdentifier> LoadedIdentifiers { get; private set; }
+        public IDictionary<string, IEmulator> LoadedEmulators { get; private set; }
+        public IDictionary<string, IScraper> LoadedScrapers { get; private set; }
+        public IDictionary<string, IGenericPlugin> LoadedPlugins { get; private set; }
+        public PluginManager(string appDataDirectory)
+        {
+            this.AppDataDirectory = appDataDirectory;
+            this.ComposeImports();
+        }
+
+        public void LoadAllPlugins()
+        {
+            this.LoadedIdentifiers = this.LoadPlugin<IIdentifier>(this.identifiers);
+            this.LoadedEmulators = this.LoadPlugin<IEmulator>(this.emulators);
+            this.LoadedScrapers = this.LoadPlugin<IScraper>(this.scrapers);
+            this.LoadedPlugins = this.LoadPlugin<IGenericPlugin>(this.plugins);
+        }
+        private void ComposeImports()
+        {
+            DirectoryCatalog catalog = new DirectoryCatalog(Path.Combine(this.AppDataDirectory, "plugins"));
+            CompositionContainer container = new CompositionContainer(catalog);
+            container.SatisfyImportsOnce(this);
+        }
+
+        private Dictionary<string, T> LoadPlugin<T>(IEnumerable<Lazy<T>> plugins)
+        {
+            if (!(typeof(IPlugin).IsAssignableFrom(typeof(T))))
+            {
+                throw new ArgumentException("Attemped to load plugin that is not inherited from IPlugin");
+            }
+            var loadedPlugins = new Dictionary<string, T>();
+            foreach (var plugin in plugins)
+            {
+                var instance = (IPlugin)plugin.Value;
+                loadedPlugins.Add(instance.PluginName, plugin.Value);
+            }
+            return loadedPlugins;
+        }
+    }
+}
