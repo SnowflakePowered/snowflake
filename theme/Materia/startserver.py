c133cda161b6dd6f66624cafcae2602dacd41093
Ronny Chan <ronny6993@gmail.com>
2014-07-17 19:24:40 -0400

    Add selector to mainmenu

--- theme/Materia/startserver.py
+++ theme/Materia/startserver.py
@@ -2,8 +2,8 @@ import http.server;
 
 def start_server(port=8000, bind="", cgi=False):
     if cgi==True:
-        http.server.test(HandlerClass=http.server.CGIHTTPRequestHandler, port=port, bind=bind);
+        http.server.test(HandlerClass=http.server.CGIHTTPRequestHandler, port=port);
     else:
-        http.server.test(HandlerClass=http.server.SimpleHTTPRequestHandler,port=port,bind=bind);
+        http.server.test(HandlerClass=http.server.SimpleHTTPRequestHandler,port=port);
 
 start_server(); #If you want cgi, set cgi to True; e.g. start_server(cgi=True);
