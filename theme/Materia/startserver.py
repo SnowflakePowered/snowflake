c05e52c8120a63c35e92d96c9d6ae9f3b3f64851
Ronny Chan <ronny6993@gmail.com>
2014-07-21 20:12:01 -0400

    Update batch file for python 3

--- theme/Materia/startserver.py
+++
@@ -1,9 +0,0 @@
-import http.server;
-
-def start_server(port=8000, bind="", cgi=False):
-    if cgi==True:
-        http.server.test(HandlerClass=http.server.CGIHTTPRequestHandler, port=port);
-    else:
-        http.server.test(HandlerClass=http.server.SimpleHTTPRequestHandler,port=port);
-
-start_server(); #If you want cgi, set cgi to True; e.g. start_server(cgi=True);
