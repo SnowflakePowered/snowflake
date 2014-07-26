91e8887a1cc8abb70036a1f06eec00c6d29bfd8b
Ronny Chan <ronny6993@gmail.com>
2014-07-26 18:03:57 -0400

    Make NodeWebkit wrapper a WinForms app

---
+++ Snowflake.Presentation.NodeWebkit/EntryCore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Snowflake.Core;
+
+
+namespace Snowflake.Presentation.NodeWebkit
+{
+    public class EntryCore
+    {
+        public void StartSnowflake()
+        {
+            FrontendCore.InitCore();
+        }
+    }
+}
