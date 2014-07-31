e19e4557973b25f0e0e214d4498e2e022d0459c9
Ronny Chan <ronny6993@gmail.com>
2014-07-31 19:16:05 -0400

    Create abstract base class for a HttpServer

---
+++ Snowflake.API/Core/Server/BaseHttpServer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Threading;
+namespace Snowflake.Core.Server
+{
+    public abstract class BaseHttpServer
+    {
+        HttpListener serverListener;
+        Thread serverThread;
+        bool cancel;
+
+        public BaseHttpServer(int port)
+        {
+            serverListener = new HttpListener();
+            serverListener.Prefixes.Add("http://localhost:" + port.ToString() + "/");
+            
+        }
+        public void StartServer()
+        {
+            this.serverThread = new Thread(
+                () =>
+                {
+                    serverListener.Start();
+                    while (!this.cancel)
+                    {
+                        HttpListenerContext context = serverListener.GetContext();
+                        Task.Run(() => this.Process(context));
+                    }
+                }
+            );
+            this.serverThread.IsBackground = true; 
+            this.serverThread.Start();          
+        }
+
+        public void StopServer()
+        {
+            this.cancel = true;
+            this.serverThread.Join();
+            this.serverListener.Stop();
+        }
+
+        public abstract void Process(HttpListenerContext context);
+    }
+}
