32b419e5b748bab7f090aa52992418c088e3a8d3
Ronny Chan <ronny6993@gmail.com>
2014-08-16 17:16:03 -0400

    Revert TypeScript project in favour of pure Javascript

--- Snowflake.Node/app.js
+++
@@ -1,1 +0,0 @@
-﻿//# sourceMappingURL=app.js.map
---
+++ Snowflake.Presentation/nodewebkit/snowflake.js
@@ -0,0 +1,15 @@
+﻿var Snowflake = function Snowflake(snowflakeUrl) {
+    
+    this.snowflakeUrl = snowflakeUrl;
+    this.platforms = {};
+    this.games = {};
+
+    this.loadGames = function () {
+       
+    };
+    this.loadPlatforms = function () {
+    };
+
+}
+
+exports.Snowflake = Snowflake;
