fa73baa27dfcd9fab94a7a69c43dd18b440abe2d
Ronny Chan <ronny6993@gmail.com>
2014-08-17 00:45:33 -0400

    Begin work on ajax

--- Snowflake.Node/snowflake.js
+++ Snowflake.Node/snowflake.js
@@ -1,14 +1,22 @@
-﻿var Snowflake = function Snowflake(snowflakeUrl) {
-    
+﻿var najax = require("najax");
+var Promise = require('promise');
+var Snowflake = function Snowflake(snowflakeUrl) {
+    var instance = this;
+
     this.snowflakeUrl = snowflakeUrl;
     this.platforms = {};
     this.games = {};
-
     this.loadGames = function () {
-       
+        return new Promise(function (resolve, reject) {
+            this.ajax('', '', function (response) { resolve(response); });
+        });
     };
     this.loadPlatforms = function () {
     };
+
+    this.ajax = function (namespace, methodname, callback) {
+        return najax('http://www.xgoogle.com', callback);
+    }
 
 }
 
