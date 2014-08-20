372980d98a25c055293669ab63b2f4a26c900ab3
Ronny Chan <ronny6993@gmail.com>
2014-08-20 01:06:12 -0400

    Init snowflake object to global scope

---
+++ Snowflake.Node/init.js
@@ -0,0 +1,11 @@
+(function(){
+	var snowflakeModule = require("./snowflake.js");
+	var Snowflake = snowflakeModule.Snowflake;
+	if(global.snowflake === undefined){
+			global.snowflake = {};
+			global.snowflake.snowflakeApi = new Snowflake("http://localhost:30001");
+	}
+})();
+exports._sflk = function(){
+	window.snowflake = global.snowflake;
+}
