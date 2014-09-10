6ae177887635409680d7c59e6d3146f058962b95
Ronny Chan <ronny6993@gmail.com>
2014-09-09 22:11:26 -0400

    Begin work on structured MediaStore

---
+++ Snowflake.API/Information/MediaStore/MediaStore.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Snowflake.Information.MediaStore
+{
+    public class MediaStore
+    {
+        private Dictionary<string, MediaStoreSection> sections;
+        public MediaStoreSection Images
+        {
+            get { return this.sections["Images"]; }
+        }
+        public MediaStoreSection Audio
+        {
+            get { return this.sections["Audio"]; }
+        }
+        public MediaStoreSection Video
+        {
+            get { return this.sections["Video"]; }
+        }
+        public MediaStoreSection Resources
+        {
+            get { return this.sections["Resources"]; }
+        }
+        public string MediaStoreKey { get; set; }
+
+        public MediaStore(string mediastoreKey, string mediastoreRoot)
+        {
+           if (!Directory.Exists(mediastoreRoot)) Directory.CreateDirectory(mediastoreRoot);
+           if (!Directory.Exists(Path.Combine(mediastoreRoot, mediastoreKey))) Directory.CreateDirectory(Path.Combine(mediastoreRoot, mediastoreKey));
+           
+        }
+        public MediaStore(string mediastoreKey) : this(mediastoreKey, Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Snowflake", "mediastores")) { }
+    }
+}
---
+++ Snowflake.API/Information/MediaStore/MediaStoreSection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Snowflake.Information.MediaStore
+{
+    public class MediaStoreSection
+    {
+        public string SectionName { get; set; }
+        public Dictionary<string, string> MediaStoreItems;
+        private string mediaStoreRoot;
+        public MediaStoreSection(string sectionName, MediaStore mediaStore)
+        {
+            this.mediaStoreRoot = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Snowflake", "mediastores", mediaStore.MediaStoreKey, sectionName);
+            this.SectionName = sectionName;
+        }
+
+        private void LoadMediaStore()
+        {
+            try
+            {
+                JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(Path.Combine(mediaStoreRoot, ".mediastore")));
+            }
+            catch
+            {
+                return;
+            }
+        }
+        
+    }
+}
