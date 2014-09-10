d06e7e88e668995e5cb64fa05fa92e712154b9fe
Ronny Chan <ronny6993@gmail.com>
2014-09-10 10:15:09 -0400

    Add methods to manipulate MediaStoreItems

--- Snowflake.API/Information/MediaStore/MediaStoreSection.cs
+++ Snowflake.API/Information/MediaStore/MediaStoreSection.cs
@@ -17,19 +17,33 @@ namespace Snowflake.Information.MediaStore
         {
             this.mediaStoreRoot = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Snowflake", "mediastores", mediaStore.MediaStoreKey, sectionName);
             this.SectionName = sectionName;
+            this.MediaStoreItems = this.LoadMediaStore();
         }
 
-        private void LoadMediaStore()
+        private Dictionary<string, string> LoadMediaStore()
         {
             try
             {
-                JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(Path.Combine(mediaStoreRoot, ".mediastore")));
+                return JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(Path.Combine(mediaStoreRoot, ".mediastore")));
             }
             catch
             {
-                return;
+                return new Dictionary<string, string>();
             }
         }
-        
+        public void Add(string key, string fileName){
+            File.Copy(fileName, Path.Combine(this.mediaStoreRoot, Path.GetFileName(fileName));
+            this.MediaStoreItems.Add(key, Path.GetFileName(fileName));
+            this.UpdateMediaStoreRecord();
+        }
+        public void Remove(string key){
+            File.Delete(Path.Combine(this.mediaStoreRoot, Path.GetFileName(fileName));
+            this.MediaStoreItems.Remove(key);
+            this.UpdateMediaStoreRecord();
+        }
+        private void UpdateMediaStoreRecord(){
+            string record = JsonConvert.SerializeObject(this.MediaStoreItems);
+            File.WriteAllText(Path.Combine(mediaStoreRoot, ".mediastore"));
+        }
     }
 }
