067ee3022c27b7ddcea298d3224c516721cdedd5
Ronny Chan <ronny6993@gmail.com>
2014-09-11 16:53:36 -0400

    Implement IMediaStore in ScrapeEngine

--- Snowflake.API/Core/ScrapeEngine.cs
+++ Snowflake.API/Core/ScrapeEngine.cs
@@ -9,37 +9,62 @@ using Snowflake.Plugin;
 using Snowflake.Constants;
 using DuoVia.FuzzyStrings;
 using Snowflake.Scraper;
+using System.IO;
 
 namespace Snowflake.Core
 {
     public class ScrapeEngine
     {
-        private Platform ScrapePlatform { get; set; }
+        private PlatformInfo ScrapePlatform { get; set; }
         private IScraper ScraperPlugin { get; set; }
         private IIdentifier IdentifierPlugin { get; set; }
-        public ScrapeEngine(Platform scrapePlatform)
+        public ScrapeEngine(PlatformInfo scrapePlatform)
         {
             this.ScrapePlatform = scrapePlatform;
             this.ScraperPlugin = FrontendCore.LoadedCore.PluginManager.LoadedScrapers[ScrapePlatform.Defaults.Scraper];
             this.IdentifierPlugin = FrontendCore.LoadedCore.PluginManager.LoadedIdentifiers[ScrapePlatform.Defaults.Identifier];
         }
 
-        public Game GetGameInfo(string fileName)
+        public GameInfo GetGameInfo(string fileName)
         {
 
             string gameName = this.IdentifierPlugin.IdentifyGame(fileName, this.ScrapePlatform.PlatformId);
             var results = this.ScraperPlugin.GetSearchResults(gameName, this.ScrapePlatform.PlatformId).OrderBy(result => result.GameTitle.LevenshteinDistance(gameName)).ToList();
             var resultdetails = this.ScraperPlugin.GetGameDetails(results[0].ID);
             var gameinfo = resultdetails.Item1;
-            return new Game(
+            var gameUuid = ShortGuid.NewShortGuid();
+            return new GameInfo(
                 this.ScrapePlatform.PlatformId,
                 gameinfo[GameInfoFields.game_title],
-                resultdetails.Item2,
+                resultdetails.Item2.ToMediaStore("game."+ScrapeEngine.ValidateFilename(gameinfo[GameInfoFields.game_title]).Replace(' ','_')+gameUuid),
                 gameinfo,
-                ShortGuid.NewShortGuid(),
+                gameUuid,
                 fileName,
                 new Dictionary<string, dynamic>()
             );
+        }
+        private static string ValidateFilename(string text, char? replacement = '_')
+        {
+            //from http://stackoverflow.com/a/25223884/1822679
+            StringBuilder sb = new StringBuilder(text.Length);
+            var invalids = Path.GetInvalidFileNameChars();
+            bool changed = false;
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                if (invalids.Contains(c))
+                {
+                    changed = true;
+                    var repl = replacement ?? '\0';
+                    if (repl != '\0')
+                        sb.Append(repl);
+                }
+                else
+                    sb.Append(c);
+            }
+            if (sb.Length == 0)
+                return "_";
+            return changed ? sb.ToString() : text;
         }
     }
 }
