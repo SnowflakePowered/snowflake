2516ae56c477d81bdf8996868d0c0048027dcecf
Ronny Chan <ronny6993@gmail.com>
2014-09-15 00:13:16 -0400

    snowflake.snowflakeApi -> snowflake.api

--- Snowflake.Node/init.js
+++ Snowflake.Node/init.js
@@ -1,10 +1,12 @@
 (function(){
-	var snowflakeModule = require("./snowflake.js");
-	
+	var snowflakeModule = require("./snowflake.api.js");
+	var snowflakeConstants = require("./snowflake.constants.js");
+
 	var Snowflake = snowflakeModule.Snowflake;
 	if(global.snowflake === undefined){
 			global.snowflake = {};
-			global.snowflake.snowflakeApi = new Snowflake("http://localhost:30001");
+			global.snowflake.api = new Snowflake("http://localhost:30001");
+			global.snowflake.constants = snowflakeConstants;
 	}
 })();
 exports._sflk = function(){
