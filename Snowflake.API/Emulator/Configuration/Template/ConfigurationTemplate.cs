b65121583c7ba9ea3853af3621485dcad76248e5
Ronny Chan <ronny6993@gmail.com>
2014-11-24 10:45:00 -0500

    Update ConfigurationTemplate.cs

--- Snowflake.API/Emulator/Configuration/Template/ConfigurationTemplate.cs
+++ Snowflake.API/Emulator/Configuration/Template/ConfigurationTemplate.cs
@@ -36,11 +36,12 @@ namespace Snowflake.Emulator.Configuration.Template
         {
             string stringTemplate = protoTemplate["template"];
             var booleanMapping = new BooleanMapping(protoTemplate["boolean"][true], protoTemplate["boolean"][false]);
-           // var types = new 
+            var types = new Dictionary<string, CustomType>();
+            var entries = new Dictionary<string, ConfigurationEntry>();
             var fileName = protoTemplate["filename"];
             var configName = protoTemplate["configuration_name"];
             var defaults = new Dictionary<string, dynamic>();
-          /*  foreach (var value in protoTemplate["types"])
+            foreach (var value in protoTemplate["types"])
             {
                 var typeValues = new List<CustomTypeValue>();
                 foreach (var type in value.Value)
@@ -48,14 +49,12 @@ namespace Snowflake.Emulator.Configuration.Template
                     typeValues.Add(new CustomTypeValue(type[0], type[1]));
                 }
                 types.Add(value.Key,new CustomType(value.Key, typeValues));
-            }*/
-           /*
+            }
             foreach (var value in protoTemplate["keys"])
             {
                 entries.Add(value.Key, new ConfigurationEntry(value.Value["description"], value.Value["protection"], value.Value["type"], value.Key, value.Value["default"]));
             }
-            */
-           IDictionary<string, ConfigurationEntry> entries = (from keys in (IDictionary<object, object>)protoTemplate["keys"] select keys)
+           /*IDictionary<string, ConfigurationEntry> entries = (from keys in (IDictionary<object, object>)protoTemplate["keys"] select keys)
                 .ToDictionary(value => (string)value.Key, value => ((IDictionary<object, object>)value.Value)
                     .ToDictionary(entry => (string)entry.Key, entry => (dynamic)entry.Value))
                 .ToDictionary(value => value.Key, value => new ConfigurationEntry(value.Value["description"], value.Value["protection"], value.Value["type"], value.Key, value.Value["default"]));
@@ -65,7 +64,7 @@ namespace Snowflake.Emulator.Configuration.Template
                    .Select(x => (IList<object>)x)
                    .Select(type => new CustomTypeValue((string)type[0], (string)type[1])))
                .ToDictionary(typeValues => typeValues.Key, typeValues => new CustomType(typeValues.Key, typeValues.Value.ToList()));
-               
+            */
                 
             return new ConfigurationTemplate(stringTemplate, entries, types, booleanMapping, fileName, configName);
         }
