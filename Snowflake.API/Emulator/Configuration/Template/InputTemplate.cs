5769693e6f5a667a10206cfeed66c03834f2e358
RonnChyran <ronny6993@gmail.com>
2014-11-24 00:01:56 -0500

    Add templatekeys to InputTemplate

--- Snowflake.API/Emulator/Configuration/Template/InputTemplate.cs
+++ Snowflake.API/Emulator/Configuration/Template/InputTemplate.cs
@@ -11,13 +11,24 @@ namespace Snowflake.Emulator.Configuration.Template
     public class InputTemplate
     {
         public string StringTemplate { get; private set; }
+        public IReadOnlyList<string> TemplateKeys { get { return this.templateKeys.AsReadOnly(); } }
+        private IList<string> templateKeys;
         public string NoBind { get; private set; }
         public IReadOnlyDictionary<string, GamepadMapping> GamepadMappings { get { return this.gamepadMappings.AsReadOnly(); } }
         private IDictionary<string, GamepadMapping> gamepadMappings;
         public IReadOnlyDictionary<string, KeyboardMapping> KeyboardMappings { get { return this.keyboardMappings.AsReadOnly(); } }
         private IDictionary<string, KeyboardMapping> keyboardMappings;
 
-        public InputTemplate(string stringTemplate, string NoBind);
+
+        public InputTemplate(string stringTemplate, IList<string> templateKeys, string noBind, IDictionary<string, GamepadMapping> gamepadMappings, IDictionary<string, KeyboardMapping> keyboardMappings)
+        {
+            this.StringTemplate = stringTemplate;
+            this.NoBind = noBind;
+            this.gamepadMappings = gamepadMappings;
+            this.keyboardMappings = keyboardMappings;
+            this.templateKeys = templateKeys;
+        }
+
 
     }
 }
