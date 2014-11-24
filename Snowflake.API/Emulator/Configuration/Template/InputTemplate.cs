770ff181967ef3fa323d469b8da37cf3eac20bfe
Ronny Chan <ronny6993@gmail.com>
2014-11-23 21:50:55 -0500

    InputTemplate and ControllerTemplate

---
+++ Snowflake.API/Emulator/Configuration/Template/InputTemplate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Snowflake.Emulator.Configuration.Mapping;
+using Snowflake.Extensions;
+
+namespace Snowflake.Emulator.Configuration.Template
+{
+    public class InputTemplate
+    {
+        public string StringTemplate { get; private set; }
+        public string NoBind { get; private set; }
+        public IReadOnlyDictionary<string, GamepadMapping> GamepadMappings { get { return this.gamepadMappings.AsReadOnly(); } }
+        private IDictionary<string, GamepadMapping> gamepadMappings;
+        public IReadOnlyDictionary<string, KeyboardMapping> KeyboardMappings { get { return this.keyboardMappings.AsReadOnly(); } }
+        private IDictionary<string, KeyboardMapping> keyboardMappings;
+
+        public InputTemplate(string stringTemplate, string NoBind);
+
+    }
+}
