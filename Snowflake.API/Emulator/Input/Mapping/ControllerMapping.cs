1f04ba9c5be9f165eeb2d6379603c99e842609e1
RonnChyran <ronny6993@gmail.com>
2014-11-25 23:50:58 -0500

    Start on controller template parsing

---
+++ Snowflake.API/Emulator/Input/Mapping/ControllerMapping.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Snowflake.Extensions;
+namespace Snowflake.Emulator.Input.Mapping
+{
+    public class ControllerMapping
+    {
+        public ControllerMappingType MappingType { get; private set; }
+        public IReadOnlyDictionary<string, string> KeyMappings { get { return this.keyMappings.AsReadOnly(); } }
+        private IDictionary<string, string> keyMappings;
+        public IReadOnlyDictionary<string, string> InputMappings { get { return this.inputMappings.AsReadOnly(); } }
+        private IDictionary<string, string> inputMappings;
+
+        public ControllerMapping(ControllerMappingType mappingType, IDictionary<string, string> keyMappings, IDictionary<string, string> inputMappings)
+        {
+            this.MappingType = mappingType;
+            this.keyMappings = keyMappings;
+            this.inputMappings = inputMappings;
+        }
+    }
+    public enum ControllerMappingType
+    {
+        GAMEPAD_MAPPING,
+        KEYBOARD_MAPPING
+    }
+
+}
---
+++ Snowflake.API/Emulator/Input/Template/ControllerTemplate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Snowflake.Extensions;
+using Snowflake.Emulator.Input.Mapping;
+namespace Snowflake.Emulator.Input.Template
+{
+    public class ControllerTemplate
+    {
+        public string ControllerID { get; private set; }
+        public string EmulatorID { get; private set; }
+        public string PlatformID { get; private set; }
+
+        public IReadOnlyDictionary<string, ControllerMapping> KeyboardControllerMappings { get { return this.keyboardControllerMappings.AsReadOnly(); } }
+        private IDictionary<string, ControllerMapping> keyboardControllerMappings;
+        public IReadOnlyDictionary<string, ControllerMapping> GamepadControllerMappings { get { return this.gamepadControllerMappings.AsReadOnly(); } }
+        private IDictionary<string, ControllerMapping> gamepadControllerMappings;
+
+        public static void FromDictionary(IDictionary<string, dynamic> protoTemplate)
+        {
+            var controllerid = protoTemplate["controller"];
+            var emulator = protoTemplate["emulator"];
+            var platformid = protoTemplate["platform"];
+            
+        }
+    }
+}
