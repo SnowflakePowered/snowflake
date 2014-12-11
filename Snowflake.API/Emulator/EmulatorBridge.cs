f044bd7027595079e981e9164293cd307a6fcb72
RonnChyran <ronny6993@gmail.com>
2014-12-11 18:39:52 -0500

    Allow keyboard configs as well

--- Snowflake.API/Emulator/EmulatorBridge.cs
+++ Snowflake.API/Emulator/EmulatorBridge.cs
@@ -8,11 +8,19 @@ using Snowflake.Emulator.Configuration;
 using Snowflake.Emulator.Input.Template;
 using Snowflake.Platform.Controller;
 
+using System.Collections;
+
 
 namespace Snowflake.Emulator
 {
     public class EmulatorBridge
     {
+
+        public IReadOnlyDictionary<string, ControllerTemplate> ControllerTemplates { get; private set; }
+        public IReadOnlyDictionary<string, InputTemplate> InputTemplates { get; private set; }
+        public IReadOnlyDictionary<string, ConfigurationTemplate> ConfigurationTemplates { get; private set; }
+        public IReadOnlyList<string> SupportedPlatforms { get; private set; }
+
         public string CompileConfiguration(ConfigurationTemplate template, ConfigurationProfile profile)
         {
             return String.Empty;
@@ -23,19 +31,25 @@ namespace Snowflake.Emulator
         {
             StringBuilder template = new StringBuilder(inputTemplate.StringTemplate);
 
+            
+            var controllerMappings = controllerProfile.ProfileType == ControllerProfileType.KEYBOARD_PROFILE ? 
+                controllerTemplate.KeyboardControllerMappings : controllerTemplate.GamepadControllerMappings;
+
             foreach (ControllerInput input in controllerDefinition.ControllerInputs.Values)
             {
-                string templateKey = controllerTemplate.GamepadControllerMappings["default"].InputMappings[input.InputName];
+                string templateKey = controllerMappings["default"].InputMappings[input.InputName];
                 string inputSetting = controllerProfile.InputConfiguration[input.InputName];
-                string emulatorValue = inputTemplate.GamepadMappings.First().Value[inputSetting];
+                string emulatorValue = controllerProfile.ProfileType == ControllerProfileType.KEYBOARD_PROFILE ? 
+                    inputTemplate.KeyboardMappings.First().Value[inputSetting] : inputTemplate.GamepadMappings.First().Value[inputSetting]; 
                 template.Replace("{" + templateKey + "}", emulatorValue);
             }
+
             foreach (var key in inputTemplate.TemplateKeys)
             {
                 template.Replace("{N}", playerIndex.ToString()); //Player Index
-                if (controllerTemplate.GamepadControllerMappings["default"].KeyMappings.ContainsKey(key))
+                if (controllerMappings["default"].KeyMappings.ContainsKey(key))
                 {
-                    template.Replace("{" + key + "}", controllerTemplate.GamepadControllerMappings["default"].KeyMappings[key]); //Non-input keys
+                    template.Replace("{" + key + "}", controllerMappings["default"].KeyMappings[key]); //Non-input keys
                 }
                 else
                 {
