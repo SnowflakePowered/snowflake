4f69d92c7eba38a4a99f81eecff9a6e7684f66d4
RonnChyran <ronny6993@gmail.com>
2015-01-04 22:14:57 -0500

    partially document scraper api

--- Snowflake.API/Scraper/IScraper.cs
+++ Snowflake.API/Scraper/IScraper.cs
@@ -5,12 +5,35 @@ using Snowflake.Utility;
 using Snowflake.Plugin;
 namespace Snowflake.Scraper
 {
+    /// <summary>
+    /// Represents a scraper plugin that is able to grab information from a source
+    /// </summary>
     [InheritedExport(typeof(IScraper))]
     public interface IScraper : IBasePlugin
     {
+        /// <summary>
+        /// A map of Snowflake's platform IDs to how the scraper source identifies platforms
+        /// BiDictionary allows for backwards mapping from how the scraper source identifiers platforms to Snowflake's platform IDs
+        /// </summary>
         BiDictionary<string, string> ScraperMap { get; }
+        /// <summary>
+        /// Gets the search results given a string query
+        /// </summary>
+        /// <param name="searchQuery">The string to search</param>
+        /// <returns></returns>
         IList<IGameScrapeResult> GetSearchResults(string searchQuery);
+        /// <summary>
+        /// Gets the search results given a string query and the Snowflake platform ID of a platform
+        /// </summary>
+        /// <param name="searchQuery">The string to search</param>
+        /// <param name="platformId">The Snowflake platform ID</param>
+        /// <returns></returns>
         IList<IGameScrapeResult> GetSearchResults(string searchQuery, string platformId);
+        /// <summary>
+        /// Gets the details of 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         Tuple<IDictionary<string, string>, IGameImagesResult> GetGameDetails(string id);
     }
 }
