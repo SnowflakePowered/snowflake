940413e7d6fc84d9a3d86e55325d27d713fed2a2
Ronny Chan <ronny6993@gmail.com>
2015-01-25 22:24:24 -0500

    Tests: Begin to add unit tests with xUnit

---
+++ Snowflake.Tests/Platform/PlatformInfoTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Snowflake.Platform;
+using Snowflake.Tests;
+using Newtonsoft.Json;
+using Xunit;
+namespace Snowflake.Platform.Tests
+{
+    public class PlatformInfoTests
+    {
+        /// <summary>
+        /// Test platform definition loading
+        /// </summary>
+        /// <param name="platformId">platform ID to load</param>
+        [Theory]
+        [MemberData("TestedPlatforms")]
+        public void PlatformLoadTests(string platformId)
+        {
+            string platformDefinition = TestUtilities.GetStringResource("Platforms." + platformId + ".platform");
+            var protoTemplate = JsonConvert.DeserializeObject<IDictionary<string, dynamic>>(platformDefinition);
+            var platform = PlatformInfo.FromJsonProtoTemplate(protoTemplate);
+            Assert.Equal(platformId, platform.PlatformId);
+        }
+
+        public static IEnumerable<object[]> TestedPlatforms
+        {
+            get
+            {
+                // Or this could read from a file. :)
+                return new[]
+                {
+                    new object[] { StonePlatforms.NINTENDO_NES },
+                    new object[] { StonePlatforms.NINTENDO_SNES }
+                };
+            }
+        }
+
+    }
+}
---
+++ Snowflake.Tests/TestUtilities.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace Snowflake.Tests
+{
+    internal static class TestUtilities
+    {
+        /// <summary>
+        /// Gets a TestResource from the TestResources folder
+        /// </summary>
+        /// <param name="resourceName">The filename of the resource</param>
+        /// <returns>The TestResource<</returns>
+        internal static Stream GetResource(string resourceName)
+        {
+            return Assembly.GetCallingAssembly().GetManifestResourceStream(Assembly.GetCallingAssembly().GetName().Name + ".TestResources." + resourceName);
+        }
+
+        /// <summary>
+        /// Gets a TestResource from the TestResources folder as a string
+        /// </summary>
+        /// <param name="resourceName">The filename of the resource</param>
+        /// <returns>The TestResource</returns>
+        internal static string GetStringResource(string resourceName)
+        {
+            using (Stream stream = TestUtilities.GetResource(resourceName))
+            using (var reader = new StreamReader(stream))
+            {
+                string file = reader.ReadToEnd();
+                return file;
+            }
+        }
+    }
+}
