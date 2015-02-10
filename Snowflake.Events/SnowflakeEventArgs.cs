eb5e1d424c0a5d8f56774ce3881eca74b1ecf5f6
Ronny Chan <ronny6993@gmail.com>
2015-02-09 19:06:19 -0500

    Events: Begin event api

---
+++ Snowflake.Events/SnowflakeEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Snowflake.Service;
+
+namespace Snowflake.Events
+{
+    internal class SnowflakeEventArgs
+    {
+        public ICoreService EventCoreService { get; }
+    }
+}
---
+++ Snowflake.Events/SnowflakeEventSource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Snowflake.Events
+{
+    public class SnowflakeEventSource
+    {
+        public delegate void 
+        public event OriginCloseEvent OriginClose;
+
+
+        public void RegisterEvent(Func<object, SnowflakeEventArgs> eventHandler, string eventName)
+        {
+
+        }
+    }
+}
