fc2971052cb3d94184dfa5805441915bbe2031fb
Ronny Chan <ronny6993@gmail.com>
2015-02-13 00:30:13 -0500

    Ajax: Make stub methods

---
+++ Snowflake.StandardAjax/StandardAjax.Platform.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Snowflake.Ajax;
+using Snowflake.Platform;
+using Snowflake.Emulator.Input;
+namespace Snowflake.StandardAjax
+{
+    public partial class StandardAjax
+    {
+        [AjaxMethod(MethodPrefix = "Platform")]
+        public IJSResponse GetPreferences(IJSRequest request)
+        {
+            return new JSResponse(request, null);
+        }
+
+        [AjaxMethod(MethodPrefix = "Platform")]
+        public IJSResponse SetPreference(IJSRequest request)
+        {
+            return new JSResponse(request, null);
+        }
+
+        [AjaxMethod(MethodPrefix = "Platform")]
+        public IJSResponse SetInputDevice(IJSRequest request)
+        {
+            return new JSResponse(request, null);
+        }
+
+        [AjaxMethod(MethodPrefix = "Platform")]
+        public IJSResponse GetPlatforms(IJSRequest request)
+        {
+            return new JSResponse(request, null);
+        }
+    }
+}
---
+++ Snowflake.StandardAjax/StandardAjax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.Reflection;
+using Snowflake.Ajax;
+using Snowflake.Service;
+namespace Snowflake.BaseAjax
+{
+    public partial class StandardAjax : BaseAjaxNamespace
+    {
+        public StandardAjax([Import("coreInstance")] ICoreService coreInstance)
+            : base(Assembly.GetExecutingAssembly(), coreInstance)
+        {
+            
+        }
+    }
+}
