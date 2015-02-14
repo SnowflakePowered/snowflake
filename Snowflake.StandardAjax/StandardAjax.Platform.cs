d38dc56123bfb1ea5c96cff3fd92a7f3b8f7bd05
RonnChyran <ronny6993@gmail.com>
2015-02-14 16:00:19 -0500

    Ajax.Platform: Implement the StandardAjax Platform API

--- Snowflake.StandardAjax/StandardAjax.Platform.cs
+++ Snowflake.StandardAjax/StandardAjax.Platform.cs
@@ -11,27 +11,53 @@ namespace Snowflake.StandardAjax
     public partial class StandardAjax
     {
         [AjaxMethod(MethodPrefix = "Platform")]
+        [AjaxMethodParameter(ParameterName = "platform", ParameterType = AjaxMethodParameterType.StringParameter)]
         public IJSResponse GetPreferences(IJSRequest request)
         {
-            return new JSResponse(request, null);
+            IPlatformInfo platform = this.CoreInstance.LoadedPlatforms[request.GetParameter("platform")];
+            return new JSResponse(request, this.CoreInstance.PlatformPreferenceDatabase.GetPreferences(platform));
         }
 
         [AjaxMethod(MethodPrefix = "Platform")]
+        [AjaxMethodParameter(ParameterName = "platform", ParameterType = AjaxMethodParameterType.StringParameter)]
+        [AjaxMethodParameter(ParameterName = "preference", ParameterType = AjaxMethodParameterType.StringParameter)]
+        [AjaxMethodParameter(ParameterName = "value", ParameterType = AjaxMethodParameterType.StringParameter)]
         public IJSResponse SetPreference(IJSRequest request)
         {
-            return new JSResponse(request, null);
-        }
-
-        [AjaxMethod(MethodPrefix = "Platform")]
-        public IJSResponse SetInputDevice(IJSRequest request)
-        {
-            return new JSResponse(request, null);
+            IPlatformInfo platform = this.CoreInstance.LoadedPlatforms[request.GetParameter("platform")];
+            string preference = request.GetParameter("preference");
+            string value = request.GetParameter("value");
+            switch (preference)
+            {
+                case "emulator":
+                    if (this.CoreInstance.PluginManager.LoadedEmulators.ContainsKey(value))
+                    {
+                        this.CoreInstance.PlatformPreferenceDatabase.SetEmulator(platform, value);
+                        return new JSResponse(request, "Emulator set to " + value, true);
+                    }
+                    else
+                    {
+                        return new JSResponse(request, "Error: Emulator not installed", false);
+                    }
+                case "scraper":
+                    if (this.CoreInstance.PluginManager.LoadedEmulators.ContainsKey(value))
+                    {
+                        this.CoreInstance.PlatformPreferenceDatabase.SetScraper(platform, value);
+                        return new JSResponse(request, "Scraper set to " + value, true);
+                    }
+                    else
+                    {
+                        return new JSResponse(request, "Error: Scraper not installed", false);
+                    }
+                default:
+                    return new JSResponse(request, "Error: unknown preference type", false);
+            }
         }
 
         [AjaxMethod(MethodPrefix = "Platform")]
         public IJSResponse GetPlatforms(IJSRequest request)
         {
-            return new JSResponse(request, null);
+            return new JSResponse(request, this.CoreInstance.LoadedPlatforms);
         }
     }
 }
