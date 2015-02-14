de197152985a43526ab12012844a230f177f0994
Ronny Chan <ronny6993@gmail.com>
2015-02-14 01:44:17 -0500

    Ajax: Create AjaxMethodParameterAttribute

---
+++ Snowflake.API/Ajax/AjaxMethodParameterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snowflake.Ajax
+{
+    /// <summary>
+    /// A metadata attribute to indicate parameter methods
+    /// Does not affect execution.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
+    public class AjaxMethodParameterAttribute
+    {
+        public string ParameterName { get; set; }
+        public AjaxMethodParameterType ParameterType { get; set; }
+    }
+    public enum AjaxMethodParameterType
+    {
+        StringParameter,
+        ObjectParameter,
+        ArrayParameter
+    }
+}
