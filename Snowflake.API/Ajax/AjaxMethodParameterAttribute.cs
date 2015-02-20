9402930be306bf433f71958f8f5014407dd9c49a
Ronny Chan <ronny6993@gmail.com>
2015-02-19 23:29:20 -0500

    JSAPI: Add 'Required' field to AjaxMethodParameterAttribute

--- Snowflake.API/Ajax/AjaxMethodParameterAttribute.cs
+++ Snowflake.API/Ajax/AjaxMethodParameterAttribute.cs
@@ -13,8 +13,16 @@ namespace Snowflake.Ajax
     [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
     public class AjaxMethodParameterAttribute : Attribute
     {
+        //The name of the parameter
         public string ParameterName { get; set; }
+        /// <summary>
+        /// The type of parameter
+        /// </summary>
         public AjaxMethodParameterType ParameterType { get; set; }
+        /// <summary>
+        /// Whether the parameter is required
+        /// </summary>
+        public bool Required { get; set; }
     }
     public enum AjaxMethodParameterType
     {
