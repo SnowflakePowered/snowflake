4c6a29d2ff4a71566b3a5811b9cd217496bbc9d2
RonnChyran <ronny6993@gmail.com>
2015-03-05 00:03:10 -0500

    Events: Create Events for server start and server stop

---
+++ Snowflake.Events/ServiceEvents/ServerStopEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Snowflake.Service;
+
+namespace Snowflake.Events.ServiceEvents
+{
+    public class ServerStopEventArgs : SnowflakeEventArgs
+    {
+        public ServerStopEventArgs(ICoreService eventCoreInstance)
+            : base(eventCoreInstance)
+        {
+
+        }
+    }
+}
