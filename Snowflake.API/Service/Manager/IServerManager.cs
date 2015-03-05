2eb676c0447d55a8d4110b298b05b6e28f8984e0
RonnChyran <ronny6993@gmail.com>
2015-03-05 00:11:43 -0500

    ServerManager: Create interface and implementation

---
+++ Snowflake.API/Service/Manager/IServerManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Snowflake.Service.HttpServer;
+namespace Snowflake.Service.Manager
+{
+    public interface IServerManager
+    {
+        public void RegisterServer(string serverName, IBaseHttpServer httpServer);
+        public void StartServer(string serverName);
+        public void StopServer(string serverName);
+        public IList<string> RegisteredServers { get; }
+    }
+}
---
+++ Snowflake.Service/Service/Manager/ServerManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Snowflake.Service.HttpServer;
+namespace Snowflake.Service.Manager
+{
+    public class ServerManager : IServerManager
+    {
+        private IDictionary<string, IBaseHttpServer> servers;
+        public void RegisterServer(string serverName, IBaseHttpServer httpServer)
+        {
+            servers.Add(serverName, httpServer);
+        }
+
+        public void StartServer(string serverName)
+        {
+            servers[serverName].StartServer();
+        }
+
+        public void StopServer(string serverName)
+        {
+            servers[serverName].StopServer();
+        }
+
+        public IList<string> RegisteredServers
+        {
+            get 
+            {
+                return this.servers.Select(server => server.Key).ToList();
+            }
+        }
+    }
+}
