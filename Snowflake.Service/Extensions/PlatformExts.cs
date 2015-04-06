a2a7777d1f8b48ad584cff63b9ca1ac11d7922b2
Ronny Chan <ronny6993@gmail.com>
2015-04-06 18:46:10 -0400

    ScrapeService: Allow specifying scraper used in a scrape service instance

--- Snowflake.Service/Extensions/PlatformExts.cs
+++ Snowflake.Service/Extensions/PlatformExts.cs
@@ -21,5 +21,17 @@ namespace Snowflake.Extensions
         {
             return new ScrapeService(platform);
         }
+        /// <summary>
+        /// This method is used as part of a core function, 
+        /// and does not serve the pure data role of the PlatformInfo class.
+        /// Extension methods are put in when they are unrelated to the class and is not used as part of the class.
+        /// </summary>
+        /// <param name="platform"></param>
+        /// <param name="scraperName">The name of the scraper</param>
+        /// <returns></returns>s
+        public static ScrapeService GetScrapeEngine(this IPlatformInfo platform, string scraperName)
+        {
+            return new ScrapeService(platform, scraperName);
+        }
     }
 }
