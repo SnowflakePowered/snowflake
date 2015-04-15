168bed016016bab1aaec618325cc5e0d0cb91c69
Ronny Chan <ronny6993@gmail.com>
2015-04-15 18:22:03 -0400

    GameMediaCache: Create Interface

---
+++ Snowflake.API/Game/IGameMediaCache.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Drawing;
+namespace Snowflake.Game
+{
+    public interface IGameMediaCache
+    {
+        string RootPath { get; }
+        void SetBoxartFront(Uri boxartFrontUri);
+        void SetBoxartBack(Uri boxartBackUri);
+        void SetBoxartFull(Uri boxartFrontUri);
+        void SetGameFanart(Uri fanartUri);
+        void SetGameVideo(Uri videoUri);
+        void SetGameMusic(Uri musicUri);
+        Image GetBoxartFrontImage();
+        Image GetBoxartBackImage();
+        Image GetBoxartFullImage();
+        Image GetGameFanartImage();
+    }
+}
