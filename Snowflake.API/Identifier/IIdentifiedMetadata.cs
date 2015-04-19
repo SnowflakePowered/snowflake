d771e5cd8f0f0a77b9ae6a1558f14f7a951cf1ed
Ronny Chan <ronny6993@gmail.com>
2015-04-19 00:51:59 -0400

    IIdentifiedMetadata: Create class to contain identified metadata

---
+++ Snowflake.API/Identifier/IIdentifiedMetadata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snowflake.Identifier
+{
+    public interface IIdentifiedMetadata
+    {
+        string IdentifierName { get; }
+        string ValueType { get; }
+        string Value { get; }
+    }
+}
---
+++ Snowflake/Identifier/IdentifiedMetadata.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snowflake.Identifier
+{
+    public class IdentifiedMetadata : IIdentifiedMetadata
+    {
+
+        public string IdentifierName { get; private set; }
+        public string ValueType { get; private set; }
+        public string Value { get; private set; }
+
+        public IdentifiedMetadata(string identifierName, string valueType, string value)
+        {
+            this.IdentifierName = identifierName;
+            this.ValueType = valueType;
+            this.Value = value;
+        }
+    }
+}
