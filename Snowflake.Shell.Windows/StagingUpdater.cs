f71a200bb7d9169e4b3b1ef0d2ce90786eb0492b
Ronny Chan <ronny6993@gmail.com>
2015-07-18 19:46:38 -0400

    Shell.Windows: Include Squirrel.Windows support

---
+++ Snowflake.Shell.Windows/StagingUpdater.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Squirrel;
+
+namespace Snowflake.Shell.Windows
+{
+    internal class StagingUpdater
+    {
+        private string stagingDirectory;
+        public StagingUpdater(string stagingDirectory)
+        {
+            this.stagingDirectory = stagingDirectory;
+        }
+
+        public async void ProcessStaging()
+        {
+            using (var mgr = new UpdateManager("https://path/to/my/update/folder"))
+            {
+                await mgr.UpdateApp();
+            }
+        }
+    }
+}
