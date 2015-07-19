967776771c7f3ebcc576863263f13543e5d984b2
Ronny Chan <ronny6993@gmail.com>
2015-07-19 16:44:18 -0400

    ThemeServer: Posting to theme server will return theme data

--- Snowflake.Shell.Windows/ThemeServer.cs
+++ Snowflake.Shell.Windows/ThemeServer.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Net;
 using System.Threading;
 using Snowflake.Extensions;
 using Snowflake.Service.HttpServer;
+using Newtonsoft.Json;
 using Unosquare.Labs.EmbedIO;
 using Unosquare.Labs.EmbedIO.Log;
 using Unosquare.Labs.EmbedIO.Modules;
@@ -30,7 +32,23 @@ namespace Snowflake.Shell.Windows
             this.themeWebServer.Module<StaticFilesModule>().UseRamCache = true;
             this.themeWebServer.Module<StaticFilesModule>().DefaultExtension = ".html";
             this.themeWebServer.Module<StaticFilesModule>().DefaultDocument = "index.html";
+            this.themeWebServer.Module<StaticFilesModule>().AddHandler(ModuleMap.AnyPath, HttpVerbs.Post, (server, context) => EchoThemes(context, server));
             this.themeWebServer.RunAsync();
+        }
+        private bool EchoThemes(HttpListenerContext context, WebServer server, bool sendBuffer = true)
+        {
+            HashSet<object> themesDirectory = new HashSet<object>();
+            foreach (string directory in Directory.GetDirectories(this.ThemeRoot))
+            {
+                if (File.Exists(Path.Combine(directory, "theme.json"))) {
+                    string themeInfo = File.ReadAllText(Path.Combine(directory, "theme.json"));
+                    themesDirectory.Add(JsonConvert.DeserializeObject(themeInfo));
+                }
+            }
+            byte[]output = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(themesDirectory));
+            context.Response.OutputStream.Write(output, 0, output.Length);
+            context.Response.OutputStream.Close();
+            return true;
         }
         public void StopServer()
         {
