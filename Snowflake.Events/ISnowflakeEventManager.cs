6ec83b6b3469a54dae1e860cd277f2aca0143972
Ronny Chan <ronny6993@gmail.com>
2015-07-25 01:24:03 -0400

    Snowflake.Events: Create a Dependency-Injection based dynamic event manager rather than hardcoding events into a class

---
+++ Snowflake.Events/ISnowflakeEventManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Snowflake.Events
+{
+    public interface ISnowflakeEventManager
+    {
+        EventHandler<T> GetEvent<T>() where T : SnowflakeEventArgs;
+        void RaiseEvent<T>(T eventArgs) where T : SnowflakeEventArgs;
+        void RegisterEvent<T>(EventHandler<T> eventHandler) where T : SnowflakeEventArgs;
+        void Subscribe<T>(EventHandler<T> eventHandler) where T : SnowflakeEventArgs;
+        void UnregisterEvent<T>() where T : SnowflakeEventArgs;
+        void Unsubscribe<T>(EventHandler<T> eventHandler) where T : SnowflakeEventArgs;
+    }
+}
