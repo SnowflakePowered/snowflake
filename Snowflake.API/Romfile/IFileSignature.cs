fb50051037dcd19e1df01ab4898f92fd838d2853
Ronny Chan <ronny6993@gmail.com>
2015-09-05 17:28:51 -0400

    Romfile: Move namespaces from Snowflake.Scraping namespace into Snowflake.Romfile namespace

--- Snowflake.API/Romfile/IFileSignature.cs
+++ Snowflake.API/Romfile/IFileSignature.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Snowflake.Scraping.FileSignatures
+namespace Snowflake.Romfile
 {
     /// <summary>
     /// Represents the ROM file signature
