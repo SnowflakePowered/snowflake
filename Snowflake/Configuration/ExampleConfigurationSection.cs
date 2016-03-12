07e97c9d474a28dec7e1cbf162cf0cb0b673f6a0
Ronny Chan <ronny6993@gmail.com>
2016-03-12 18:15:47 -0500

    IniConfigurationSerializer: Implement a basic INI-format serializer

---
+++ Snowflake/Configuration/ExampleConfigurationSection.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Snowflake.Configuration.Attributes;
+
+namespace Snowflake.Configuration
+{
+    //todo move this to tests
+    //Taken from a section of Dolphin.ini
+    /*
+    FullscreenResolution = Auto
+    Fullscreen = True
+    RenderToMain = True
+    RenderWindowXPos = -1
+    RenderWindowYPos = -1
+    RenderWindowWidth = 640
+    RenderWindowHeight = 480
+    RenderWindowAutoSize = False
+    KeepWindowOnTop = False
+    ProgressiveScan = False
+    PAL60 = False
+    */
+
+    public class ExampleConfigurationSection : ConfigurationSection
+    {
+        [ConfigurationOption("FullscreenResolution")]
+        public FullscreenResolution FullscreenResolution { get; set; } = FullscreenResolution.Auto;
+
+        [ConfigurationOption("Fullscreen")]
+        public bool Fullscreen { get; set; } = true;
+
+        [ConfigurationOption("RenderToMain")]
+        public bool RenderToMain { get; set; } = true;
+
+        [ConfigurationOption("RenderWindowWidth")]
+        public int RenderWindowWidth { get; set; } = 640;
+
+        [ConfigurationOption("RenderWindowHeight")]
+        public int RenderWindowHeight { get; set; } = 480;
+
+        [ConfigurationOption("SomeIterableOption{N}", IsIterable = true)]
+        public int MyOption { get; set; } = 480;
+
+        public ExampleConfigurationSection()
+        {
+            this.SectionName = "Display";
+            this.ConfigurationFileName = "Dolphin.ini";
+            this.DisplayName = "Display Options";
+        }
+    }
+
+    public enum FullscreenResolution
+    {
+        [SelectionOption("Auto", DisplayName = "Auto")]
+        Auto,
+        [SelectionOption("640x480")]
+        Resolution640X480,
+        [SelectionOption("720x480")]
+        Resolution720X480,
+        [SelectionOption("720x576")]
+        Resolution720X576,
+        [SelectionOption("800x480")]
+        Resolution800X480,
+        [SelectionOption("800x600")]
+        Resolution800X600,
+        [SelectionOption("1024x600")]
+        Resolution1024X600,
+        [SelectionOption("1024x768")]
+        Resolution1024X768,
+        [SelectionOption("1152x648")]
+        Resolution1152X648,
+        [SelectionOption("1280x720")]
+        Resolution1280X720,
+        [SelectionOption("1280x768")]
+        Resolution1280X768,
+        [SelectionOption("1280x1024")]
+        Resolution1280X1024,
+        [SelectionOption("1360x768")]
+        Resolution1360X768,
+        [SelectionOption("1400x1050")]
+        Resolution1400X1050,
+        [SelectionOption("1600x900")]
+        Resolution1600X900,
+        [SelectionOption("1600x1050")]
+        Resolution1600X1050,
+        [SelectionOption("1776x1000")]
+        Resolution1776X1000,
+        [SelectionOption("1920x1080")]
+        Resolution1920X1080,
+        [SelectionOption("2560x1440")]
+        Resolution2560X1440,
+        [SelectionOption("3840x2160")]
+        Resolution3840X2160
+
+
+
+
+    }
+
+    
+}
---
+++ Snowflake/Configuration/IniConfigurationSerializer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using Snowflake.Configuration.Attributes;
+using Snowflake.Emulator.Configuration;
+
+namespace Snowflake.Configuration
+{
+    public class IniConfigurationSerializer : ConfigurationSerializer
+    {
+        public bool OutputHeader { get; set; }
+       
+        public IniConfigurationSerializer(IBooleanMapping booleanMapping, string nullSerializer, bool outputHeader)
+            : base(booleanMapping, nullSerializer)
+        {
+            this.OutputHeader = outputHeader;
+        }
+
+        public override string SerializeLine<T>(string key, T value)
+        {
+            return $"{key} = {this.SerializeValue(value)}";
+        }
+
+        public override string SerializeIterableLine<T>(string key, T value, int iteration)
+        {
+            return $"{key.Replace(ConfigurationSerializer.IteratorKey, iteration.ToString())} = {this.SerializeValue(value)}";
+        }
+
+        public override string Serialize(IConfigurationSection configurationSection)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            IEnumerable<PropertyInfo> properties = configurationSection.GetType()
+                .GetRuntimeProperties()
+                .Where(propertyInfo => propertyInfo.IsDefined(typeof (ConfigurationOptionAttribute), true));
+
+            if(this.OutputHeader) stringBuilder.AppendLine($"[{configurationSection.SectionName}]");
+
+            foreach (var prop in properties)
+            {
+                var data = prop.GetCustomAttribute<ConfigurationOptionAttribute>();
+                var value = prop.GetValue(configurationSection);
+                stringBuilder.AppendLine(this.SerializeLine(data.OptionName, value));
+            }
+            return stringBuilder.ToString();
+        }
+
+        public override string SerializeIterable(IConfigurationSection iterableConfigurationSection, int iteration)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            IEnumerable<PropertyInfo> properties = iterableConfigurationSection.GetType()
+                .GetRuntimeProperties()
+                .Where(propertyInfo => propertyInfo.IsDefined(typeof(ConfigurationOptionAttribute), true));
+
+            if (this.OutputHeader) stringBuilder.AppendLine($@"[{iterableConfigurationSection.SectionName
+                .Replace(ConfigurationSerializer.IteratorKey, iteration.ToString())}]");
+
+            foreach (var prop in properties)
+            {
+                var data = prop.GetCustomAttribute<ConfigurationOptionAttribute>();
+                var value = prop.GetValue(iterableConfigurationSection);
+                stringBuilder.AppendLine(data.IsIterable
+                    ? this.SerializeIterableLine(data.OptionName, value, iteration)
+                    : this.SerializeLine(data.OptionName, value));
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
