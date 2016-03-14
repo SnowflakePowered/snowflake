41c4855d38e2e230fa26b6f9f20265874d105a09
Ronny Chan <ronny6993@gmail.com>
2016-03-14 00:28:55 -0400

    InputManager: Rework to include more data such as type of device, enumeration order, product guids and instance guids as GUID objects, and separate XInput controller indicators.

--- Snowflake.InputManager.DirectInput/InputManager.cs
+++ Snowflake.InputManager.DirectInput/InputManager.cs
@@ -1,63 +1,104 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
+using SharpDX;
+using SharpDX.Multimedia;
 using SharpDX.DirectInput;
-using Snowflake.Emulator.Input.InputManager;
-
+using SharpDX.XInput;
+using Snowflake.Input;
+using DeviceType = Snowflake.Input.DeviceType; 
 namespace Snowflake.InputManager
 {
     public class InputManager : IInputManager
     {
 
-        public IList<IInputDevice> GetGamepads()
+        public IEnumerable<IInputDevice> GetAllDevices()
         {
             IList<IInputDevice> inputDevices = new List<IInputDevice>();
             var directInput = new DirectInput();
-            var gamepads = directInput.GetDevices().Where(device => device.Usage == SharpDX.Multimedia.UsageId.GenericGamepad);
+            var devices = directInput.GetDevices(DeviceClass.All, DeviceEnumerationFlags.AttachedOnly);
+                    
+                //    .Where(device => device.Information.IsHumanInterfaceDevice);
 
-            int xinputCounter = 1; //hacky way to "guess" xinput position.
-            gamepads.Each((device, i) =>
+            var directInputGamepads =
+                this.GetGenericGamepads(devices.Where(device => device.Usage == UsageId.GenericGamepad), directInput);
+            var xinputGamepads = this.GetXInputGamepads();
+            var keyboards = directInput.GetDevices(DeviceClass.Keyboard, DeviceEnumerationFlags.AllDevices).Select(keyboard => new InputDevice()
             {
-                var gamepadDevice = new Joystick(directInput, device.InstanceGuid);
+                DI_InstanceGUID = keyboard.InstanceGuid,
+                DI_InstanceName = keyboard.InstanceName.Trim('\0'),
+                DI_ProductName = keyboard.ProductName.Trim('\0'),
+                DI_ProductGUID = keyboard.ProductGuid,
+                DI_DeviceType = DeviceType.Keyboard
+            });
 
-                var info = new InputDevice
+            var mice = directInput.GetDevices(DeviceClass.Pointer, DeviceEnumerationFlags.AllDevices).Select(mouse => new InputDevice()
+            {
+                DI_InstanceGUID = mouse.InstanceGuid,
+                DI_InstanceName = mouse.InstanceName.Trim('\0'),
+                DI_ProductName = mouse.ProductName.Trim('\0'),
+                DI_ProductGUID = mouse.ProductGuid,
+                DI_DeviceType = DeviceType.Keyboard
+            });
+
+            return directInputGamepads.Concat(xinputGamepads).Concat(keyboards).Concat(mice);
+        }
+
+        private IEnumerable<IInputDevice> GetGenericGamepads(IEnumerable<DeviceInstance> gamepads, DirectInput directInput)
+        {
+            var inputDevices = new List<IInputDevice>();
+            for (int i = 0; i < gamepads.Count(); i++)
+            {
+                DeviceInstance deviceInstance = gamepads.ElementAt(i);
+                Device device = new Joystick(directInput, deviceInstance.InstanceGuid);
+
+                var inputDevice = new InputDevice()
                 {
-                    DeviceIndex = i,
-                    DI_InstanceGUID = gamepadDevice.Information.InstanceGuid.ToString(),
-                    DI_InstanceName = gamepadDevice.Information.InstanceName,
-                    DI_InterfacePath = gamepadDevice.Properties.InterfacePath,
-                    DI_ProductGUID = gamepadDevice.Information.ProductGuid.ToString(),
-                    DI_ProductID = gamepadDevice.Properties.ProductId,
-                    DI_JoystickID = gamepadDevice.Properties.JoystickId,
-                    DI_ProductName = gamepadDevice.Information.ProductName
+                    DI_InstanceGUID = deviceInstance.InstanceGuid,
+                    DI_InstanceName = deviceInstance.InstanceName.Trim('\0'),
+                    DI_ProductName = deviceInstance.ProductName.Trim('\0'),
+                    DI_ProductGUID = deviceInstance.ProductGuid,
+                    DI_DeviceType = DeviceType.Gamepad,
+                    DI_EnumerationNumber = i
                 };
 
-                if (info.DI_InterfacePath.Contains("IG_", StringComparison.OrdinalIgnoreCase))
+                try
                 {
-                    info.XI_IsXInput = true;
-                    info.XI_GamepadIndex = xinputCounter;
-                    xinputCounter++;
+                    inputDevice.DI_InterfacePath = device.Properties.InterfacePath.Trim('\0');
+                    inputDevice.DI_JoystickID = device.Properties.JoystickId;
+                    inputDevice.DI_ProductID = device.Properties.ProductId;
+                    inputDevice.DI_VendorID = device.Properties.VendorId;
                 }
-                else
+                catch (SharpDXException)
                 {
-                    info.XI_IsXInput = false;
+                    inputDevice.DI_JoystickID = null;
+                    inputDevice.DI_InterfacePath = null;
                 }
-                inputDevices.Add(info);
-            });
+                finally
+                {
+                    inputDevices.Add(inputDevice);
+                }
+            }
             return inputDevices;
         }
-    }
 
-    internal static class Extensions
-    {
-        internal static void Each<T>(this IEnumerable<T> ie, Action<T, int> action)
+        private IEnumerable<IInputDevice> GetXInputGamepads()
         {
-            var i = 0;
-            foreach (var e in ie) action(e, i++);
-        }
-        internal static bool Contains(this string source, string toCheck, StringComparison comp)
-        {
-            return source.IndexOf(toCheck, comp) >= 0;
+            var inputDevices = new List<IInputDevice>();
+            for (int i = 0; i < 4; i++)
+            {
+                var xinput = new SharpDX.XInput.Controller((UserIndex)i);
+                if (!xinput.IsConnected) continue;
+                var device = new InputDevice()
+                {
+                    DI_DeviceType = DeviceType.Gamepad,
+                    XI_GamepadIndex = i,
+                    XI_IsXInput = true
+                };
+                inputDevices.Add(device);
+            }
+            return inputDevices;
         }
     }
 }
