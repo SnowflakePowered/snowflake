1462aaa4c3c3c8aa01ce4a93bfdd7b8759bb3e12
Ronny Chan <ronny6993@gmail.com>
2016-05-09 17:22:16 -0400

    SqliteDatabase: Refine simple query API based on Dapper

--- Snowflake.Utility/SqliteDatabase.cs
+++ Snowflake.Utility/SqliteDatabase.cs
@@ -28,6 +28,15 @@ namespace Snowflake.Utility
             return new SQLiteConnection(this.dbConnectionString);
         }
 
+        /// <summary>
+        /// Queries on a new connection to the database.
+        /// The connection will be safely closed after the operation.
+        /// <br/>
+        /// Attempting to close the connection yourself will result in an exception.
+        /// </summary>
+        /// <typeparam name="T">The type the query will return</typeparam>
+        /// <param name="queryFunction">A function to query the database using the opened connection</param>
+        /// <returns>The requested data.</returns>
         public T Query<T>(Func<DbConnection, T> queryFunction)
         {
             T record;
@@ -40,6 +49,13 @@ namespace Snowflake.Utility
             return record;
         }
 
+        /// <summary>
+        /// Executes on a new connection to the database.
+        /// The connection will be safely closed after the operation.
+        /// <br/>
+        /// Attempting to close the connection yourself will result in an exception.
+        /// </summary>
+        /// <param name="queryFunction">A function to query the database using the opened connection</param>
         public void Execute(Action<DbConnection> queryFunction)
         {
             using (SQLiteConnection dbConnection = this.GetConnection())
@@ -50,19 +66,41 @@ namespace Snowflake.Utility
             }
         }
 
-        public IEnumerable<T> QuerySimple<T>(string query, object param = null)
+        /// <summary>
+        /// Queries on a new connection to the database.
+        /// The connection will be safely closed after the operation.
+        /// </summary>
+        /// <typeparam name="T">The type the query will return</typeparam>
+        /// <param name="query">The SQL query to execute</param>
+        /// <param name="param">The query parameters</param>
+        /// <returns>The requested data.</returns>
+        public IEnumerable<T> Query<T>(string query, object param = null)
         {
             return this.Query(dbConnection => dbConnection.Query<T>(query, param));
         }
 
-        public void ExecuteSimple(string query, object param = null)
+        /// <summary>
+        /// Executes on a new connection to the database.
+        /// The connection will be safely closed after the operation.
+        /// </summary>
+        /// <param name="query">The SQL query to execute</param>
+        /// <param name="param">The query parameters</param>
+        public void Execute(string query, object param = null)
         {
             this.Execute(dbConnection => dbConnection.Execute(query, param));
         }
 
-        public T QuerySingleSimple<T>(string query, object param = null)
+        /// <summary>
+        /// Executes a single row query
+        /// The connection will be safely closed after the operation.
+        /// </summary>
+        /// <typeparam name="T">The type the query will return</typeparam>
+        /// <param name="query">The SQL query to execute</param>
+        /// <param name="param">The query parameters</param>
+        /// <returns>The requested data, or null if not present.</returns>
+        public T QueryFirstOrDefault<T>(string query, object param = null)
         {
-            return this.Query(dbConnection => dbConnection.QueryFirst<T>(query, param));
+            return this.Query(dbConnection => dbConnection.QueryFirstOrDefault<T>(query, param));
         }
 
   
