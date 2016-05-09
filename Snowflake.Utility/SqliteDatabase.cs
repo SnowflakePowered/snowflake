eb1ce2a6eebd0c3a027064e8d38460370eb856d7
Ronny Chan <ronny6993@gmail.com>
2016-05-08 23:36:25 -0400

    SqliteDatabase: Implement simple query wrappers using Dapper.

--- Snowflake.Utility/SqliteDatabase.cs
+++ Snowflake.Utility/SqliteDatabase.cs
@@ -1,5 +1,8 @@
-﻿using System.Data.SQLite;
+﻿using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SQLite;
 using System.IO;
+using Dapper;
 
 namespace Snowflake.Utility
 {
@@ -22,6 +25,40 @@ namespace Snowflake.Utility
         public SQLiteConnection GetConnection()
         {
             return new SQLiteConnection(this.dbConnectionString);
+        }
+
+        public IEnumerable<T> QuerySimple<T>(string query, object param = null)
+        {
+            IEnumerable<T> records;
+            using (SQLiteConnection dbConnection = this.GetConnection())
+            {
+                dbConnection.Open();
+                records = dbConnection.Query<T>(query, param);
+                dbConnection.Close();
+            }
+            return records;
+        }
+
+        public void ExecuteSimple(string query, object param = null)
+        {
+            using (SQLiteConnection dbConnection = this.GetConnection())
+            {
+                dbConnection.Open();
+                dbConnection.Execute(query, param);
+                dbConnection.Close();
+            }
+        }
+
+        public T QuerySingleSimple<T>(string query, object param = null)
+        {
+            T record;
+            using (SQLiteConnection dbConnection = this.GetConnection())
+            {
+                dbConnection.Open();
+                record = dbConnection.QueryFirst<T>(query);
+                dbConnection.Close();
+            }
+            return record;
         }
     }
 }
