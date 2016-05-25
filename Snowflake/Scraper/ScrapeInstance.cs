65b58e3e0cb5af9a20f7524bcadc8ed7e0c0f5cd
Ronny Chan <ronny6993@gmail.com>
2016-05-24 22:00:40 -0400

    Framework: Target 4.5.2 for now

--- Snowflake/Scraper/ScrapeInstance.cs
+++ Snowflake/Scraper/ScrapeInstance.cs
@@ -6,11 +6,13 @@ using System.Text;
 using System.Threading.Tasks;
 using Snowflake.Records.Game;
 using Snowflake.Romfile;
-using Snowflake.Service;
 using System.IO.Compression;
 using Snowflake.Platform;
 using Snowflake.Records.File;
 using Snowflake.Records.Metadata;
+using Snowflake.Scraper.Shiragame;
+using Snowflake.Service;
+using Snowflake.Utility;
 
 namespace Snowflake.Scraper
 {
@@ -18,52 +20,21 @@ namespace Snowflake.Scraper
     {
         private readonly IList<string> fileNames;
         private readonly IStoneProvider stoneProvider;
+        private readonly IShiragameProvider shiragameProvider;
         private readonly IEnumerable<IFileSignature> fileSignatures;
         public ScrapeInstance(IEnumerable<string> fileNames, 
             IScraper gameScraper, 
             IStoneProvider stoneProvider,
+            IShiragameProvider shiragameProvider,
             IEnumerable<IFileSignature> fileSignatures)
         {
             this.fileNames = fileNames.ToList();
             this.stoneProvider = stoneProvider;
             this.fileSignatures = fileSignatures;
+            this.shiragameProvider = shiragameProvider;
         }
 
-        IFileRecord GetZipFileInformation(string romfile)
-        {
-            using (Stream stream = File.Open(romfile, FileMode.Open, FileAccess.Read))
-            using (ZipArchive archive = new ZipArchive(stream))
-            {
-                var romContents = archive.Entries.FirstOrDefault();
-                if (romContents == null) return null; ;
-                var potentialMimetypes = (from platform in this.stoneProvider.Platforms.Values
-                                          from fileType in platform.FileTypes
-                                          let extension = Path.GetExtension(romContents.FullName)
-                                          where fileType.Key == extension
-                                          select new KeyValuePair<string, string>(fileType.Value, fileType.Key))
-                                          .ToDictionary(t => t.Key, t => t.Value);
-                if (!potentialMimetypes.Any()) return null;
-                using (Stream romStream = romContents.Open())
-                {
-                    var fileSignature = (from fs in this.fileSignatures
-                                         where fs.FileTypes.Intersect(potentialMimetypes.Keys).Any()
-                                         where fs.HeaderSignatureMatches(romStream)
-                                         select fs).FirstOrDefault();
-                    //todo compare with crc32 database
-                    //todo deal with mame
-                  
-                    if (fileSignature == null) return null;
-                    var platform = this.stoneProvider.Platforms[fileSignature.SupportedPlatform];
-                    string mimeType = platform.FileTypes[Path.GetExtension(romContents.FullName)];
-                    IFileRecord record = new FileRecord(romfile, $"{mimeType}+zip");
-                    record.Metadata.Add("rom_internalname", fileSignature.GetInternalName(romStream));
-                    record.Metadata.Add("rom_gameid", fileSignature.GetGameId(romStream));
-                    record.Metadata.Add("file_crc32", Utility.FileHash.GetCRC32(romStream));
-                    return record;
-                }
-            }
-        }
-
+  
         IFileRecord GetFileInformation(string romfile)
         {
             var potentialMimetypes = (from platform in this.stoneProvider.Platforms.Values
@@ -79,9 +50,14 @@ namespace Snowflake.Scraper
                                      where fs.FileTypes.Intersect(potentialMimetypes.Keys).Any()
                                      where fs.HeaderSignatureMatches(romStream)
                                      select fs).FirstOrDefault();
-                //todo compare with crc32 database
 
-                if (fileSignature == null) return null;
+
+                if (fileSignature == null)
+                {
+                    var romInfo = this.shiragameProvider.GetFromMd5(FileHash.GetMD5(romStream));
+                    if (romInfo == null) return null;
+                    //todo mame.
+                }
                 var platform = this.stoneProvider.Platforms[fileSignature.SupportedPlatform];
                 string mimeType = platform.FileTypes[Path.GetExtension(romfile)];
                 IFileRecord record = new FileRecord(romfile, $"{mimeType}");
@@ -91,10 +67,8 @@ namespace Snowflake.Scraper
                 return record;
             }
         }
-        IEnumerable<IFileRecord> GetZipFileInformation(IEnumerable<string> zipfiles)
-        {
-            return zipfiles.AsParallel().Select(this.GetZipFileInformation).Where(f => f != null);
-        }
+
+
         IEnumerable<IFileRecord> GetFileInformation(IEnumerable<string> filenames)
         {
 
