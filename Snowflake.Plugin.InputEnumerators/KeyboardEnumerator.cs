9ee961138f1c20f314eb8a580ef1e6d6b1bead08
RonnChyran <ronny6993@gmail.com>
2016-08-10 16:08:41 -0400

    Implement KeyboardEnumerator

---
+++ Snowflake.Plugin.InputEnumerators/KeyboardEnumerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Win32.SafeHandles;
+using Snowflake.Extensibility;
+using Snowflake.Input;
+using Snowflake.Input.Device;
+using Snowflake.Service;
+
+namespace Snowflake.Plugin.InputEnumerators
+{
+    [Plugin("InputEnumerator-Keyboard")]
+    public class KeyboardEnumerator : InputEnumerator
+    {
+        private readonly IInputManager inputManager;
+
+        public KeyboardEnumerator(IInputManager inputManager)
+        {
+            this.inputManager = inputManager;
+        }
+
+        public override IEnumerable<IInputDevice> GetConnectedDevices()
+        {
+            return from device in this.inputManager.GetAllDevices()
+                where device.DI_DeviceType == DeviceType.Keyboard 
+                select new InputDevice(InputApi.DirectInput, device, this.ControllerLayout);
+        }
+    }
+}
