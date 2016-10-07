f70cbbf35f872dda9d4f2177b25cddd50a120662
RonnChyran <ronny6993@gmail.com>
2016-10-07 00:29:34 -0400

    Dependencies: Update

---
+++ Snowflake.Exports/Stone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Snowflake.Platform;
+using Snowflake.Service;
+
+namespace Snowflake.Exports
+{
+    public class Stone
+    {
+        private readonly ICoreService coreService;
+        public Stone(ICoreService coreService)
+        {
+            this.coreService = coreService;
+        }
+
+        public IList<IPlatformInfo> GetPlatformInfos()
+        {
+            return this.coreService.Get<IStoneProvider>().Platforms.Values.ToList();
+        }
+
+    }
+}
