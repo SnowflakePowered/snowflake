dfa77ec1a73c0f6d1b47a714f8ca3ca505dc47cf
Ronny Chan <ronny6993@gmail.com>
2016-11-21 16:53:35 -0500

    Cross-platform: Begin to port InputEnumerators

---
+++ Snowflake.Support.Windows.InputManager/PluginContainer.cs
@@ -0,0 +1,15 @@
+﻿using Snowflake.Extensibility;
+using Snowflake.Input.Device;
+using Snowflake.Services;
+
+namespace Snowflake.Plugin.InputManager.Win32
+{
+    [ContainerLoadPriority(ContainerLoadPriority.Service)]
+    public class InputManagerContainer : IPluginContainer
+    {
+        public void Compose(ICoreService coreInstance)
+        {
+            coreInstance.RegisterService<IInputManager>(new InputManager());
+        }
+    }
+}
