1162155293df75481b67abff10017aa31511fb6d
RonnChyran <ronny6993@gmail.com>
2016-12-12 17:31:14 -0500

    Remoting: Lay out the foundations for the REST remoting server

---
+++ src/Snowflake.Support.Remoting/Resources/Plugins.cs
@@ -0,0 +1,39 @@
+﻿using Snowflake.Records.Game;
+using Snowflake.Services;
+using Snowflake.Support.Remoting.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Snowflake.Support.Remoting.Resources
+{
+    public class Plugins
+    {
+
+        private IPluginManager PluginManager{ get; }
+        public Plugins(ICoreService core)
+        {
+            this.PluginManager = core.Get<IPluginManager>();
+        }
+
+        
+        [Endpoint(RemotingVerbs.Read, "~:plugins")]
+        public IEnumerable<string> ListPlugins()
+        {
+            return this.PluginManager.Registry.Select(p => p.Key);
+        }
+
+        [Endpoint(RemotingVerbs.Read, "~:plugins:{echo}")]
+        public string Echo(string echo)
+        {
+            return echo;
+        }
+
+        [Endpoint(RemotingVerbs.Read, "~:plugins:{echo}:{concat}")]
+        public string EchoConcat(string echo, string concat)
+        {
+            return echo;
+        }
+    }
+}
