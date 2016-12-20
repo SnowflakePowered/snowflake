5a4116dbcfdc8070d26171329a8181cce8dc167c
RonnChyran <ronny6993@gmail.com>
2016-12-19 21:31:34 -0500

    React: Enforce Immutability

--- remoting/src/components/Hello.tsx
+++ remoting/src/components/Hello.tsx
@@ -1,22 +1,26 @@
-import * as React from "react";
-import * as Games from "../remoting/Games";
+import * as React from "react"
+import * as Games from "../remoting/Games"
+import * as Stone from "../remoting/Stone"
 
-export interface HelloProps { compiler: string; framework: string; }
+export interface HelloProps { 
+    compiler: string
+    framework: string 
+}
 
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the 'undefined' type.
 export class Hello extends React.Component<HelloProps, undefined> {
     render() {
-        return <h1>Everything is working! {this.props.compiler} and {this.props.framework}!</h1>;
+        return <h1>Nothing is working! {this.props.compiler} and {this.props.framework}!</h1>
     }
-    
+
     componentDidMount() {
         this.getGame()
-        console.log("hello");
+        console.log("hello")
     }
 
     async getGame() {
-        let games = await Games.getGames();
+        let games = await Stone.getPlatforms()
         console.log(games)
     }
 }
