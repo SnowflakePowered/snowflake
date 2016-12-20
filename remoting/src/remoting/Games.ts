127e36c8990731b8eddc9981fb0d2534be33767f
RonnChyran <ronny6993@gmail.com>
2016-12-19 21:31:34 -0500

    React: Enforce Immutability

--- remoting/src/remoting/Games.ts
+++ remoting/src/remoting/Games.ts
@@ -1,9 +1,50 @@
-import { Response, request } from "./Remoting";
-
+import { Response, request } from "./Remoting"
+import { Platform } from "./Stone"
+import * as Immutable from "seamless-immutable"
 export interface Game {
-
+    Files : File[]
+    Guid  : string
+    Metadata :  { [key: string] : Metadata }
+    PlatformId : string
+    Title : string
+}
+export interface File {
+    FilePath : string
+    Guid : string
+    Metadata : { [key: string] : Metadata }
+    MimeType : string
+    Record : string
 }
 
-export async function getGames() : Promise<Game[]>  {
-   return (await request<Game[]>("http://localhost:9696/games")).Response;
+export interface Metadata {
+    Guid : string
+    Key : string
+    Record : string
+    Value : string
 }
+
+export async function getGames() : Promise<Iterable<Game>>  {
+   let games = await request<Game[]>("http://localhost:9696/games")
+   if (games.Error) throw games.Error
+   
+   return Immutable.from(games.Response)
+}
+
+export async function getGame(uuid : string) : Promise<Game> {
+    let game = await request<Game>(`http://localhost:9696/games/${uuid}`)
+    if (game.Error) throw game.Error
+    return Immutable.from(game.Response)
+}
+
+export async function createGame(title : string, platform : Platform) : Promise<Game> {
+    let game = await request<Game>("http://localhost:9696/games", { title: title, platform: platform.PlatformID }, "Create")
+    if (game.Error) throw game.Error
+    return Immutable.from(game.Response)
+}
+
+export async function createFile(game : Game, path : string, mimetype: string) {
+     let newGame = await request<Game>(`http://localhost:9696/games/${game.Guid}/files`, { path : path, mimetype : mimetype }, "Create")
+     if (newGame.Error) throw newGame.Error
+     return Immutable.from(newGame.Response)
+}
+
--- remoting/src/remoting/Remoting.ts
+++ remoting/src/remoting/Remoting.ts
@@ -1,4 +1,4 @@
-import "isomorphic-fetch";
+import "isomorphic-fetch"
 
 export interface Response<T> {
     Response : T
@@ -9,11 +9,32 @@ export interface Error {
     Message?: string
 }
 
-export async function request<T>(url:string, payload:string = "") : Promise<Response<T>>  {
-    let result = await fetch(url);
+export type Verb = "Create" | "Read" | "Delete" | "Update"
+
+export async function request<T>(url : string, payload : any = "", verb : Verb = "Read") : Promise<Response<T>>  {
+    if (typeof payload != "string") payload = JSON.stringify(payload)
+    if( verb == "Read" || verb == "Delete") payload = undefined
+    let result = await fetch(url, {
+        method: toHttpVerb(verb),
+        body: payload,
+        mode: "cors"
+    })
     if (result.ok) {
         let json = await result.json()
         return { Response: json.Response, Error:json.Error }
     }
     return { Response: null, Error: { Message: "Unable to resolve promise." }}
+}
+
+function toHttpVerb(verb : Verb) : string {
+    switch(verb) {
+        case "Create":
+            return "POST"
+        case "Read": 
+            return "GET"
+        case "Delete":
+            return "DELETE"
+        case "Update":
+            return "PUT"
+    }
 }
