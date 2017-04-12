5cf6f21f4135c0aac1e40c40c723c0a0b1816340
RonnChyran <ronny6993@gmail.com>
2017-04-11 21:39:02 -0400

    Cleanup

--- js/snowflake-react-boilerplate/src/App.js
+++ js/snowflake-react-boilerplate/src/App.js
@@ -1,32 +1,18 @@
 import React, { Component } from 'react'
-import UIFrame from './components/uiframe/'
-import './App.css'
-import withSnowflake from './snowflake/withSnowflake/'
-import SnowflakeProvider from './snowflake/SnowflakeProvider/'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import injectTapEventPlugin from 'react-tap-event-plugin'
-injectTapEventPlugin()
-
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
-
-import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
-
+import SnowflakeProvider from './snowflake/SnowflakeProvider'
+import { Route } from 'react-router-dom'
+import { ConnectedRouter } from 'react-router-redux'
 
 class App extends Component {
   render () {
     return (
-      <MuiThemeProvider>
-        <ConnectedRouter history={this.props.history}>
-          <Route component={(({location}) =>
-            <SnowflakeProvider location={location}>
-              <UIFrame/>
-            </SnowflakeProvider>
+      <ConnectedRouter history={this.props.history}>
+        <Route component={(({location}) =>
+          <SnowflakeProvider location={location}>
+            <div>Hello World</div>
+          </SnowflakeProvider>
           )} />
-        </ConnectedRouter>
-      </MuiThemeProvider>
+      </ConnectedRouter>
     )
   }
 }
