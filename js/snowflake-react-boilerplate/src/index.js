7fa2a3c3b5ecce2c4fc8bed9ab3eaa1b757973e5
RonnChyran <ronny6993@gmail.com>
2017-04-11 21:59:21 -0400

    Setup sass infrastructure

--- js/snowflake-react-boilerplate/src/index.js
+++ js/snowflake-react-boilerplate/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './index.css'
+import './index.scss'
 import { Provider } from 'react-redux'
 import Store from './store'
 import createHistory from 'history/createBrowserHistory'
