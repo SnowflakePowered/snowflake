d1d79826f77073efae5f909f32f2abc4c13823ac
RonnChyran <ronny6993@gmail.com>
2017-04-26 14:46:18 -0400

    react/components: GameDetailsHeaderView

---
+++ js/snowflake-react-boilerplate/src/stories/components/GameCard.story.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import injectSheet from 'react-jss'
+import GameCard from 'components/presentation/GameCard'
+
+export const MultiCard = injectSheet({
+  container: {
+    display: 'flex'
+  },
+  card: {
+    margin: 10
+  }
+})(({ classes }) => (
+  <div className={classes.container}>
+    <GameCard className={classes.card}
+        image="https://upload.wikimedia.org/wikipedia/en/3/32/Super_Mario_World_Coverart.png"
+        title="Super Mario World" publisher="Nintendo" />
+    <GameCard className={classes.card} image="http://vignette2.wikia.nocookie.net/mario/images/6/60/SMBBoxart.png/revision/latest?cb=20120609143443"
+        title="Super Mario Bros." publisher="Nintendo" />
+  </div>
+))
+
+export const SingleCard = () => <GameCard
+    image="http://vignette2.wikia.nocookie.net/mario/images/6/60/SMBBoxart.png/revision/latest?cb=20120609143443"
+    title="Super Mario Bros." publisher="Nintendo" />
---
+++ js/snowflake-react-boilerplate/src/stories/utils/constants.js
@@ -0,0 +1,4 @@
+export const landscapeCover = 'https://upload.wikimedia.org/wikipedia/en/3/32/Super_Mario_World_Coverart.png'
+export const portraitCover = 'http://vignette2.wikia.nocookie.net/mario/images/6/60/SMBBoxart.png/revision/latest?cb=20120609143443'
+export const publisher = 'Nintendo'
+export const title = 'Super Mario Bros'
