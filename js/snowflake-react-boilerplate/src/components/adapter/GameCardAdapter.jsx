dd11b9d6655985e6ac595935044048fc7b877cee
RonnChyran <ronny6993@gmail.com>
2017-06-03 00:43:49 -0400

    abstract sidebar to sidebar visible view

--- js/snowflake-react-boilerplate/src/components/adapter/GameCardAdapter.jsx
+++ js/snowflake-react-boilerplate/src/components/adapter/GameCardAdapter.jsx
@@ -4,8 +4,6 @@ import GameCard from 'components/presentation/game/GameCard'
 import aspectRatio from 'snowflake/platformsupport/aspectratio'
 
 const GameCardAdapter = ({ game }) => {
-  console.log (game)
-  console.log(aspectRatio["NINTENDO_SNE"])
   return (
     <GameCard title={game.Title} {...aspectRatio[game.PlatformId]} publisher={game.Metadata.game_publisher}/>
   )
