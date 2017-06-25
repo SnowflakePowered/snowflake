4defe273efcdc53150332f0820012924c2ed8fc9
RonnChyran <ronny6993@gmail.com>
2017-06-24 20:34:13 -0400

    Tooling: Add a module packager to publish plugins as a module.

---
+++ src/Snowflake.Tooling.AssemblyModulePacker/DotNetBuilder.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using CliWrap;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Snowflake.Tooling.AssemblyModulePacker
+{
+    internal class DotNetBuilder
+    {
+        private readonly Cli dotnetProcess;
+        private readonly FileInfo projectFile;
+        private readonly ModuleDefinition moduleDefinition;
+        private readonly IEnumerable<string> args;
+        public DotNetBuilder(ModuleDefinition moduleDefinition, FileInfo projectFile, IEnumerable<string> args)
+        {
+            this.dotnetProcess = new Cli("dotnet", projectFile.DirectoryName);
+            this.projectFile = projectFile;
+            this.moduleDefinition = moduleDefinition;
+            this.args = args;
+        }
+
+        public DirectoryInfo Clean()
+        {
+            var outputDirectory = projectFile.Directory
+                           .CreateSubdirectory("bin")
+                           .CreateSubdirectory("module");
+            try
+            {
+                foreach(var directory in outputDirectory.EnumerateDirectories())
+                {
+                    directory.Delete(true);
+                }
+            }
+            catch (IOException ex)
+            {
+                throw new IOException("Unable to clean output directory, is it in use?", ex);
+            }
+            var moduleRoot = outputDirectory
+                .CreateSubdirectory(Path.GetFileNameWithoutExtension(moduleDefinition.Entry));
+            return moduleRoot.CreateSubdirectory("contents");
+        }
+
+        public async Task<DirectoryInfo> Build()
+        {
+            var moduleContents = this.Clean();
+            var dotnetArgs = this.args.Prepend(moduleContents.FullName)
+                .Prepend("-o")
+                .Prepend(projectFile.FullName)
+                .Prepend("publish");
+            var output = await this.dotnetProcess.ExecuteAsync(String.Join(" ", dotnetArgs));
+            Console.WriteLine(output.StandardOutput);
+            return this.CopyModuleFile(moduleContents).Directory;
+        }
+
+        public FileInfo CopyModuleFile(DirectoryInfo contentsDirectory)
+        {
+            var moduleFile = contentsDirectory.EnumerateFiles().FirstOrDefault(f => f.Name == "module.json");
+            return moduleFile.CopyTo(Path.Combine(moduleFile.Directory.Parent.FullName, moduleFile.Name)); 
+        }
+
+    }
+}
---
+++ src/Snowflake.Tooling.AssemblyModulePacker/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Snowflake.Tooling.AssemblyModulePacker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(() => Program.MainAsync(args)).Wait();
+        }
+
+        public static async Task MainAsync(string[] args)
+        {
+
+            Console.WriteLine($"Snowball Assembly Packer version {Assembly.GetEntryAssembly().GetName().Version}");
+            var stateMachine = new StateMachine();
+            var cwd = DirectoryProvider.WorkingDirectory;
+            if (!DirectoryProvider.IsProjectDirectory(cwd))
+            {
+                await stateMachine.ExitWithState("No valid project file found.", 1);
+                return;
+            }
+
+            if (!DirectoryProvider.IsModuleDirectory(cwd))
+            {
+                await stateMachine.ExitWithState("No valid module.json found. Check for JSON errors or missing file.", 1);
+                return;
+            }
+            //todo: migrate to use states!
+            (var projectFile, var moduleFile) = DirectoryProvider.GetProjectFiles(cwd);
+            ModuleDefinition module;
+
+            try
+            {
+                module = JsonConvert.DeserializeObject<ModuleDefinition>(File.ReadAllText(moduleFile?.FullName));
+            }
+            catch
+            {
+                await stateMachine.ExitWithState("No valid module.json found. Check for JSON errors or missing file.", 1);
+                return;
+            }
+
+            if (!module.Entry.EndsWith(".dll") || module.Loader != "assembly")
+            {
+                await stateMachine.ExitWithState("Module is not a proper assembly module, can not pack non-assembly modules!", 1);
+                return;
+            }
+
+            string assemblyName = (from groups in XDocument.Parse(File.ReadAllText(projectFile.FullName)).Root.Descendants()
+                                   from element in groups.Descendants()
+                                   where element.Name.LocalName == "AssemblyName"
+                                   select element.Value).FirstOrDefault();
+
+            if (assemblyName == null)
+            {
+                await stateMachine.ExitWithState("Project file is missing AssemblyName.", 1);
+                return;
+            }
+
+            if(assemblyName != Path.GetFileNameWithoutExtension(module.Entry))
+            {
+                await stateMachine.ExitWithState($"Entry point {module.Entry} is not consistent with output assembly name {assemblyName}!", 1);
+                return;
+            }
+
+            await stateMachine.Transition($"Found module {module.Entry}");
+            var builder = new DotNetBuilder(module, projectFile, args);
+            DirectoryInfo buildResult = await stateMachine.Transition<DirectoryInfo>(async () =>
+               {
+                   try
+                   {
+                       return buildResult = await builder.Build();
+                   }
+                   catch (IOException ex)
+                   {
+                       await stateMachine.ExitWithState(ex.Message, 1);
+                       return null;
+                   }
+               }, "Cleaning and building module...");
+            await stateMachine.ExitWithState($"Finished building module at {Environment.NewLine} {buildResult.FullName}");
+
+        }
+    }
+}
