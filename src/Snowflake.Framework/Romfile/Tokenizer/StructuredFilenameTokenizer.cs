df5f70248cefe3691c12ad7c3128cd17f5210d04
Ronny Chan <ronny6993@gmail.com>
2017-06-27 12:08:16 -0400

    StructuredFilename: Use a tokenizing approach to improve accuracy. (#250)
    * StructuredFilename: Use a tokenizing approach to improve accuracy and information
    
    * Tokenizer: Increase test coverage

---
+++ src/Snowflake.Framework/Romfile/Tokenizer/StructuredFilenameTokenizer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Snowflake.Romfile.Tokenizer
+{
+    internal class StructuredFilenameTokenizer
+    {
+        private readonly string filename;
+        public StructuredFilenameTokenizer(string filename)
+        {
+            this.filename = filename;
+        }
+
+        public string GetTitle()
+        {
+            return Regex.Match(this.filename, @"(\([^]]*\))*(\[[^]]*\])*([\w\+\~\@\!\#\$\%\^\&\*\;\,\'\""\?\-\.\-\s]+)").Groups[3].Value.Trim();
+        }
+        
+        public IEnumerable<(string tokenValue, int tokenPosition)> GetParensTokens()
+        {
+            var tagData = Regex.Matches(this.filename,@"(\()([^)]+)(\))").ToList();
+            for (int i = 0; i < tagData.Count; i++)
+            {
+                yield return (tagData[i].Groups[2].Value, i);
+            }
+        }
+
+        public IEnumerable<(string tokenValue, int tokenPosition)> GetBracketTokens()
+        {
+            var tagData = Regex.Matches(this.filename, @"(\[)([^)]+)(\])").ToList();
+            for (int i = 0; i < tagData.Count; i++)
+            {
+                yield return (tagData[i].Groups[2].Value, i);
+            }
+        }
+    }
+}
