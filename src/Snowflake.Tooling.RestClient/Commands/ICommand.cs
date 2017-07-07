ca351d96e81587b1791b655dae77dec7984cad5c
Ronny Chan <ronny6993@gmail.com>
2017-07-07 11:12:54 -0400

    RestClient: Work on a CLI rest client

---
+++ src/Snowflake.Tooling.RestClient/Commands/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snowflake.Tooling.RestClient.Commands
+{
+    public interface ICommand<TCommandOptions>
+    {
+        Task<int> Execute(TCommandOptions options);
+    }
+}
