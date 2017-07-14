5dee7a965ef3d48fd8755f33b25256f31d84da4b
Ronny Chan <ronny6993@gmail.com>
2017-07-14 15:48:11 -0400

    Remoting: Start RequestError API

---
+++ src/Snowflake.Framework.Primitives/Remoting/Requests/IRequestError.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snowflake.Remoting.Requests
+{
+    public interface IRequestError
+    {
+        string Message { get; }
+        string Type { get; }
+        int Code { get; }
+    }
+}
