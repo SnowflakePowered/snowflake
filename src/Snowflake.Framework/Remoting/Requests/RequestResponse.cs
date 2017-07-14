0757afda29f20b579ac9761eec18133945b96bd7
Ronny Chan <ronny6993@gmail.com>
2017-07-14 15:48:11 -0400

    Remoting: Start RequestError API

--- src/Snowflake.Framework/Remoting/Requests/RequestResponse.cs
+++ src/Snowflake.Framework/Remoting/Requests/RequestResponse.cs
@@ -6,6 +6,7 @@ namespace Snowflake.Remoting.Requests
     public class RequestResponse : IRequestResponse
     {
         public static RequestResponse NoEndpointFoundResponse { get; }
+
         static RequestResponse() {
             RequestResponse.NoEndpointFoundResponse = new RequestResponse(null);
         }
