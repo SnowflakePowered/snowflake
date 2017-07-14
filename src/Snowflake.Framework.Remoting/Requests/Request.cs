88f51bd7215d9806a45e8f3df2b56a73c1d354ce
Ronny Chan <ronny6993@gmail.com>
2017-07-13 22:53:56 -0400

    Remoting: Begin work on a declarative, attribute based, safe remoting framework.

---
+++ src/Snowflake.Framework.Remoting/Requests/Request.cs
@@ -0,0 +1,22 @@
+﻿using Snowflake.Framework.Remoting.Marshalling;
+using Snowflake.Framework.Remoting.Resources;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snowflake.Framework.Remoting.Requests
+{
+    public class Request
+    {
+        public RequestPath RequestPath { get; }
+        public IEnumerable<EndpointArgument> EndpointArguments { get; }
+        public EndpointVerb Verb { get; }
+        public Request(RequestPath path, EndpointVerb verb,
+            IEnumerable<EndpointArgument> arguments)
+        {
+            this.Verb = verb;
+            this.RequestPath = path;
+            this.EndpointArguments = arguments;
+        }
+    }
+}
---
+++ src/Snowflake.Framework.Remoting/Resources/Attributes/EndpointAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snowflake.Framework.Remoting.Resources.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public sealed class EndpointAttribute : Attribute
+    {
+        public EndpointVerb Verb { get; }
+        public EndpointAttribute(EndpointVerb endpointVerb)
+        {
+            this.Verb = endpointVerb;
+        }
+    }
+}
