83c3e9fc3262c47c96604c117076f7970c0e03f2
Ronny Chan <ronny6993@gmail.com>
2017-07-24 12:09:54 -0400

    ConfigurationKey: Document ConfigurationKey

--- js/hydrogen-paper/src/support/ConfigurationKey.ts
+++ js/hydrogen-paper/src/support/ConfigurationKey.ts
@@ -16,12 +16,27 @@ class ConfigurationKeyProxyHandler implements ProxyHandler<ConfigurationKey> {
   }
 }
 
-export interface ConfigurationKey {
+/**
+ * Used to access a ConfigurationCollection object.
+ */
+export type ConfigurationKey = {
   gameGuid: string,
   emulatorName: string,
   profileName: string
 }
-
+/**
+ * Used to create a ConfigurationKey.
+ *
+ * Do not manually create one from a plain JavaScript object, it will not have proper value semantics.
+ *
+ * This function creates a Proxy that is backed by Immutable for proper value semantics when using
+ * as a Map key.
+ *
+ * @param gameGuid The GUID of the game.
+ * @param emulatorName The name of the emulator this collection is for.
+ * @param profileName The name of the collection profile.
+ *
+ */
 export const ConfigurationKey = (gameGuid: string, emulatorName: string, profileName: string): ConfigurationKey => {
   const handler = new ConfigurationKeyProxyHandler(gameGuid, emulatorName, profileName)
   return new Proxy<ConfigurationKey>({gameGuid: gameGuid, emulatorName: emulatorName, profileName: profileName}, handler)
