d252fb5b6afc6b71c83718df1a291cba7c26e4ba
RonnChyran <ronny6993@gmail.com>
2017-11-19 02:10:16 -0500

    AssemblyModuleInstaller: Super-rudimentary tool to copy built module folders to the application data directory. Only works on Windows so far.

---
+++ src/Snowflake.Tooling.AssemblyModuleInstaller/Copy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System;
+using System.IO;
+
+namespace Snowflake.Tooling.AssemblyModulePacker
+{
+
+    static class CopyDir
+    {
+
+        internal static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        {
+            Directory.CreateDirectory(target.FullName);
+
+            // Copy each file into the new directory.
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+            }
+
+            // Copy each subdirectory using recursion.
+            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+            {
+                DirectoryInfo nextTargetSubDir =
+                    target.CreateSubdirectory(diSourceSubDir.Name);
+                CopyAll(diSourceSubDir, nextTargetSubDir);
+            }
+        }
+        // Output will vary based on the contents of the source directory.
+    }
+}
---
+++ src/Snowflake.Tooling.AssemblyModuleInstaller/Program.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using Snowflake.Tooling.AssemblyModulePacker;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Snowflake.Tooling.AssemblyModuleInstaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(() => Program.MainAsync(args)).Wait();
+        }
+        public static void ExitWithState(string stateMessage, int exitCode = 0)
+        {
+            Console.WriteLine(stateMessage);
+            Environment.Exit(exitCode);
+        }
+        public static async Task MainAsync(string[] args)
+        {
+            // todo: this code is real shit.
+            Console.WriteLine($"Snowball Assembly Installer version {Assembly.GetEntryAssembly().GetName().Version}");
+            var cwd = DirectoryProvider.WorkingDirectory;
+            if (!DirectoryProvider.IsProjectDirectory(cwd))
+            {
+                ExitWithState("Error! No valid project file found.", 1);
+                return;
+            }
+
+            if (!DirectoryProvider.IsModuleDirectory(cwd))
+            {
+                ExitWithState("Error! No valid module.json found. Check for JSON errors or missing file.", 1);
+                return;
+            }
+            (var projectFile, var moduleFile) = DirectoryProvider.GetProjectFiles(cwd);
+            ModuleDefinition module;
+
+            try
+            {
+                module = JsonConvert.DeserializeObject<ModuleDefinition>(File.ReadAllText(moduleFile?.FullName));
+            }
+            catch
+            {
+                ExitWithState("Error! No valid module.json found. Check for JSON errors or missing file.", 1);
+                return;
+            }
+
+            if (!module.Entry.EndsWith(".dll") || module.Loader != "assembly")
+            {
+                ExitWithState("Error! Module is not a proper assembly module, can not pack non-assembly modules!", 1);
+                return;
+            }
+            Console.WriteLine($"Found module {module.Entry}");
+
+            var buildDir = DirectoryProvider.GetBuildDirectory(Path.GetFileNameWithoutExtension(module.Entry));
+            if (!buildDir.Exists)
+            {
+                ExitWithState("Error! Module was not built! Build the module before installing!", 1);
+                return;
+            }
+
+            var installedDir = DirectoryProvider.GetDefaultModuleInstallDirectory.CreateSubdirectory(Path.GetFileNameWithoutExtension(module.Entry));
+            Console.WriteLine("Removing old installation from modules...");
+            try
+            {
+                installedDir.Delete(true);
+            }
+            catch (IOException ex)
+            {
+                ExitWithState("Unable to clean output directory, is it in use?");
+            }
+            Console.WriteLine("Installing new installation...");
+            try
+            {
+                CopyDir.CopyAll(buildDir, installedDir);
+
+            }catch(IOException ex)
+            {
+                ExitWithState("Unable to install, is it in use?");
+            }
+        }
+    }
+}
