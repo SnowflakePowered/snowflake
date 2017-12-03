3a7e11d3f0be646d0b01da8e04d478778f8df577
Ronny Chan <ronny6993@gmail.com>
2017-12-03 03:06:00 -0500

    GraphQL: Add input device

---
+++ src/Snowflake.Support.Remoting.GraphQl/Types/InputDevice/InputDeviceGraphType.cs
@@ -0,0 +1,22 @@
+﻿using GraphQL.Types;
+using Snowflake.Input.Device;
+using Snowflake.Support.Remoting.GraphQl.Types.ControllerLayout;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snowflake.Support.Remoting.GraphQl.Types.InputDevice
+{
+    public class InputDeviceGraphType : ObjectGraphType<IInputDevice>
+    {
+        public InputDeviceGraphType()
+        {
+            Field<InputApiEnum>("inputApi",
+                description: "Input API",
+                resolve: context => context.Source.DeviceApi);
+            Field<ControllerLayoutType>("deviceLayout",
+               description: "Controller Layout",
+               resolve: context => context.Source.DeviceLayout);
+        }
+    }
+}
