af0ba928d5956edd94f4524b386af7c985bd90df
Ronny Chan <ronny6993@gmail.com>
2017-12-04 18:06:19 -0500

    cleanup: remove JS files

--- js/snowflake-remoting/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Games } from "./remoting/Games";
-import { Stone } from "./remoting/Stone";
-export default class Snowflake {
-    private _games;
-    private _stone;
-    constructor(rootUrl?: string);
-    readonly games: Games;
-    readonly stone: Stone;
-}
