0f652c2fa46889e0c60226cb799928d32533da9e
RonnChyran <ronny6993@gmail.com>
2017-12-13 19:14:09 -0500

    begin some new APIs...

---
+++ src/Snowflake.Framework.Primitives/Emulator/IEmulatedGameProvision.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Snowflake.Emulator
+{
+    public interface IEmulatedGameProvision
+    {
+        Guid ProvisionGuid { get; }
+        DirectoryInfo ProvisionDirectory { get; }
+    }
+}
---
+++ src/Snowflake.Framework.Primitives/Emulator/Saving/ISaveFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Snowflake.Records.File;
+using Snowflake.Records.Game;
+
+namespace Snowflake.Emulator.Saving
+{
+    public interface ISaveFile
+    {
+        IGameRecord GameRecord { get; }
+        IFileRecord SaveFileRecord { get; }
+
+    }
+}
