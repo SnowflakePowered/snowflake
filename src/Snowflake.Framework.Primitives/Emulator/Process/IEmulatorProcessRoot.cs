f6bd432dce4d573fafc9f0aa0950811d60c5decd
RonnChyran <ronny6993@gmail.com>
2017-12-14 21:07:33 -0500

    interfaces for savefile api

---
+++ src/Snowflake.Framework.Primitives/Emulator/Process/IEmulatorProcessRoot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Snowflake.Emulator.Process
+{
+    public interface IEmulatorProcessRoot
+    {
+        DirectoryInfo ProcessRoot { get; }
+        DirectoryInfo SaveDirectory { get; }
+        DirectoryInfo ConfigurationDirectory { get; }
+        DirectoryInfo SystemDirectory { get; }
+    }
+}
