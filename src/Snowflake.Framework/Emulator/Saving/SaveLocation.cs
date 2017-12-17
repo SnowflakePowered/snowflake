cecf61cdd2c76e688a3466b46b399710a732d119
Ronny Chan <ronny6993@gmail.com>
2017-12-16 22:24:04 -0500

    Saving: Implement SaveLocation

---
+++ src/Snowflake.Framework/Emulator/Saving/SaveLocation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Snowflake.Records.Game;
+
+namespace Snowflake.Emulator.Saving
+{
+    public class SaveLocation : ISaveLocation
+    {
+        public SaveLocation(IGameRecord gameRecord, string saveFormat, DirectoryInfo providerRoot)
+        {
+            this.GameRecord = gameRecord;
+            this.SaveFormat = saveFormat;
+            this.LocationGuid = Guid.NewGuid();
+            this.LocationRoot = providerRoot.CreateSubdirectory(this.LocationGuid.ToString());
+        }
+
+        internal SaveLocation(IGameRecord gameRecord, string saveFormat, DirectoryInfo locationRoot, Guid locationGuid)
+        {
+            this.GameRecord = gameRecord;
+            this.SaveFormat = saveFormat;
+            this.LocationRoot = locationRoot;
+            this.LocationGuid = locationGuid;
+        }
+
+        public Guid LocationGuid { get; }
+
+        public DirectoryInfo LocationRoot { get; }
+
+        public IGameRecord GameRecord { get; }
+
+        public string SaveFormat { get; }
+
+        public DateTimeOffset LastModified { get; }
+
+        public IEnumerable<FileInfo> PersistFrom(DirectoryInfo emulatorSaveDirectory)
+        {
+            var list = new List<FileInfo>();
+            foreach (var fileInfo in emulatorSaveDirectory.GetFiles("*.*", SearchOption.AllDirectories))
+            {
+                string fileName = Path.GetFileName(fileInfo.FullName);
+                list.Add(fileInfo.CopyTo(Path.Combine(this.LocationRoot.FullName, fileName), true));
+            }
+
+            return list;
+        }
+
+        public IEnumerable<FileInfo> RetrieveTo(DirectoryInfo emulatorSaveDirectory)
+        {
+
+            var list = new List<FileInfo>();
+            foreach (var fileInfo in this.LocationRoot.GetFiles("*.*", SearchOption.AllDirectories))
+            {
+                string fileName = Path.GetFileName(fileInfo.FullName);
+                list.Add(fileInfo.CopyTo(Path.Combine(emulatorSaveDirectory.FullName, fileName), true));
+            }
+
+            return list;
+        }
+    }
+}
