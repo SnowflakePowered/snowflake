983f4a94043b24b1d868072b344d90202ebfca0e
Ronny Chan <ronny6993@gmail.com>
2017-12-17 19:24:27 -0500

    SystemFiles: Implement and test SystemFileProvider

--- src/Snowflake.Framework.Primitives/Emulator/SystemFIles/ISystemFileProvider.cs
+++ src/Snowflake.Framework.Primitives/Emulator/SystemFIles/ISystemFileProvider.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 using Snowflake.Platform;
 
 namespace Snowflake.Emulator.SystemFIles
@@ -25,7 +26,7 @@ namespace Snowflake.Emulator.SystemFIles
         /// <throws>FileNotFoundException if the BIOS file is not available.</throws>
         /// <param name="biosFile">The BIOS file specification.</param>
         /// <returns>The path to the file.</returns>
-        DirectoryInfo GetSystemFilePath(IBiosFile biosFile);
+        FileInfo GetSystemFilePath(IBiosFile biosFile);
 
         /// <summary>
         /// Adds the specified BIOS file to the registry.
@@ -42,5 +43,21 @@ namespace Snowflake.Emulator.SystemFIles
         /// <param name="biosFile">The BIOS file specification.</param>
         /// <param name="systemFileStream">The file as a stream.</param>
         void AddSystemFile(IBiosFile biosFile, Stream systemFileStream);
+
+        /// <summary>
+        /// Adds the specified BIOS file to the registry.
+        /// </summary>
+        /// <exception cref="FileNotFoundException">If the system file is not found.</exception>
+        /// <param name="biosFile">The BIOS file specification.</param>
+        /// <param name="systemFileStream">The file as a stream.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        Task AddSystemFileAsync(IBiosFile biosFile, Stream systemFileStream);
+
+        /// <summary>
+        /// Returns whether or not a given BIOS file is contained in the registery
+        /// </summary>
+        /// <param name="biosFile">The requested BIOS file.</param>
+        /// <returns>Whether or not the registry contiains the given BIOS file.</returns>
+        bool ContainsSystemFile(IBiosFile biosFile);
     }
 }
---
+++ src/Snowflake.Framework/Emulator/SystemFiles/SystemFileProvider.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Snowflake.Emulator.SystemFIles;
+using Snowflake.Platform;
+using Snowflake.Services;
+
+namespace Snowflake.Emulator.SystemFiles
+{
+    public class SystemFileProvider : ISystemFileProvider
+    {
+        private DirectoryInfo SystemFileRoot { get; }
+
+        public SystemFileProvider(IContentDirectoryProvider cdp)
+        {
+            this.SystemFileRoot = cdp.ApplicationData.CreateSubdirectory("bios");
+        }
+
+        public void AddSystemFile(IBiosFile biosFile, FileInfo systemFilePath)
+        {
+            systemFilePath.CopyTo(this.GetSystemFileInfo(biosFile).FullName, true);
+        }
+
+        public void AddSystemFile(IBiosFile biosFile, Stream systemFileStream)
+        {
+            using (FileStream stream = File.Create(this.GetSystemFileInfo(biosFile).FullName))
+            {
+                stream.Position = 0;
+                systemFileStream.Position = 0;
+                systemFileStream.CopyTo(stream);
+                stream.Close();
+            }
+        }
+
+        public Stream GetSystemFile(IBiosFile biosFile)
+        {
+            return File.OpenRead(this.GetSystemFileInfo(biosFile).FullName);
+        }
+
+        private FileInfo GetSystemFileInfo(IBiosFile biosFile)
+        {
+            var fileInfo = new FileInfo(Path.Combine(this.SystemFileRoot.FullName,
+                $"{biosFile.Md5Hash}.{biosFile.FileName}"));
+            return fileInfo;
+        }
+
+        public FileInfo GetSystemFilePath(IBiosFile biosFile)
+        {
+            var fileInfo = this.GetSystemFileInfo(biosFile);
+            if (!fileInfo.Exists)
+            {
+                throw new FileNotFoundException($"BIOS File with hash {biosFile.Md5Hash} does not exist.");
+            }
+
+            return fileInfo;
+        }
+
+        public async Task AddSystemFileAsync(IBiosFile biosFile, Stream systemFileStream)
+        {
+            using (FileStream stream = File.Create(this.GetSystemFileInfo(biosFile).FullName))
+            {
+                stream.Position = 0;
+                systemFileStream.Position = 0;
+                await systemFileStream.CopyToAsync(stream);
+                stream.Close();
+            }
+        }
+
+        public bool ContainsSystemFile(IBiosFile biosFile)
+        {
+            return this.GetSystemFileInfo(biosFile).Exists;
+        }
+    }
+}
