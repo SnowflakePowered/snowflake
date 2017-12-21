e77560eb9d39706bac1a841b34698faeea938e84
RonnChyran <ronny6993@gmail.com>
2017-12-21 00:39:17 -0500

    comment more

--- src/Snowflake.Framework.Primitives/Execution/Extensibility/IEmulator.cs
+++ src/Snowflake.Framework.Primitives/Execution/Extensibility/IEmulator.cs
@@ -11,12 +11,30 @@ using Snowflake.Records.Game;
 
 namespace Snowflake.Execution.Extensibility
 {
+    /// <summary>
+    /// An <see cref="IEmulator"/> handles execution of a game by creating tasks with
+    /// contextual information to execute with the provided task runner.
+    /// </summary>
     public interface IEmulator : IPlugin
     {
+        /// <summary>
+        /// Gets the task runner that this emulator plugin delegates.
+        /// </summary>
         IEmulatorTaskRunner Runner { get; }
 
+        /// <summary>
+        /// Gets the emulator specific properties for this emulator.
+        /// </summary>
         IEmulatorProperties Properties { get; }
 
+        /// <summary>
+        /// Creates a task to exeut
+        /// </summary>
+        /// <param name="executingGame"></param>
+        /// <param name="saveLocation"></param>
+        /// <param name="controllerConfiguration"></param>
+        /// <param name="profileContext"></param>
+        /// <returns></returns>
         IEmulatorTask CreateTask(IGameRecord executingGame,
             ISaveLocation saveLocation,
             IList<IEmulatedController> controllerConfiguration,
