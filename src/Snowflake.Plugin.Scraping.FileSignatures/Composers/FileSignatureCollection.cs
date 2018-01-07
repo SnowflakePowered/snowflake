8824621f4a7d67ddba5da00b9876e0d64e79d6de
Ronny Chan <ronny6993@gmail.com>
2018-01-07 15:57:31 -0500

    Scraper: Implement RomFileInfoScraper

---
+++ src/Snowflake.Plugin.Scraping.FileSignatures/Composers/FileSignatureCollection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snowflake.Composers
+{
+    public class FileSignatureCollection
+    {
+    }
+}
--- src/Snowflake.Plugin.Scraping.FileSignatures/Scrapers/RomFileInfoScraper.cs
+++ src/Snowflake.Plugin.Scraping.FileSignatures/Scrapers/RomFileInfoScraper.cs
@@ -1,18 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Snowflake.Extensibility;
+using Snowflake.Romfile;
 using Snowflake.Scraping;
 using Snowflake.Scraping.Extensibility;
+using static Snowflake.Scraping.Extensibility.SeedBuilder;
 
 namespace Snowflake.Scrapers
 {
+    [Directive(AttachTarget.Root, Directive.Requires, "file")]
+    [Directive(AttachTarget.Target, Directive.Requires, "mimetype")]
+    [Plugin("RomFileInfoScraper")]
     public class RomFileInfoScraper : Scraper
     {
-        public override Task<IEnumerable<SeedTreeAwaitable>> ScrapeAsync(ISeed parent, ILookup<string, SeedContent> rootSeeds, ILookup<string, SeedContent> childSeeds, ILookup<string, SeedContent> siblingSeeds)
+        IDictionary<string, IFileSignature> FileSignatures { get; }
+        public RomFileInfoScraper()
+          : base(typeof(RomFileInfoScraper), AttachTarget.Target, "file")
+            {
+            }
+
+        public override async Task<IEnumerable<SeedTreeAwaitable>> ScrapeAsync(ISeed parent, ILookup<string, SeedContent> rootSeeds, ILookup<string, SeedContent> childSeeds, ILookup<string, SeedContent> siblingSeeds)
         {
-            throw new NotImplementedException();
+            string mimeType = childSeeds["mimetype"].First().Value;
+            if (!File.Exists(parent.Content.Value) || !this.FileSignatures.ContainsKey(mimeType))
+            {
+                return _();
+            }
+
+            var signature = this.FileSignatures[mimeType];
+            using (FileStream romStream = File.OpenRead(parent.Content.Value))
+            {
+                return _(
+                  ("rom_serial", signature.GetSerial(romStream)),
+                  ("rom_internal", signature.GetInternalName(romStream))
+                );
+            }
         }
     }
 }
