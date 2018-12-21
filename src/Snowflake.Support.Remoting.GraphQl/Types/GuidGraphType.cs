f51b50117dc22b7bb42e5f9d00c2eb8d8e3cddc8
Ronny Chan <ronny6993@gmail.com>
2018-12-21 03:09:09 -0500

    GraphQL: Fix GUIDs not being parsed from Strings

---
+++ src/Snowflake.Support.Remoting.GraphQl/Types/GuidGraphType.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using GraphQL.Language.AST;
+using GraphQL.Types;
+
+namespace GraphQL.Types
+{
+    public class GuidGraphType : ScalarGraphType
+    {
+        public GuidGraphType()
+        {
+            Name = "Guid";
+            Description = "Globally Unique Identifier.";
+        }
+
+        public override object ParseValue(object value) =>
+          ValueConverter.ConvertTo(value, typeof(Guid));
+
+        public override object Serialize(object value) => ParseValue(value);
+
+        /// <inheritdoc/>
+        public override object ParseLiteral(IValue value)
+        {
+            if (value is GuidValue guidValue)
+            {
+                return guidValue.Value;
+            }
+
+            if (value is StringValue str)
+            {
+                return ParseValue(str.Value);
+            }
+
+            return null;
+        }
+    }
+}
