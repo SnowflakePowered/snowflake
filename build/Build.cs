1cd2fa7d3f14dabc082d1f20cb2b8f2522532260
Ronny Chan <ronny6993@gmail.com>
2019-03-09 01:27:05 -0500

    Switch to NUKE build

---
+++ build/Build.cs
@@ -0,0 +1,119 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using Nuke.Common;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.MSBuild;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using SDKOptions = Microsoft.Build.Definition.ProjectOptions;
+using SDKProject = Microsoft.Build.Evaluation.Project;
+
+[CheckBuildProjectConfigurations]
+[UnsetVisualStudioEnvironmentVariables]
+class Build : NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main () => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution] readonly Solution Solution;
+    [GitRepository] readonly GitRepository GitRepository;
+
+    AbsolutePath SourceDirectory => RootDirectory / "src";
+    AbsolutePath OutputDirectory => RootDirectory / "out";
+    AbsolutePath Tooling => SourceDirectory / "Snowflake.Tooling.Taskrunner";
+    AbsolutePath Tests => SourceDirectory / "Snowflake.Framework.Tests";
+
+    AbsolutePath BuildDirectory => RootDirectory / "build";
+
+    AbsolutePath ToolingDirectory =>  BuildDirectory / "snowflake-cli";
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            EnsureCleanDirectory(OutputDirectory);
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore());
+        });
+    Target BuildTooling => _ => _
+       .DependsOn(Restore)
+       .Executes(() =>
+       {
+           DotNetBuild(s => s
+               .SetProjectFile(Tooling)
+               .SetOutputDirectory(ToolingDirectory));
+       });
+
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetTest(s => s
+                .SetProjectFile(Tests));
+        });
+
+    Target PackModules => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            EnsureExistingDirectory(OutputDirectory);
+
+            foreach (Project p in Solution.GetProjects("Snowflake.*"))
+            {
+                if (!GetSdkVersion(p).StartsWith("Snowflake.Framework.Sdk/")) continue;
+                if (!File.Exists((p.Directory / "module.json"))) continue;
+
+                DotNet("snowflake build", p.Directory);
+                DotNet($"snowflake pack \"./bin/module/{p.Name}\" -o \"{OutputDirectory}\"", p.Directory);
+            }
+        });
+
+    static string GetSdkVersion(Project p)
+    {
+        using (XmlReader reader = XmlReader.Create(p.Path))
+        {
+            if (reader.MoveToContent() == XmlNodeType.Element && reader.HasAttributes)
+            {
+                if (reader.MoveToAttribute("Sdk"))
+                {
+                    return reader.ReadContentAsString();
+                }
+            }
+        }
+        return String.Empty;
+    }
+}
