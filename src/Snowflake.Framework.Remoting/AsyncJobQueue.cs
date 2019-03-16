9f3d91f7f054e4aaaf4bb673e5088697bc6a44a9
Ronny Chan <ronny6993@gmail.com>
2019-03-15 22:54:38 -0400

    GameInstallers: Start work on the GraphQL Query

---
+++ src/Snowflake.Framework.Remoting/AsyncJobQueue.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Snowflake.Framework.Scheduling;
+
+namespace Snowflake.Framework.Remoting
+{
+    public sealed class AsyncJobQueue<T> : IAsyncJobQueue<T>
+    {
+        public AsyncJobQueue()
+        {
+            this.Enumerators = new ConcurrentDictionary<Guid, IAsyncEnumerator<T>>();
+        }
+
+        public IDictionary<Guid, IAsyncEnumerator<T>> Enumerators { get; }
+
+        public IAsyncEnumerator<T> GetEnumerator(Guid jobId)
+        {
+            var exists = this.Enumerators.TryGetValue(jobId, out IAsyncEnumerator<T> value);
+            if (!exists) return Empty().GetAsyncEnumerator();
+            return value;
+        }
+
+        private static async IAsyncEnumerable<T> Empty()
+        {
+            yield break;
+        }
+
+        public async ValueTask<(T, bool)> GetNext(Guid jobId)
+        {
+            var enumerator = this.GetEnumerator(jobId);
+            
+            var result =  (value: enumerator.Current, hasNext: await enumerator.MoveNextAsync());
+            if (!result.hasNext)
+            {
+                await enumerator.DisposeAsync();
+                this.Enumerators.Remove(jobId);
+            }
+            return result;
+        }
+
+        public Guid QueueJob(IAsyncEnumerable<T> asyncEnumerable)
+        {
+            var guid = Guid.NewGuid();
+            this.Enumerators.Add(guid, asyncEnumerable.GetAsyncEnumerator());
+            return guid;
+        }
+
+        public Guid QueueJob(IAsyncEnumerable<T> asyncEnumerable, Guid guid)
+        {
+            this.Enumerators.Add(guid, asyncEnumerable.GetAsyncEnumerator());
+            return guid;
+        }
+
+    }
+}
