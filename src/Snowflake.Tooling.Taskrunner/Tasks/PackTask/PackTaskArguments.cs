d0fab5afdf4c6fff018b106ae4b50b7c3637dcd8
Ronny Chan <ronny6993@gmail.com>
2019-04-30 01:41:39 -0400

    Taskrunner: Fix RSA provider being disposed too early

--- src/Snowflake.Tooling.Taskrunner/Tasks/PackTask/PackTaskArguments.cs
+++ src/Snowflake.Tooling.Taskrunner/Tasks/PackTask/PackTaskArguments.cs
@@ -7,7 +7,8 @@ namespace Snowflake.Tooling.Taskrunner.Tasks.PackTask
 {
     public class PackTaskArguments
     {
-        [PositionalArgument(0, "The module to pack.")]
+        [PositionalArgument(0, "The module to pack. If no module directory is detected, and the current directory is a " +
+                               "project directory, will default to the default build location of the assembly module.")]
         public string ModuleDirectory { get; set; }
 
         [NamedArgument("o", "output", "The name of the output file.")]
