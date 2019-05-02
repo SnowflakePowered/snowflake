828faed193e0856934c3478b9238dcdaa6c4dc1d
Ronny Chan <ronny6993@gmail.com>
2019-05-01 20:33:11 -0400

    CopyFileTaskTests: Write some random data to simulate real conditions to files

--- src/Snowflake.Framework.Tests/Installation/CopyFileTaskTests.cs
+++ src/Snowflake.Framework.Tests/Installation/CopyFileTaskTests.cs
@@ -16,6 +16,13 @@ namespace Snowflake.Installation.Tests
 {
     public class CopyFileTaskTests
     {
+        public static byte[] Data { get; }
+        static CopyFileTaskTests() {
+            Data = new byte[1 * 1024 * 1024];
+            Random rng = new Random();
+            rng.NextBytes(Data);
+        }
+
         [Fact]
         public async Task CopyFile_Test()
         {
@@ -24,11 +31,19 @@ namespace Snowflake.Installation.Tests
             var pfs = fs.GetOrCreateSubFileSystem(fs.ConvertPathFromInternal(temp));
             var dir = new FS.Directory("test", pfs, pfs.GetDirectoryEntry("/"));
             var temp2 = Path.GetTempFileName();
-
+            await System.IO.File.WriteAllBytesAsync(temp2, Data);
             // Execute the results.
             await foreach (var res in EmitCopyResult(temp2, dir));
 
             Assert.True(dir.ContainsFile(Path.GetFileName(temp2)));
+
+            using (var stream = dir.OpenFile(Path.GetFileName(temp2)).OpenStream())
+            {
+                Assert.Equal(Data.Length, stream.Length);
+            }
+
+            dir.OpenFile(Path.GetFileName(temp2)).Delete();
+            Assert.False(dir.ContainsFile(Path.GetFileName(temp2)));
         }
 
         [Fact]
@@ -41,20 +56,34 @@ namespace Snowflake.Installation.Tests
 
             var dirToCopy = new DirectoryInfo(temp).CreateSubdirectory(Path.GetRandomFileName());
             var subDirToCopy = dirToCopy.CreateSubdirectory(Path.GetRandomFileName());
-            var file = System.IO.File.Create(Path.Combine(subDirToCopy.FullName, Path.GetRandomFileName()));
-            var fileName = Path.GetFileName(file.Name);
-            file.Close();
+            string fileName;
+            using (var file = System.IO.File.Create(Path.Combine(subDirToCopy.FullName, Path.GetRandomFileName())))
+            {
+                fileName = Path.GetFileName(file.Name);
+                await file.WriteAsync(Data, 0, Data.Length);
+                file.Close();
+            }
 
 
             var subSubDirToCopy = subDirToCopy
                 .CreateSubdirectory(Path.GetRandomFileName());
-            
+
             // Execute the results.
             await foreach (var res in EmitCopyDirResult(dirToCopy, dir)) ;
 
             Assert.True(dir.ContainsDirectory(subDirToCopy.Name), "Did not copy parent successfully");
             Assert.True(dir.OpenDirectory(subDirToCopy.Name).ContainsFile(fileName), "Did not copy file successfully");
+
+            using (var file = dir.OpenDirectory(subDirToCopy.Name).OpenFile(fileName).OpenStream())
+            {
+                Assert.Equal(Data.Length, file.Length);
+            }
+
             Assert.True(dir.OpenDirectory(subDirToCopy.Name).ContainsDirectory(subSubDirToCopy.Name), "Did not copy nested folder successfully");
+
+            dir.OpenDirectory(subDirToCopy.Name).OpenFile(fileName).Delete();
+            Assert.False(dir.OpenDirectory(subDirToCopy.Name).ContainsFile(fileName), "Did not copy file successfully");
+
         }
 
         [Fact]
@@ -67,10 +96,13 @@ namespace Snowflake.Installation.Tests
 
             var dirToCopy = new DirectoryInfo(temp).CreateSubdirectory(Path.GetRandomFileName());
             var subDirToCopy = dirToCopy.CreateSubdirectory(Path.GetRandomFileName());
-            var file = System.IO.File.Create(Path.Combine(subDirToCopy.FullName, Path.GetRandomFileName()));
-            var fileName = Path.GetFileName(file.Name);
-            file.Close();
-
+            string fileName;
+            using (var file = System.IO.File.Create(Path.Combine(subDirToCopy.FullName, Path.GetRandomFileName())))
+            {
+                fileName = Path.GetFileName(file.Name);
+                await file.WriteAsync(Data, 0, Data.Length);
+                file.Close();
+            }
 
             var subSubDirToCopy = subDirToCopy
                 .CreateSubdirectory(Path.GetRandomFileName());
@@ -84,11 +116,23 @@ namespace Snowflake.Installation.Tests
 
             Assert.True(dir.ContainsDirectory(dirToCopy.Name), "Did not extract parent successfully");
             Assert.True(dir.OpenDirectory(dirToCopy.Name).OpenDirectory(subDirToCopy.Name).ContainsFile(fileName), "Did not copy file successfully");
+            using (var file = dir.OpenDirectory(dirToCopy.Name).OpenDirectory(subDirToCopy.Name).OpenFile(fileName).OpenStream()) {
+                Assert.Equal(Data.Length, file.Length);
+            }
             Assert.True(dir.OpenDirectory(dirToCopy.Name).OpenDirectory(subDirToCopy.Name).ContainsDirectory(subSubDirToCopy.Name), "Did not copy nested folder successfully");
+
+            dir.OpenDirectory(dirToCopy.Name).OpenDirectory(subDirToCopy.Name).OpenFile(fileName).Delete();
+            Assert.False(dir.OpenDirectory(dirToCopy.Name).OpenDirectory(subDirToCopy.Name).ContainsFile(fileName), "Did not cleanup file successfully");
+
+            try
+            {
+                // nothing to do with tests here if this fails 
+                System.IO.File.Delete(zipFile); 
+            } catch
+            {
+
+            }
         }
-
-
-
 
         public async IAsyncEnumerable<TaskResult<IFile>> EmitZipResult(string tempFile, IDirectory dir)
         {
