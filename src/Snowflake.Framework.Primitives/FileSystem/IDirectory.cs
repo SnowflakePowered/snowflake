9e57ea7667e0f78f522f52fb70a12b2d53da9c9e
Ronny Chan <ronny6993@gmail.com>
2019-05-03 21:56:27 -0400

    Filesystem: Fully document and test the Filesystem API

--- src/Snowflake.Framework.Primitives/FileSystem/IDirectory.cs
+++ src/Snowflake.Framework.Primitives/FileSystem/IDirectory.cs
@@ -32,45 +32,197 @@ namespace Snowflake.Filesystem
         IDirectory OpenDirectory(string name);
 
         /// <summary>
-        /// Opens or creates a file.
+        /// Opens or creates a file, adding it to the manifest and assigning it a
+        /// unique <see cref="Guid"/>.
+        /// 
+        /// Unlike <see cref="OpenDirectory(string)"/>, you can not use the path separator to
+        /// open a nested file. Paths will be truncated with <see cref="Path.GetFileName(string)"/>.
+        /// 
+        /// Instead, use <see cref="OpenDirectory(string)"/> to open the directory of the desired file,
+        /// then call <see cref="OpenFile(string)"/> on the returned instance.
+        /// 
+        /// Keep in mind this does not actually create a file on the underlying file system. 
+        /// However, you can use <see cref="IFile.OpenStream()"/> and friends to create the file
+        /// if it does not yet exist.
         /// </summary>
-        /// <param name="file"></param>
-        /// <returns></returns>
+        /// <param name="file">The name of the file. If this is a path, will be truncated with <see cref="Path.GetFileName(string)"/></param>
+        /// <returns>An object that associates a given file with a with a unique <see cref="Guid"/></returns>
         IFile OpenFile(string file);
 
-        IFile? CopyFrom(FileInfo source);
+        /// <summary>
+        /// Copies a file from an unmanaged <see cref="FileInfo"/> that exists outside of
+        /// a <see cref="IDirectory"/>.
+        /// Do not use this method to copy from a managed <see cref="IDirectory"/>.
+        /// Instead, use <see cref="CopyFrom(IFile)"/>.
+        /// </summary>
+        /// <exception cref="IOException">If a file with the same name exists in the target destination.</exception>
+        /// <exception cref="FileNotFoundException">If the source file can not be found.</exception>
+        /// <param name="source">The <see cref="FileInfo"/></param>
+        /// <returns>The <see cref="IFile"/> that describes the file in the current <see cref="IDirectory"/>.</returns>
+        IFile CopyFrom(FileInfo source);
 
-        IFile? CopyFrom(FileInfo source, bool overwrite);
+        /// <summary>
+        /// Copies a file from an unmanaged <see cref="FileInfo"/> that exists outside of
+        /// a <see cref="IDirectory"/>.
+        /// Do not use this method to copy from a managed <see cref="IDirectory"/>.
+        /// Instead, use <see cref="CopyFrom(IFile)"/>.
+        /// </summary>
+        /// <exception cref="IOException">If a file with the same name exists in the target destination and <paramref name="overwrite"/> is false.</exception>
+        /// <exception cref="FileNotFoundException">If the source file can not be found.</exception>
+        /// <param name="source">The <see cref="FileInfo"/></param>
+        /// <param name="overwrite">Overwrite the file if it already exists in this <see cref="IDirectory"/></param>
+        /// <returns>The <see cref="IFile"/> that describes the file in the current <see cref="IDirectory"/>.</returns>
+        IFile CopyFrom(FileInfo source, bool overwrite);
 
-        Task<IFile?> CopyFromAsync(FileInfo source);
+        /// <summary>
+        /// Copies a file asynchronously from an unmanaged <see cref="FileInfo"/> that exists outside of
+        /// a <see cref="IDirectory"/>.
+        /// Do not use this method to copy from a managed <see cref="IDirectory"/>.
+        /// Instead, use <see cref="CopyFrom(IFile)"/>.
+        /// </summary>
+        /// <exception cref="IOException">If a file with the same name exists in the target destination.</exception>
+        /// <exception cref="FileNotFoundException">If the source file can not be found.</exception>
+        /// <param name="source">The <see cref="FileInfo"/></param>
+        /// <returns>The <see cref="IFile"/> that describes the file in the current <see cref="IDirectory"/>.</returns>
+        Task<IFile> CopyFromAsync(FileInfo source, CancellationToken cancellation = default);
 
-        Task<IFile?> CopyFromAsync(FileInfo source, bool overwrite);
+        /// <summary>
+        /// Copies a file asynchronously from an unmanaged <see cref="FileInfo"/> that exists outside of
+        /// a <see cref="IDirectory"/>.
+        /// Do not use this method to copy from a managed <see cref="IDirectory"/>.
+        /// Instead, use <see cref="CopyFrom(IFile)"/>.
+        /// </summary>
+        /// <exception cref="IOException">If a file with the same name exists in the target destination and <paramref name="overwrite"/> is false.</exception>
+        /// <exception cref="FileNotFoundException">If the source file can not be found.</exception>
+        /// <param name="source">The <see cref="FileInfo"/></param>
+        /// <param name="overwrite">Overwrite the file if it already exists in this <see cref="IDirectory"/></param>
+        /// <returns>The <see cref="IFile"/> that describes the file in the current <see cref="IDirectory"/>.</returns>
+        Task<IFile> CopyFromAsync(FileInfo source, bool overwrite, CancellationToken cancellation = default);
 
-        Task<IFile?> CopyFromAsync(FileInfo source, bool overwrite, CancellationToken cancellation);
+        /// <summary>
+        /// Moves a file between <see cref="IDirectory"/>, updating the 
+        /// manifests such that the resulting file has the same <see cref="IFile.FileGuid"/> as the source file.
+        /// 
+        /// The source file will cease to exist in its original <see cref="IDirectory"/>.
+        ///
+        /// There is no asychronous equivalent by design, since <see cref="MoveFrom(IFile, bool)"/> is intended to
+        /// be faster than <see cref="CopyFromAsync(IFile, CancellationToken)"/> if the <see cref="IDirectory"/> instances
+        /// are on the same file system. Otherwise, you should use <see cref="CopyFromAsync(IFile, CancellationToken)"/>,
+        /// then <see cref="IFile.Delete"/> the old file.
+        /// </summary>
+        /// <exception cref="IOException">If a file with the same name exists in the target destination.</exception>
+        /// <exception cref="FileNotFoundException">If the source file can not be found.</exception>
+        /// <param name="source">The <see cref="FileInfo"/></param>
+        /// <returns>The <see cref="IFile"/> that describes the file in the current <see cref="IDirectory"/>.</returns>
+        IFile MoveFrom(IFile source);
 
-        IFile? MoveFrom(IFile source);
+        /// <summary>
+        /// Moves a file between <see cref="IDirectory"/>, updating the 
+        /// manifests such that the resulting file has the same <see cref="IFile.FileGuid"/> as the source file.
+        /// 
+        /// The source file will cease to exist in its original <see cref="IDirectory"/>.
+        /// 
+        /// There is no asychronous equivalent by design, since <see cref="MoveFrom(IFile, bool)"/> is intended to
+        /// be faster than <see cref="CopyFromAsync(IFile, bool, CancellationToken)"/> if the <see cref="IDirectory"/> instances
+        /// are on the same file system. Otherwise, you should use <see cref="CopyFromAsync(IFile, bool, CancellationToken)"/>,
+        /// then <see cref="IFile.Delete"/> the old file.
+        /// </summary>
+        /// <exception cref="IOException">If a file with the same name exists in the target destination and <paramref name="overwrite"/> is false.</exception>
+        /// <exception cref="FileNotFoundException">If the source file can not be found.</exception>
+        /// <param name="source">The <see cref="FileInfo"/></param>
+        /// <returns>The <see cref="IFile"/> that describes the file in the current <see cref="IDirectory"/>.</returns>
+        /// <param name="overwrite">Overwrite the file if it already exists in this <see cref="IDirectory"/></param>
+        IFile MoveFrom(IFile source, bool overwrite);
 
-        IFile? MoveFrom(IFile source, bool overwrite);
+        /// <summary>
+        /// Copies a file from a <see cref="IFile"/> from another <see cref="IDirectory"/>, updating the
+        /// manifests such that the resulting file has the same <see cref="IFile.FileGuid"/> as the source file.
+        /// </summary>
+        /// <exception cref="IOException">If a file with the same name exists in the target destination.</exception>
+        /// <exception cref="FileNotFoundException">If the source file can not be found.</exception>
+        /// <param name="source">The <see cref="FileInfo"/></param>
+        /// <returns>The <see cref="IFile"/> that describes the file in the current <see cref="IDirectory"/>.</returns>
+        IFile CopyFrom(IFile source);
 
-        IFile? CopyFrom(IFile source);
+        /// <summary>
+        /// Copies a file from a <see cref="IFile"/> from another <see cref="IDirectory"/>, updating the
+        /// manifests such that the resulting file has the same <see cref="IFile.FileGuid"/> as the source file.
+        /// </summary>
+        /// <exception cref="IOException">If a file with the same name exists in the target destination and <paramref name="overwrite"/> is false.</exception>
+        /// <exception cref="FileNotFoundException">If the source file can not be found.</exception>
+        /// <param name="source">The <see cref="FileInfo"/></param>
+        /// <param name="overwrite">Overwrite the file if it already exists in this <see cref="IDirectory"/></param>
+        /// <returns>The <see cref="IFile"/> that describes the file in the current <see cref="IDirectory"/>.</returns>
+        IFile CopyFrom(IFile source, bool overwrite);
 
-        IFile? CopyFrom(IFile source, bool overwrite);
+        /// <summary>
+        /// Copies a file asynchronously from a <see cref="IFile"/> from another <see cref="IDirectory"/>, updating the
+        /// manifests such that the resulting file has the same <see cref="IFile.FileGuid"/> as the source file.
+        /// </summary>
+        /// <exception cref="IOException">If a file with the same name exists in the target destination.</exception>
+        /// <exception cref="FileNotFoundException">If the source file can not be found.</exception>
+        /// <param name="source">The <see cref="FileInfo"/></param>
+        /// <param name="cancellation">Cancellation token for the asynchronous task.</param>
+        /// <returns>The <see cref="IFile"/> that describes the file in the current <see cref="IDirectory"/>.</returns>
+        Task<IFile> CopyFromAsync(IFile source, CancellationToken cancellation = default);
 
-        Task<IFile?> CopyFromAsync(IFile source);
+        /// <summary>
+        /// Copies a file asynchronously from a <see cref="IFile"/> from another <see cref="IDirectory"/>, updating the
+        /// manifests such that the resulting file has the same <see cref="IFile.FileGuid"/> as the source file.
+        /// </summary>
+        /// <exception cref="IOException">If a file with the same name exists in the target destination and <paramref name="overwrite"/> is false.</exception>
+        /// <exception cref="FileNotFoundException">If the source file can not be found.</exception>
+        /// <param name="source">The <see cref="FileInfo"/></param>
+        /// <param name="overwrite">Overwrite the file if it already exists in this <see cref="IDirectory"/></param>
+        /// <param name="cancellation">Cancellation token for the asynchronous task.</param>
+        /// <returns>The <see cref="IFile"/> that describes the file in the current <see cref="IDirectory"/>.</returns>
+        Task<IFile> CopyFromAsync(IFile source, bool overwrite, CancellationToken cancellation = default);
 
-        Task<IFile?> CopyFromAsync(IFile source, bool overwrite);
-
-        Task<IFile?> CopyFromAsync(IFile source, bool overwrite, CancellationToken cancellation);
-
+        /// <summary>
+        /// Enumerates all direct child directories of this <see cref="IDirectory"/>.
+        /// </summary>
+        /// <returns>All direct children directories.</returns>
         IEnumerable<IDirectory> EnumerateDirectories();
 
+        /// <summary>
+        /// Enumerates all files in this directory,
+        /// </summary>
+        /// <returns>All direct children files.</returns>
         IEnumerable<IFile> EnumerateFiles();
 
+        /// <summary>
+        /// Recursively enumerates all files that are contained in this directory.
+        /// 
+        /// This method is usually implemented as a Breadth-first search, but no order is guaranteed.
+        /// </summary>
+        /// <returns>All files contained within this directory, including descendant subfolders.</returns>
         IEnumerable<IFile> EnumerateFilesRecursive();
 
+        /// <summary>
+        /// Whether or not this directory contains a file in its manifest. If provided a
+        /// full path, this will truncate the path using <see cref="Path.GetFileName(string)"/>
+        /// </summary>
+        /// <param name="file">The name of the file to check.</param>
+        /// <returns>Whether or not this directory contains the given file.</returns>
         bool ContainsFile(string file);
+
+        /// <summary>
+        /// Whether or not this directory contains directory as a direct child.
+        /// full path, this will truncate the path using <see cref="Path.GetDirectoryName(string)"/>
+        /// </summary>
+        /// <param name="directory">The name of the directory to check.</param>
+        /// <returns>Whether or not this directory contains the given directory.</returns>
         bool ContainsDirectory(string directory);
 
+        /// <summary>
+        /// Gets the underlying <see cref="DirectoryInfo"/> where files are contained.
+        /// 
+        /// This is very rarely necessary, and most IO tasks can be done efficiently and safely using the
+        /// provided API. Manipulating the underlying file system will potentially desync the manifest,
+        /// and cause loss of associated metadata.
+        /// </summary>
+        /// <returns>The underlying <see cref="DirectoryInfo"/> where files are contained.</returns>
+        [Obsolete("Avoid accessing the underlying file path, and use the object methods instead.")]
         DirectoryInfo GetPath();
     }
 }
--- src/Snowflake.Framework.Primitives/FileSystem/IFile.cs
+++ src/Snowflake.Framework.Primitives/FileSystem/IFile.cs
@@ -6,19 +6,69 @@ using Snowflake.Model.Records.File;
 
 namespace Snowflake.Filesystem
 {
+    /// <summary>
+    /// Represents a file contained within an <see cref="IDirectory"/> that could have
+    /// metadata potentially attached to it by a unique <see cref="Guid"/> that tracks the file
+    /// within the file system, as long as the <see cref="Snowflake.Filesystem"/> API methods
+    /// are used.
+    /// </summary>
     public interface IFile
     {
+        /// <summary>
+        /// The name of the file.
+        /// </summary>
         string Name { get; }
+        /// <summary>
+        /// The length of the file in bytes.
+        /// </summary>
         long Length { get; }
+        /// <summary>
+        /// The <see cref="IDirectory"/> under which this file is located.
+        /// </summary>
         IDirectory ParentDirectory { get; }
+        /// <summary>
+        /// Opens a read-write stream to the file. If it does not currently exist,
+        /// it will be created. 
+        /// </summary>
+        /// <returns>A read-write stream to the file.</returns>
         Stream OpenStream();
+        /// <summary>
+        /// Opens a stream to the file with the given <see cref="FileAccess"/>. 
+        /// If it does not currently exist, it will be created. 
+        /// </summary>
+        /// <param name="rw">The <see cref="FileAccess"/> permissions to open the stream with.</param>
+        /// <returns>A stream to the file with the given <see cref="FileAccess"/>.</returns>
         Stream OpenStream(FileAccess rw);
+        /// <summary>
+        /// Renames the file, keeping the same <see cref="FileGuid"/>.
+        /// If the provided name is a path, it will be truncated with <see cref="Path.GetFileName(string)"/>.
+        /// 
+        /// To move files between directories, use <see cref="IDirectory.MoveFrom(IFile)"/> and
+        /// <see cref="IDirectory.CopyFrom(IFile)"/> and friends.
+        /// </summary>
+        /// <param name="newName">The new name of the <see cref="IFile"/></param>
         void Rename(string newName);
+
+        /// <summary>
+        /// Deletes a <see cref="IFile"/> from the underlying file system as well as the
+        /// manifest of the <see cref="ParentDirectory"/>.
+        /// 
+        /// If <see cref="IFile.Created"/> is false, the file will simply be removed from the
+        /// manifest.
+        /// </summary>
         void Delete();
 
-        ///
+        /// <summary>
+        /// The unique ID identifying this file in the directory manifest.
+        /// </summary>
         Guid FileGuid { get; }
 
+        /// <summary>
+        /// Whether or not this file has been created or currently exists.
+        /// 
+        /// Keep in mind that <see cref="IDirectory.OpenFile(string)"/> does not actually
+        /// create a file on the file system, merely
+        /// </summary>
         bool Created { get; }
 
         /// <summary>
