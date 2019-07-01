bd79213169add5fc3c1889d4e51d6efbb09ff8ab
Ronny Chan <ronny6993@gmail.com>
2019-07-01 00:36:13 -0400

    Tests: Test config traversal more throughly

--- src/Snowflake.Framework.Tests/Configuration/Serialization/ConfigurationTraversalContextTests.cs
+++ src/Snowflake.Framework.Tests/Configuration/Serialization/ConfigurationTraversalContextTests.cs
@@ -28,6 +28,27 @@ namespace Snowflake.Configuration.Serialization
             var context = new ConfigurationTraversalContext(dir);
 
             var list = context.TraverseSection(configuration.Configuration.ExampleConfiguration);
+
+            Assert.Equal(6, list.Count);
+            Assert.Equal("FullscreenResolution", list[0].Key);
+            Assert.IsType<EnumConfigurationNode>(list[0]);
+            Assert.Equal("1152x648", ((EnumConfigurationNode)list[0]).Value);
+            Assert.Equal(FullscreenResolution.Resolution1152X648, list[0].Value);
+
+            Assert.Equal("Fullscreen", list[1].Key);
+            Assert.IsType<BoolConfigurationNode>(list[1]);
+
+            Assert.Equal("RenderToMain", list[2].Key);
+            Assert.IsType<BoolConfigurationNode>(list[2]);
+
+            Assert.Equal("RenderWindowWidth", list[3].Key);
+            Assert.IsType<IntegralConfigurationNode>(list[3]);
+
+            Assert.Equal("RenderWindowHeight", list[4].Key);
+            Assert.IsType<IntegralConfigurationNode>(list[4]);
+
+            Assert.Equal("ISOPath0", list[5].Key);
+            Assert.IsType<StringConfigurationNode>(list[5]);
         }
 
         [Fact]
@@ -45,6 +66,43 @@ namespace Snowflake.Configuration.Serialization
             var context = new ConfigurationTraversalContext(dir);
 
             var list = context.TraverseCollection(configuration.Configuration);
+            Assert.Equal(2, list.Count);
+            Assert.Equal(2, list["#dolphin"].Count);
+
+            var dolphinList = list["#dolphin"];
+            foreach (var node in dolphinList)
+            {
+                if (node.Key == "Display")
+                {
+                    var confList = (node as ListConfigurationNode).Value;
+                    Assert.Equal(6, confList.Count);
+                    Assert.Equal("FullscreenResolution", confList[0].Key);
+                    Assert.IsType<EnumConfigurationNode>(confList[0]);
+                    Assert.Equal("1152x648", ((EnumConfigurationNode)confList[0]).Value);
+                    Assert.Equal(FullscreenResolution.Resolution1152X648, confList[0].Value);
+
+                    Assert.Equal("Fullscreen", confList[1].Key);
+                    Assert.IsType<BoolConfigurationNode>(confList[1]);
+
+                    Assert.Equal("RenderToMain", confList[2].Key);
+                    Assert.IsType<BoolConfigurationNode>(confList[2]);
+
+                    Assert.Equal("RenderWindowWidth", confList[3].Key);
+                    Assert.IsType<IntegralConfigurationNode>(confList[3]);
+
+                    Assert.Equal("RenderWindowHeight", confList[4].Key);
+                    Assert.IsType<IntegralConfigurationNode>(confList[4]);
+
+                    Assert.Equal("ISOPath0", confList[5].Key);
+                    Assert.IsType<StringConfigurationNode>(confList[5]);
+                }
+
+                if (node.Key == "TestNestedSection")
+                {
+                    Assert.Equal("TestNestedNestedSection", (node as ListConfigurationNode).Value[0].Key);
+                }
+            }
+           
         }
     }
 }
