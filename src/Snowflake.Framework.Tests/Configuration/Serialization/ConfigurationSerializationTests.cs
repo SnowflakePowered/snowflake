c8c1a966b03a73e7cb3b13ba2ec12ac12b4f7a0a
Ronny Chan <ronny6993@gmail.com>
2019-07-04 20:59:12 -0400

    Configuration: Pass index to node serializer

--- src/Snowflake.Framework.Tests/Configuration/Serialization/ConfigurationSerializationTests.cs
+++ src/Snowflake.Framework.Tests/Configuration/Serialization/ConfigurationSerializationTests.cs
@@ -14,7 +14,7 @@ namespace Snowflake.Configuration.Serialization
     public class ConfigurationSerializationTests
     {
         [Fact]
-        public void CollectionToAbstractConfigurationNode_Test()
+        public void CollectionToAbstractConfigurationNodeIniSerializer_Test()
         {
             var configuration =
               new ConfigurationCollection<ExampleConfigurationCollection>(new ConfigurationValueCollection());
@@ -31,14 +31,71 @@ namespace Snowflake.Configuration.Serialization
             IAbstractConfigurationNode dolphinList = list["#dolphin"];
 
             var iniSerializer = new SimpleIniConfigurationSerializer();
-            var xmlSerializer = new SimpleXmlConfigurationSerializer("Config");
-            var cfgSerializer = new SimpleCfgConfigurationSerializer();
-            var jsonSerializer = new SimpleJsonConfigurationSerializer();
-
             string outputIni = iniSerializer.Serialize(dolphinList);
-            string outputXml = xmlSerializer.Serialize(dolphinList);
-            string outputJson = jsonSerializer.Serialize(dolphinList);
+          
+        }
+
+        [Fact]
+        public void CollectionToAbstractConfigurationNodeCfgSerializer_Test()
+        {
+            var configuration =
+              new ConfigurationCollection<ExampleConfigurationCollection>(new ConfigurationValueCollection());
+
+            var fs = new PhysicalFileSystem();
+            var temp = Path.GetTempPath();
+            var pfs = fs.GetOrCreateSubFileSystem(fs.ConvertPathFromInternal(temp));
+            var dir = new FS.Directory("test", pfs, pfs.GetDirectoryEntry("/"));
+
+            configuration.Configuration.ExampleConfiguration.FullscreenResolution = FullscreenResolution.Resolution1152X648;
+            var context = new ConfigurationTraversalContext(dir);
+
+            var list = context.TraverseCollection(configuration.Configuration);
+            IAbstractConfigurationNode dolphinList = list["#dolphin"];
+
+            var cfgSerializer = new SimpleCfgConfigurationSerializer();
             string outputCfg = cfgSerializer.Serialize(dolphinList);
+        }
+
+        [Fact]
+        public void CollectionToAbstractConfigurationNodeXmlSerializer_Test()
+        {
+            var configuration =
+              new ConfigurationCollection<ExampleConfigurationCollection>(new ConfigurationValueCollection());
+
+            var fs = new PhysicalFileSystem();
+            var temp = Path.GetTempPath();
+            var pfs = fs.GetOrCreateSubFileSystem(fs.ConvertPathFromInternal(temp));
+            var dir = new FS.Directory("test", pfs, pfs.GetDirectoryEntry("/"));
+
+            configuration.Configuration.ExampleConfiguration.FullscreenResolution = FullscreenResolution.Resolution1152X648;
+            var context = new ConfigurationTraversalContext(dir);
+            var list = context.TraverseCollection(configuration.Configuration);
+            IAbstractConfigurationNode dolphinList = list["#dolphin"];
+
+            var xmlSerializer = new SimpleXmlConfigurationSerializer("Config");
+           
+            string outputXml = xmlSerializer.Serialize(dolphinList);
+        }
+
+        [Fact]
+        public void CollectionToAbstractConfigurationNodeJsonSerializer_Test()
+        {
+            var configuration =
+              new ConfigurationCollection<ExampleConfigurationCollection>(new ConfigurationValueCollection());
+
+            var fs = new PhysicalFileSystem();
+            var temp = Path.GetTempPath();
+            var pfs = fs.GetOrCreateSubFileSystem(fs.ConvertPathFromInternal(temp));
+            var dir = new FS.Directory("test", pfs, pfs.GetDirectoryEntry("/"));
+
+            configuration.Configuration.ExampleConfiguration.FullscreenResolution = FullscreenResolution.Resolution1152X648;
+            var context = new ConfigurationTraversalContext(dir);
+
+            var list = context.TraverseCollection(configuration.Configuration);
+            IAbstractConfigurationNode dolphinList = list["#dolphin"];
+
+            var jsonSerializer = new SimpleJsonConfigurationSerializer();
+            string outputJson = jsonSerializer.Serialize(dolphinList);
         }
     }
 }
--- src/Snowflake.Framework/Configuration/Serialization/Serializers/Implementations/SimpleXmlConfigurationSerializer.cs
+++ src/Snowflake.Framework/Configuration/Serialization/Serializers/Implementations/SimpleXmlConfigurationSerializer.cs
@@ -19,42 +19,42 @@ namespace Snowflake.Configuration.Serialization.Serializers.Implementations
             context.AppendLine($"<{this.RootElementName}>");
         }
 
-        public override void SerializeFooter(IConfigurationSerializationContext<string> context)
+        public override void SerializeFooter( IConfigurationSerializationContext<string> context)
         {
             context.AppendLine($"</{this.RootElementName}>");
         }
 
-        public override void SerializeBlockBegin(IConfigurationSerializationContext<string> context)
+        public override void SerializeBlockBegin( IConfigurationSerializationContext<string> context, int index)
         {
             // todo: sanitize
             context.AppendLine($"<{context.GetCurrentScope()}>");
         }
-        public override void SerializeBlockEnd(IConfigurationSerializationContext<string> context)
+        public override void SerializeBlockEnd( IConfigurationSerializationContext<string> context, int index)
         {
             // todo: sanitize
             context.AppendLine($"</{context.GetCurrentScope()}>");
         }
-        public override void SerializeNodeValue(bool value, string key, IConfigurationSerializationContext<string> context)
+        public override void SerializeNodeValue(bool value, string key,  IConfigurationSerializationContext<string> context, int index)
         {
             context.AppendLine($"<{key}>{value}</{key}>");
         }
-        public override void SerializeNodeValue(double value, string key, IConfigurationSerializationContext<string> context)
+        public override void SerializeNodeValue(double value, string key,  IConfigurationSerializationContext<string> context, int index)
         {
             context.AppendLine($"<{key}>{value}</{key}>");
         }
-        public override void SerializeNodeValue(Enum value, string enumValue, string key, IConfigurationSerializationContext<string> context)
+        public override void SerializeNodeValue(Enum value, string enumValue, string key,  IConfigurationSerializationContext<string> context, int index)
         {
             context.AppendLine($"<{key}>{enumValue}</{key}>");
         }
-        public override void SerializeNodeValue(long value, string key, IConfigurationSerializationContext<string> context)
+        public override void SerializeNodeValue(long value, string key,  IConfigurationSerializationContext<string> context, int index)
         {
             context.AppendLine($"<{key}>{value}</{key}>");
         }
-        public override void SerializeNodeValue(string value, string key, IConfigurationSerializationContext<string> context)
+        public override void SerializeNodeValue(string value, string key,  IConfigurationSerializationContext<string> context, int index)
         {
             context.AppendLine($"<{key}>{value}</{key}>");
         }
-        public override void SerializeNodeValue(string key, IConfigurationSerializationContext<string> context)
+        public override void SerializeNodeValue(string key,  IConfigurationSerializationContext<string> context, int index)
         {
             context.AppendLine($"<{key}></{key}>");
         }
