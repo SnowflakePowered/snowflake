9290ddde9d0469fabd0d390118e484211c9b7cd0
Ronny Chan <ronny6993@gmail.com>
2019-07-07 02:08:24 -0400

    Serialization: Test edge cases for string serialization

---
+++ src/Snowflake.Framework.Tests/Configuration/Serialization/StringSerializationContextTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Snowflake.Configuration.Serialization.Serializers;
+using Xunit;
+
+namespace Snowflake.Configuration.Serialization
+{
+    public class StringSerializationContextTests
+    {
+        [Fact]
+        public void Scope_Tests()
+        {
+           var context = new StringSerializationContext();
+            Assert.Equal(string.Empty, context.ExitScope());
+            Assert.Equal(string.Empty, context.GetCurrentScope());
+
+        }
+    }
+}
