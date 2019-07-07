e663654163447d7e8d0ffb8ff3a7d4a99e9b84f5
Ronny Chan <ronny6993@gmail.com>
2019-07-07 01:03:00 -0400

    ConfigurationTarget: remove useless equality check

--- src/Snowflake.Framework/Configuration/Serialization/ConfigurationTarget.cs
+++ src/Snowflake.Framework/Configuration/Serialization/ConfigurationTarget.cs
@@ -8,11 +8,8 @@ namespace Snowflake.Configuration.Serialization
     /// Defines a unit of a <see cref="IConfigurationCollection{T}"/> that will be
     /// evaluated independently into a tree of <see cref="IAbstractConfigurationNode"/>
     /// that will be evaluated and produce an output or side effects.
-    /// 
-    /// As an implementation detail, two configuaration targets of the same name
-    /// are equal.
     /// </summary>
-    public class ConfigurationTarget : IConfigurationTarget, IEquatable<ConfigurationTarget>
+    public class ConfigurationTarget : IConfigurationTarget
     {
         internal Dictionary<string, IConfigurationTarget> ChildTargets { get; }
 
@@ -24,22 +21,6 @@ namespace Snowflake.Configuration.Serialization
         {
             this.ChildTargets = new Dictionary<string, IConfigurationTarget>();
             this.TargetName = targetName;
-        }
-
-        public bool Equals(ConfigurationTarget other)
-        {
-            return other.TargetName == this.TargetName;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (obj is ConfigurationTarget t) return this.Equals(obj);
-            return base.Equals(obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return this.TargetName.GetHashCode();
         }
     }
 }
