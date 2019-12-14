467c21b7263a99698aed8896659425227b9c5879
Ronny Chan <ronny6993@gmail.com>
2019-12-13 22:44:46 -0500

    Cleanup: remove unnecessary use of anonymous classes when tuples will do

--- src/Snowflake.Framework/JsonConverters/PlatformInfoConverter.cs
+++ src/Snowflake.Framework/JsonConverters/PlatformInfoConverter.cs
@@ -27,7 +27,7 @@ namespace Snowflake.JsonConverters
             var biosFiles = jObject.Value<JToken>("BiosFiles") != null
                 ? (from property in biosProps
                     from hash in property?.Values<JToken>().Values<string>().DefaultIfEmpty(string.Empty)
-                    select new {FileName = property?.Name, Hash = hash})?
+                    select (FileName: property?.Name, Hash: hash))?
                 .Select(p => new BiosFile(p.FileName, p.Hash))
                 .ToList() ?? Enumerable.Empty<IBiosFile>()
                 : Enumerable.Empty<IBiosFile>();
