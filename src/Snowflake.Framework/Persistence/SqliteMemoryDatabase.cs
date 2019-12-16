1c32eede1bdd07ef74abd15ba9dc75efc083c863
Ronny Chan <ronny6993@gmail.com>
2019-12-16 02:18:40 -0500

    Build: Update dependencies

--- src/Snowflake.Framework/Persistence/SqliteMemoryDatabase.cs
+++ src/Snowflake.Framework/Persistence/SqliteMemoryDatabase.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Data.Common;
-using System.IO;
-using System.Linq;
-using System.Text;
 using Dapper;
 using Microsoft.Data.Sqlite;
 
