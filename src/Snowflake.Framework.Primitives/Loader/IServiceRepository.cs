feddb4d3a12bc4437993d9ba41464ea8bd1c5bf0
Ronny Chan <ronny6993@gmail.com>
2020-03-01 22:39:31 -0500

    IServiceProvider: Fix semantics for IServiceProvider

--- src/Snowflake.Framework.Primitives/Loader/IServiceRepository.cs
+++ src/Snowflake.Framework.Primitives/Loader/IServiceRepository.cs
@@ -26,7 +26,10 @@ namespace Snowflake.Loader
         IEnumerable<string> Services { get; }
         
         /// <summary>
-        /// Returns a <see cref="IServiceProvider"/> that includes the services available in this instance of <see cref="IServiceRepository"/>
+        /// Returns a <see cref="IServiceProvider"/> that includes the services available in this instance of <see cref="IServiceRepository"/>.
+        /// The returned <see cref="IServiceProvider"/> has different usage semantics for <see cref="IServiceProvider.GetService(Type)"/> than
+        /// <see cref="IServiceRepository.Get{T}"/>, namely, getting a service that has not been authorized for this <see cref="IServiceRepository"/>,
+        /// or one that does not exist, will return null instead of throwing <see cref="InvalidOperationException"/>.
         /// </summary>
         /// <returns>A <see cref="IServiceProvider"/> that contain all available services in this instance of <see cref="IServiceRepository"/>.</returns>
         IServiceProvider ToServiceProvider();
--- src/Snowflake.Framework/Loader/ServiceProvider.cs
+++ src/Snowflake.Framework/Loader/ServiceProvider.cs
@@ -27,22 +27,29 @@ namespace Snowflake.Loader
         public T Get<T>()
             where T : class
         {
-            return (T)this.GetService(typeof(T));
-        }
-
-        public object GetService(Type serviceType)
-        {
-            if (serviceType.FullName == null)
+            if (typeof(T).FullName == null)
                 throw new InvalidOperationException($"Attempted to get service of a generic type parameter," +
                     $" array type, pointer type, or byref type based on type parameter.");
 
-            if (services.Contains(serviceType.FullName))
+            if (services.Contains(typeof(T).FullName!))
+            {
+                return this.coreService.Get<T>();
+            }
+
+            throw new InvalidOperationException(
+                $"Service container is not authorized to provide service {typeof(T).FullName} or service does not exist.");
+        }
+
+        public object? GetService(Type serviceType)
+        {
+            if (serviceType.FullName == null) return null;
+
+            if (services.Contains(serviceType.FullName!))
             {
                 return this.coreService.Get(serviceType);
             }
 
-            throw new InvalidOperationException(
-                $"Service container is not authorized to provide service {serviceType.FullName} or service does not exist.");
+            return null;
         }
     }
 }
