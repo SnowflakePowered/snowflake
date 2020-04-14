954b76415dd9c061defd72500e5615fea3636fef
Ronny Chan <ronny6993@gmail.com>
2020-04-13 20:32:19 -0400

    HotChocolate: Add a simple debug query

---
+++ src/Snowflake.Support.GraphQLFrameworkQueries/Queries/Debug/EchoQueries.cs
@@ -0,0 +1,19 @@
+﻿using HotChocolate.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snowflake.Support.GraphQLFrameworkQueries.Queries.Debug
+{
+    public class EchoQueries
+        : ObjectTypeExtension
+    {
+        protected override void Configure(IObjectTypeDescriptor descriptor)
+        {
+            descriptor
+                .Name("Query");
+            descriptor.Field("DEBUG__HelloWorld")
+                .Resolver("Hello World");
+        }
+    }
+}
