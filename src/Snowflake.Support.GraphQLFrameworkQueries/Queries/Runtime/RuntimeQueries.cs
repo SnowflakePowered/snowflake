47348b53bff084b6bfc9e1dff666b59fa1068167
Ronny Chan <ronny6993@gmail.com>
2020-04-14 20:37:57 -0400

    HC.runtime: Implement some runtime queries

---
+++ src/Snowflake.Support.GraphQLFrameworkQueries/Queries/Runtime/RuntimeQueries.cs
@@ -0,0 +1,26 @@
+﻿using HotChocolate.Language;
+using HotChocolate.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snowflake.Support.GraphQLFrameworkQueries.Queries.Runtime
+{
+    public class RuntimeQueryRoot {
+        public static RuntimeQueryRoot Root = new RuntimeQueryRoot();
+    }
+
+    public sealed class RuntimeQueries
+        : ObjectTypeExtension
+    {
+        protected override void Configure(IObjectTypeDescriptor descriptor)
+        {
+            descriptor
+                .Name("Query");
+            descriptor.Field("runtime")
+                .Type<RuntimeQueryType>()
+                .Description("Provides access to Snowflake runtime details.")
+                .Resolver(RuntimeQueryRoot.Root);
+        }
+    }
+}
