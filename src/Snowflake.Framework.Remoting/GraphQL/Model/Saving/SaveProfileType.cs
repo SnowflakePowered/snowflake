b169c3d02369dda56cd82bb50b24c17f17be7341
Ronny Chan <ronny6993@gmail.com>
2020-04-16 01:14:04 -0400

    HC.games: Save Queries

---
+++ src/Snowflake.Framework.Remoting/GraphQL/Model/Saving/SaveProfileType.cs
@@ -0,0 +1,40 @@
+﻿using HotChocolate.Types;
+using Snowflake.Orchestration.Saving;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snowflake.Framework.Remoting.GraphQL.Model.Saving
+{
+    public sealed class SaveProfileType
+        : ObjectType<ISaveProfile>
+    {
+        protected override void Configure(IObjectTypeDescriptor<ISaveProfile> descriptor)
+        {
+
+            descriptor.Name("SaveProfile")
+                .Description("Describes a save profile, which is a linked list of save games, forming a lineage of saves from the first game in the profile " +
+                "to the head");
+            descriptor.Field(s => s.Guid)
+                .Name("profileID")
+                .Description("The GUID of the save profile. This GUID is contextualized, and only has meaning when the Game this save belongs to is known.")
+                .Type<NonNullType<UuidType>>();
+            descriptor.Field(s => s.ManagementStrategy)
+                .Description("The strategy used to manage save games in this profile.")
+                .Type<SaveManagementStrategyEnum>();
+            descriptor.Field(s => s.SaveType)
+                .Description("A string value that identifies the format of the save data. All save games with the same format should use the same string.")
+                .Type<NonNullType<StringType>>();
+            descriptor.Field("head")
+                .Description("The 'head' save, or the latest save in the profile. If the profile is empty, this is null.")
+                .Resolver(ctx => ctx.Parent<ISaveProfile>().GetHeadSave())
+                .Type<SaveGameType>();
+            descriptor.Field("history")
+                .Description("All saves in the profile history. " +
+                "If not using a DIFF or COPY strategy, this will consist only of the head save. " +
+                "If this profile is empty, consists of an empty array.")
+                .Resolver(ctx => ctx.Parent<ISaveProfile>().GetHistory())
+                .Type<NonNullType<ListType<NonNullType<SaveGameType>>>>();
+        }
+    }
+}
---
+++ src/Snowflake.Support.GraphQLFrameworkQueries/Queries/Game/LibraryExtensions/SaveQueries.cs
@@ -0,0 +1,56 @@
+﻿using HotChocolate;
+using HotChocolate.Types;
+using Snowflake.Framework.Remoting.GraphQL.Model.Saving;
+using Snowflake.Model.Game;
+using Snowflake.Model.Game.LibraryExtensions;
+using Snowflake.Orchestration.Saving;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snowflake.Support.GraphQLFrameworkQueries.Queries.Game
+{
+    public sealed class SaveQueries
+        : ObjectTypeExtension<IGame>
+    {
+        protected override void Configure(IObjectTypeDescriptor<IGame> descriptor)
+        {
+            descriptor.Name("Game");
+
+            descriptor.Field("saves")
+                .Argument("saveType", arg => 
+                    arg.Description("A string that identifies all save profiles with the same format.")
+                    .Type<StringType>())
+                .Argument("profileID", arg =>
+                    arg.Description("The GUID of a specific profile.")
+                    .Type<UuidType>())
+                .Description("The save profiles associated with this game.")
+                .Resolver(ctx =>
+                {
+                    var profileId = ctx.Argument<Guid>("profileID");
+                    var saveType = ctx.Argument<string>("saveType");
+                    var saves = ctx.Parent<IGame>().WithFiles().WithSaves();
+                    if (profileId != default)
+                    {
+                        var profile = saves.GetProfile(profileId);
+
+                        // Check for save type, to maintain consistent behaviour if both arguments are provided.
+                        if (saveType != null) return profile?.SaveType == saveType ?
+                            new[] { profile } : Enumerable.Empty<ISaveProfile>();
+
+                        // saveType has no value.
+                        return profile != null
+                            ? new[] { profile }
+                            : Enumerable.Empty<ISaveProfile>();
+                    }
+
+                    if (saveType != null) 
+                        return saves.GetProfiles(saveType);
+
+                    return saves.GetProfiles();
+                })
+                .Type<NonNullType<ListType<SaveProfileType>>>();
+        }
+    }
+}
