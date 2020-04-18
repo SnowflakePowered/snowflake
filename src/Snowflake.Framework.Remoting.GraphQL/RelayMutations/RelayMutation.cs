8b8d8a38143867ef992be13fc20dea7eecb013d9
Ronny Chan <ronny6993@gmail.com>
2020-04-18 03:40:11 -0400

    HC.mutations: POC createGame mutation

---
+++ src/Snowflake.Framework.Remoting.GraphQL/RelayMutations/RelayMutation.cs
@@ -0,0 +1,33 @@
+﻿using HotChocolate.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snowflake.Framework.Remoting.GraphQL.RelayMutations
+{
+    public static class RelayMutation
+    {
+        public static IInputObjectTypeDescriptor<T> WithClientMutationId<T>(this IInputObjectTypeDescriptor<T> descriptor)
+            where T : RelayMutationBase
+        {
+            descriptor.Field(i => i.ClientMutationID)
+                .Name("clientMutationId")
+                .Type<StringType>();
+            return descriptor;
+        }
+
+        public static IObjectTypeDescriptor<T> WithClientMutationId<T>(this IObjectTypeDescriptor<T> descriptor)
+            where T : RelayMutationBase
+        {
+            descriptor.Field(i => i.ClientMutationID)
+                .Name("clientMutationId")
+                .Type<StringType>();
+            return descriptor;
+        }
+
+        public static void Reconcile(RelayMutationBase input, RelayMutationBase payload)
+        {
+            payload.ClientMutationID = input.ClientMutationID;
+        }
+    }
+}
---
+++ src/Snowflake.Support.GraphQL.FrameworkQueries/Mutations/Game/GamePayload.cs
@@ -0,0 +1,29 @@
+﻿using HotChocolate.Types;
+using Snowflake.Framework.Remoting.GraphQL.Model.Records;
+using Snowflake.Framework.Remoting.GraphQL.RelayMutations;
+using Snowflake.Model.Game;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snowflake.Support.GraphQL.FrameworkQueries.Mutations.Game
+{
+    internal sealed class GamePayload
+        : RelayMutationBase
+    {
+        public IGame Game { get; set; }
+    }
+
+    internal sealed class GamePayloadType 
+        : ObjectType<GamePayload>
+    {
+        protected override void Configure(IObjectTypeDescriptor<GamePayload> descriptor)
+        {
+            descriptor.Name("GamePayload")
+                .WithClientMutationId();
+
+            descriptor.Field(p => p.Game)
+                .Type<GameType>();
+        }
+    }
+}
