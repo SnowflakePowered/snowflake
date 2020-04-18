6d8781479913efe726f785010bdca0a1be5d4139
Ronny Chan <ronny6993@gmail.com>
2020-04-17 21:12:51 -0400

    HC.runtime: Clarify plugin name

--- src/Snowflake.Framework.Remoting.GraphQL/Model/Extensibility/PluginType.cs
+++ src/Snowflake.Framework.Remoting.GraphQL/Model/Extensibility/PluginType.cs
@@ -14,9 +14,13 @@ namespace Snowflake.Framework.Remoting.GraphQL.Model.Extensibility
         {
             descriptor.Name("Plugin")
                 .Description("Describes a loaded plugin.");
-            descriptor.Field(p => p.Author).Description("The author of the plugin.");
-            descriptor.Field(p => p.Description).Description("The description of the plugin.");
-            descriptor.Field(p => p.Name).Description("The plugin name.");
+            descriptor.Field(p => p.Author)
+                .Description("The author of the plugin.");
+            descriptor.Field(p => p.Description)
+                .Description("The description of the plugin.");
+            descriptor.Field(p => p.Name)
+                .Name("pluginName")
+                .Description("The plugin name.");
             descriptor.Field("version")
                       .Description("The version of the module.")
                       .Resolver(ctx => ctx.Parent<IPlugin>().Version.ToString());
