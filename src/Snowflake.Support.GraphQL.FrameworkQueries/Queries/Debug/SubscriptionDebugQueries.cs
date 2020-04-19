509b15f7877e3b78017b87e13ad060dcd0b964d1
Ronny Chan <ronny6993@gmail.com>
2020-04-18 22:09:28 -0400

    Debug: Subscription debug queries

---
+++ src/Snowflake.Support.GraphQL.FrameworkQueries/Queries/Debug/SubscriptionDebugQueries.cs
@@ -0,0 +1,35 @@
+﻿using HotChocolate.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snowflake.Support.GraphQL.FrameworkQueries.Queries.Debug
+{
+    public class SubscriptionDebugQueries
+        : ObjectTypeExtension
+    {
+        static async IAsyncEnumerable<string> HelloWorldAsync()
+        {
+            yield return "Hello World Sub 1";
+            yield return "Hello World Sub 2";
+        }
+
+        protected override void Configure(IObjectTypeDescriptor descriptor)
+        {
+            descriptor
+                .Name("Subscription");
+
+            descriptor.Field("helloWorldSubscription")
+                .Subscribe(ctx =>
+                {
+                    return SubscriptionDebugQueries.HelloWorldAsync();
+                })
+                .Resolver(ctx =>
+                {
+                    var message = (string)ctx.ContextData["HotChocolate.Execution.EventMessage"];
+                    return message;
+                })
+                .Type<StringType>();
+        }
+    }
+}
