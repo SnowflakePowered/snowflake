65969c1b3548b90e2aa05c006255d840f8cbc4c0
Ronny Chan <ronny6993@gmail.com>
2020-04-20 03:05:58 -0400

    FrameworkQueries: Implement experimental auto-subscriptions that trigger on mutation

--- src/Snowflake.Support.GraphQL.FrameworkQueries/Mutations/Game/GameMutations.cs
+++ src/Snowflake.Support.GraphQL.FrameworkQueries/Mutations/Game/GameMutations.cs
@@ -1,6 +1,8 @@
-﻿using HotChocolate.Types;
+﻿using HotChocolate.Subscriptions;
+using HotChocolate.Types;
 using Snowflake.Model.Game;
 using Snowflake.Model.Records;
+using Snowflake.Support.GraphQL.FrameworkQueries.Subscriptions;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -14,11 +16,13 @@ namespace Snowflake.Support.GraphQL.FrameworkQueries.Mutations.Game
         {
             descriptor.Name("Mutation");
             descriptor.Field("createGame")
+                .UseAutoSubscription()
                 .Description("Creates a new game in the database.")
                 .Argument("input", arg => arg.Type<CreateGameInputType>())
                 .Resolver(async ctx =>
                 {
                     var gameLibrary = ctx.Service<IGameLibrary>();
+
                     CreateGameInput args = ctx.Argument<CreateGameInput>("input");
                     var game = await gameLibrary.CreateGameAsync(args.PlatformID);
                     return new GamePayload()
@@ -27,9 +31,10 @@ namespace Snowflake.Support.GraphQL.FrameworkQueries.Mutations.Game
                         ClientMutationID = args.ClientMutationID,
                     };
                 })
-                .Type<GamePayloadType>();
+                .Type<NonNullType<GamePayloadType>>();
 
             descriptor.Field("deleteGame")
+                .UseAutoSubscription()
                 .Description("Marks a game as deleted. This does not actually purge the game from the database. " +
                 "Instead, a metadata value `game_deleted` is set to true. " +
                 "Permanently deleting a game once created from the database is not permitted.")
@@ -37,6 +42,7 @@ namespace Snowflake.Support.GraphQL.FrameworkQueries.Mutations.Game
                 .Resolver(async ctx =>
                 {
                     var gameLibrary = ctx.Service<IGameLibrary>();
+
                     DeleteGameInput args = ctx.Argument<DeleteGameInput>("input");
                     var game = await gameLibrary.GetGameAsync(args.GameID);
                     game.Record.Metadata["game_deleted"] = "true";
@@ -47,7 +53,7 @@ namespace Snowflake.Support.GraphQL.FrameworkQueries.Mutations.Game
                         ClientMutationID = args.ClientMutationID,
                     };
                 })
-                .Type<GamePayloadType>();
+                .Type<NonNullType<GamePayloadType>>();
 
         }
     }
---
+++ src/Snowflake.Support.GraphQL.FrameworkQueries/Subscriptions/AutoSubscriptionHelper.cs
@@ -0,0 +1,65 @@
+﻿using HotChocolate.Resolvers;
+using HotChocolate.Subscriptions;
+using HotChocolate.Types;
+using HotChocolate.Types.Descriptors.Definitions;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snowflake.Support.GraphQL.FrameworkQueries.Subscriptions
+{
+    internal static class AutoSubscriptionHelper
+    {
+        public static IObjectFieldDescriptor UseAutoSubscription(this IObjectFieldDescriptor descriptor)
+        {
+            descriptor
+                .Extend()
+                .OnBeforeNaming((configure, defn) =>
+                {
+                    if (!configure.ContextData.ContainsKey(AutoSubscriptionTypeInterceptor.AutoSubscriptionContext))
+                    {
+                        configure.ContextData[AutoSubscriptionTypeInterceptor.AutoSubscriptionContext] = new List<ObjectFieldDefinition>();
+                    }
+                    ((List<ObjectFieldDefinition>)configure.ContextData[AutoSubscriptionTypeInterceptor.AutoSubscriptionContext]).Add(defn);
+                });
+            descriptor.Use(next => async context =>
+            {
+                await next(context);
+                await context.SendSimpleSubscription(context.Result);
+            });
+            return descriptor;
+        }
+
+        public static T GetEventMessage<T>(this IResolverContext context) => (T)context.ContextData["HotChocolate.Execution.EventMessage"];
+        public static EventMessage GetEventMessage(this IResolverContext context) => (EventMessage)context.ContextData["HotChocolate.Execution.EventMessage"];
+        
+        private static async ValueTask<T> SendSimpleSubscription<T>(this IResolverContext context, T payload)
+        {
+            var eventSender = context.Service<IEventSender>();
+            string subscriptionName = $"on{context.Field.Name.Value.ToPascalCase()}";
+            await eventSender.SendAsync(new SimpleEventMessage<T>(subscriptionName, payload));
+            return payload;
+        }
+
+        /// <summary>
+        /// Returns a pascal case version of the string.
+        /// </summary>
+        /// <param name="s">The source string.</param>
+        /// <returns>System.String.</returns>
+        internal static string ToPascalCase(this string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return string.Empty;
+            }
+
+            var newFirstLetter = char.ToUpperInvariant(s[0]);
+            if (newFirstLetter == s[0])
+                return s;
+
+            return newFirstLetter + s.Substring(1);
+        }
+    }
+}
