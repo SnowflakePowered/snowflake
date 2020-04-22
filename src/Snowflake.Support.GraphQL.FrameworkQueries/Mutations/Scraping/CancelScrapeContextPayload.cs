069bbff2b461ce9656b3fb19589fdf1ca7af7188
Ronny Chan <ronny6993@gmail.com>
2020-04-21 21:26:39 -0400

    HC.mutations: Implement cancelScrapeContext and allow deleteScrapeContext at any time safely

---
+++ src/Snowflake.Support.GraphQL.FrameworkQueries/Mutations/Scraping/CancelScrapeContextPayload.cs
@@ -0,0 +1,38 @@
+﻿using HotChocolate.Types;
+using Snowflake.Remoting.GraphQL.FrameworkQueries.Mutations.Relay;
+using Snowflake.Remoting.GraphQL.Model.Scraping;
+using Snowflake.Scraping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snowflake.Support.GraphQL.FrameworkQueries.Mutations.Scraping
+{
+    internal sealed class CancelScrapeContextPayload
+        : RelayMutationBase
+    {
+        public IScrapeContext ScrapeContext { get; set; }
+        public Guid JobID { get; set; }
+    }
+
+    internal sealed class CancelScrapeContextPayloadType
+        : ObjectType<CancelScrapeContextPayload>
+    {
+        protected override void Configure(IObjectTypeDescriptor<CancelScrapeContextPayload> descriptor)
+        {
+            descriptor.Name(nameof(CancelScrapeContextPayload))
+                .WithClientMutationId();
+         
+            descriptor.Field(c => c.JobID)
+                .Name("jobId")
+                .Description("The job GUID.")
+                .Type<NonNullType<UuidType>>();
+
+            descriptor.Field(c => c.ScrapeContext)
+                .Name("context")
+                .Description("The context that cancellation was requested for.")
+                .Type<ScrapeContextType>();
+        }
+    }
+
+}
