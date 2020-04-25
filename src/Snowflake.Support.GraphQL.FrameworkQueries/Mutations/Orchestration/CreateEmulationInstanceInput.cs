7cc3f7eec7760eacc353bc1f89dcb6ec38ab7f94
Ronny Chan <ronny6993@gmail.com>
2020-04-25 19:25:20 -0400

    HC.orchestration: Port create orchestration

--- src/Snowflake.Support.GraphQL.FrameworkQueries/Mutations/Orchestration/CreateEmulationInstanceInput.cs
+++ src/Snowflake.Support.GraphQL.FrameworkQueries/Mutations/Orchestration/CreateEmulationInstanceInput.cs
@@ -1,15 +1,41 @@
-﻿using System;
+﻿using HotChocolate.Types;
+using Snowflake.Remoting.GraphQL.FrameworkQueries.Mutations.Relay;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Snowflake.Support.GraphQL.FrameworkQueries.Mutations.Orchestration
 {
-    public sealed class CreateEmulationInstanceInput
+    internal sealed class CreateEmulationInstanceInput
+        : RelayMutationBase
     {
         public string Orchestrator { get; set; }
         public Guid GameID { get; set; }
         public Guid CollectionID { get; set; }
         public Guid SaveProfileID { get; set; }
+    }
 
+    internal sealed class CreateEmulationInstanceInputType
+        : InputObjectType<CreateEmulationInstanceInput>
+    {
+        protected override void Configure(IInputObjectTypeDescriptor<CreateEmulationInstanceInput> descriptor)
+        {
+            descriptor.Name(nameof(CreateEmulationInstanceInput))
+                .WithClientMutationId();
+            descriptor.Field(i => i.Orchestrator)
+                .Type<NonNullType<StringType>>();
+
+            descriptor.Field(i => i.GameID)
+                .Name("gameId")
+                .Type<NonNullType<UuidType>>();
+
+            descriptor.Field(i => i.CollectionID)
+                .Name("collectionId")
+                .Type<NonNullType<UuidType>>();
+
+            descriptor.Field(i => i.SaveProfileID)
+                .Name("saveProfileId")
+                .Type<NonNullType<UuidType>>();
+        }
     }
 }
--- src/Snowflake.Support.GraphQL.FrameworkQueries/Mutations/Orchestration/EmulationInstanceInput.cs
+++ src/Snowflake.Support.GraphQL.FrameworkQueries/Mutations/Orchestration/EmulationInstanceInput.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using HotChocolate.Types;
+using Snowflake.Remoting.GraphQL.FrameworkQueries.Mutations.Relay;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Snowflake.Support.GraphQL.FrameworkQueries.Mutations.Orchestration
 {
     public sealed class EmulationInstanceInput
+        : RelayMutationBase
     {
-        Guid InstanceID { get; set; }
+        public Guid InstanceID { get; set; }
+    }
+    internal sealed class CEmulationInstanceInputType
+        : InputObjectType<EmulationInstanceInput>
+    {
+        protected override void Configure(IInputObjectTypeDescriptor<EmulationInstanceInput> descriptor)
+        {
+            descriptor.Name(nameof(EmulationInstanceInput))
+                .WithClientMutationId();
+            
+            descriptor.Field(i => i.InstanceID)
+                .Name("instanceId")
+                .Type<NonNullType<UuidType>>();
+        }
     }
 }
