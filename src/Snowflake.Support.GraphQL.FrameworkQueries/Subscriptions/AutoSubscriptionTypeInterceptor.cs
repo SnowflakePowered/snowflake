38ca30b828a7ac17b063cd89c074990fb77fd6b9
Ronny Chan <ronny6993@gmail.com>
2020-04-27 02:25:04 -0400

    HC: describe different subscriptions if using autosubscriptions

--- src/Snowflake.Support.GraphQL.FrameworkQueries/Subscriptions/AutoSubscriptionTypeInterceptor.cs
+++ src/Snowflake.Support.GraphQL.FrameworkQueries/Subscriptions/AutoSubscriptionTypeInterceptor.cs
@@ -53,6 +53,20 @@ namespace Snowflake.Support.GraphQL.FrameworkQueries.Subscriptions
                     });
                 def.Fields.Add(descriptor.CreateDefinition());
             }
+            def.Description = 
+@"Snowflake provides two types of definition in its framework queries:
+  * `onVerbObject`
+  * `onObjectVerb(Uuid!)`
+
+`onVerbObject` subscriptions are global, and are broadcast whenever the corresponding mutation occurs. These can be used to subscribe to mutations that are triggered by the client.
+`onObjectVerb(Uuid!)` subscriptions are primarily used by scraping, installation, and orchestration mutations. These are used to subscribe to events happening on a specific long-existing object that may or may not be the result of a client request.
+
+There is some subtlely in the different types of subscriptions that one should be aware about.
+
+For example, `onStopEmulation` is broadcast when the `stopEmulation` mutation responds to some client. However, `onEmulationStop` is broadcast when the emulation process exits. Hence, `onStopEmulation` may never be broadcast, even if `onEmulationStop` was.
+
+In most cases, it is more useful to subscribe to the `onObjectVerb` subscription for an object whenever feasible.
+";
         }
 
         public void OnBeforeRegisterDependencies(IInitializationContext context, DefinitionBase definition, IDictionary<string, object> contextData)
