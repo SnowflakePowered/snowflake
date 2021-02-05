82ab2263c2c750b61faa9d8f5ed25b058dca6e29
Ronny Chan <ronny6993@gmail.com>
2021-02-05 17:25:22 -0500

    graphql: Replace Playground debug middleware with Altair

--- src/Snowflake.Plugin.Debug.GraphQLVisualizers/GraphQLVisualizerComposable.cs
+++ src/Snowflake.Plugin.Debug.GraphQLVisualizers/GraphQLVisualizerComposable.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Text;
 using GraphQL.Server;
 using GraphQL.Server.Ui.GraphiQL;
-using GraphQL.Server.Ui.Playground;
 using GraphQL.Server.Ui.Voyager;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Snowflake.Remoting.Kestrel;
 using Snowflake.Loader;
+using GraphQL.Server.Ui.Altair;
 
 namespace Snowflake.Plugin.Debug.GraphQLVisualizers
 {
@@ -25,14 +25,9 @@ namespace Snowflake.Plugin.Debug.GraphQLVisualizers
     {
         public void Configure(IApplicationBuilder app)
         {
-            // use graphiQL middleware at default url /graphiql
             app.UseGraphiQLServer(new GraphiQLOptions() { Path = "/debug/gql/graphiql" });
-
-            // use graphql-playground middleware at default url /ui/playground
-            app.UseGraphQLPlayground(new GraphQLPlaygroundOptions() { Path = "/debug/gql/playground" });
-
-            // use voyager middleware at default url /ui/voyager
             app.UseGraphQLVoyager(new GraphQLVoyagerOptions() { Path = "/debug/gql/voyager" });
+            app.UseGraphQLAltair(new GraphQLAltairOptions() { Path = "/debug/gql/altair" });
         }
 
         public void ConfigureServices(IServiceCollection services)
