308e93062b3556ced81a9ba25fd158d621e96af0
Ronny Chan <ronny6993@gmail.com>
2021-02-07 21:29:37 -0500

    config: wip proof of concept for cemu xml

---
+++ src/Snowflake.Framework.Tests/Configuration/CemuGraphicPackConfigurationProofOfConcept.cs
@@ -0,0 +1,34 @@
+﻿using Snowflake.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snowflake.Configuration
+{
+    [ConfigurationTarget("#cemu")]
+    public interface CemuConfigurationCollection : IConfigurationCollection<CemuConfigurationCollection>
+    {
+        [ConfigurationTargetMember("#cemu")]
+        CemuRootConfigSection Content { get; set; }
+
+        [ConfigurationTargetMember("#cemu")]
+        GameListConfig Style { get; set; }
+    }
+
+    [ConfigurationSection("content", "Main Config")]
+    public interface CemuRootConfigSection : IConfigurationSection<CemuRootConfigSection>
+    {
+        [ConfigurationOption("fullscreen", true)]
+        bool Fullscreen { get; set; }
+    }
+
+    [ConfigurationSection("gamelist", "Gamelist Config")]
+    public interface GameListConfig : IConfigurationSection<GameListConfig>
+    {
+        [ConfigurationOption("style", 0)]
+        int Style { get; set; }
+    }
+
+}
