6da7544a4d44a12bbe9ccb71df096e009cdcdd61
Ronny Chan <ronny6993@gmail.com>
2021-02-11 22:17:05 -0500

    Projections: very early WIP of a declarative projections API

---
+++ src/Snowflake.Framework.Tests/Filesystem/DirectoryProjectionTests.cs
@@ -0,0 +1,21 @@
+﻿using Snowflake.Orchestration.Projections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snowflake.Filesystem
+{
+    public class DirectoryProjectionTests
+    {
+        public void Test()
+        {
+            var p = new DirectoryProjection();
+            p.Enter("")
+                .Project("project", null)
+                .Project("project", null)
+            .Exit();
+        }
+    }
+}
