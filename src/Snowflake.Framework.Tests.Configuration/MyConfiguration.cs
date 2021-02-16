9854383d5f903921dab0cf1f0c4d7618e501f498
Ronny Chan <ronny6993@gmail.com>
2021-02-15 22:21:37 -0500

    config: setup exploratory framework for srcgens

---
+++ src/Snowflake.Framework.Tests.Configuration/MyConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Snowflake.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snowflake.Framework.Tests.Configuration
+{
+    [ConfigurationSection("myconfig", "myconfig")]
+    partial interface MyConfiguration
+    {
+        [ConfigurationOption("myOption", false)]
+        bool MyBoolean { get; set; }
+    }
+}
