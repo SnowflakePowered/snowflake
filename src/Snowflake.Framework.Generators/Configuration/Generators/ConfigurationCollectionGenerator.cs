be96f8d5b2e8bf18c0cfcffcca7ca1174ff547d7
chyyran <ronny6993@gmail.com>
2021-03-01 02:53:04 -0500

    srcgen(collection): Use analyzers to verify compile state instead of ad-hoc reporting

--- src/Snowflake.Framework.Generators/Configuration/Generators/ConfigurationCollectionGenerator.cs
+++ src/Snowflake.Framework.Generators/Configuration/Generators/ConfigurationCollectionGenerator.cs
@@ -2,6 +2,7 @@
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
+using Snowflake.Generators.Analyzers;
 using Snowflake.Generators.Configuration.Analyzers;
 using System;
 using System.Collections.Generic;
@@ -14,6 +15,18 @@ namespace Snowflake.Configuration.Generators
     [Generator]
     public sealed class ConfigurationCollectionGenerator : ISourceGenerator
     {
+        private static List<AbstractSyntaxNodeAnalyzer> Analyzers = new()
+        {
+            { new TemplateInterfaceTopLevelAnalyzer() },
+            { new UnextendibleInterfaceAnalyzer() },
+            { new CannotHideInheritedPropertyAnalyzer() },
+            { new InvalidTemplateAccessorAnalyzer() },
+            { new CollectionPropertyNotSectionAnalyzer() },
+            { new CollectionPropertyInvalidAccessorAnalyzer() },
+            { new CollectionPropertyMustHaveGetterAnalyzer() },
+            { new DuplicateConfigurationTargetAnalyzer() },
+        };
+
         public void Execute(GeneratorExecutionContext context)
         {
             if (context.SyntaxReceiver is not ConfigurationTemplateInterfaceSyntaxReceiver receiver)
@@ -23,10 +36,8 @@ namespace Snowflake.Configuration.Generators
             var types = new ConfigurationTypes(compilation);
             if (!types.CheckContext(context, ref errorOccurred))
                 return;
-            var diagnostics = new List<Diagnostic>();
             foreach (var ifaceSyntax in receiver.CandidateInterfaces)
             {
-                errorOccurred = false;
                 var model = compilation.GetSemanticModel(ifaceSyntax.SyntaxTree);
                 var ifaceSymbol = model.GetDeclaredSymbol(ifaceSyntax);
 
@@ -37,166 +48,33 @@ namespace Snowflake.Configuration.Generators
                         .Any(a => SymbolEqualityComparer.Default.Equals(a.AttributeClass, types.ConfigurationCollectionAttribute)))
                     continue;
 
-                diagnostics.AddRange(new TemplateInterfaceTopLevelAnalyzer()
-                     .Analyze(compilation, model, ifaceSyntax));
+                var diagnostics = Analyzers.AsParallel()
+                    .SelectMany(a => a.Analyze(context.Compilation, model, ifaceSyntax))
+                    .ToList();
 
-                diagnostics.AddRange(new UnextendibleInterfaceAnalyzer()
-                    .Analyze(compilation, model, ifaceSyntax));
+                foreach(var diag in diagnostics)
+                {
+                    context.ReportDiagnostic(diag);
+                }
+                if (diagnostics.Any())
+                    return;
 
                 var properties = new List<(INamedTypeSymbol, IPropertySymbol)>();
-                var seenProps = new HashSet<string>();
-                var targetAttrs = new List<AttributeData>();
 
-                // Verify collection props bottom up.
+                // Add collection props bottom up.
                 foreach (var childIface in ifaceSymbol.AllInterfaces.Reverse().Concat(new[] { ifaceSymbol }))
                 {
-                    // No need to check if child interfaces are partial because we only add to the root interface.
                     foreach (var member in childIface.GetMembers())
                     {
-                        if (ConfigurationCollectionGenerator.VerifyCollectionProperty(context, 
-                            compilation, types, member, ifaceSymbol, in seenProps, 
-                            out var property))
+                        if (member is IPropertySymbol property)
                         {
                             properties.Add((childIface, property!));
-                            continue;
                         }
-                        errorOccurred = true;
                     }
-
-                    // Add child targets to list while we're at it.
-                    targetAttrs.AddRange(childIface.GetAttributes()
-                        .Where(a => SymbolEqualityComparer.Default.Equals(a.AttributeClass, types.ConfigurationTargetAttribute)));
                 }
-
-                errorOccurred = errorOccurred || !VerifyConfigurationTargets(context, ifaceSymbol, targetAttrs);
-
-                if (errorOccurred)
-                    continue;
-
                 string classSource = GenerateSource(ifaceSymbol, properties, types);
                 context.AddSource($"{ifaceSymbol.Name}_ConfigurationCollection.cs", SourceText.From(classSource, Encoding.UTF8));
             }
-        }
-
-        private static bool VerifyConfigurationTargets(GeneratorExecutionContext context, 
-            INamedTypeSymbol ifaceSymbol, List<AttributeData> targetAttrs)
-        {
-            if (!targetAttrs.Any())
-                return true;
-            bool errorOccurred = false;
-            var rootTargets = new HashSet<string>();
-            var childTargets = new HashSet<(string, string)>();
-            foreach (var targetAttr in targetAttrs)
-            {
-                if (targetAttr.ConstructorArguments.Length == 1)
-                {
-                    string rootTarget = (string)targetAttr.ConstructorArguments[0].Value!;
-                    if (!rootTargets.Contains(rootTarget))
-                    {
-                        rootTargets.Add(rootTarget);
-                    }
-                    else
-                    {
-                        context.ReportError(DiagnosticError.DuplicatedTarget,
-                         "Duplicate configuration target declared.",
-                         $"A root configuration target called '{rootTarget}' has already been declared for template interface '{ifaceSymbol.Name}'",
-                            targetAttr.ApplicationSyntaxReference?.GetSyntax().GetLocation() ?? Location.None, ref errorOccurred);
-                    }
-                }
-                else if (targetAttr.ConstructorArguments.Length == 2)
-                {
-                    (string childTarget, string parentTarget) = ((string)targetAttr.ConstructorArguments[0].Value!,
-                        (string)targetAttr.ConstructorArguments[1].Value!);
-                    if (!childTargets.Contains((parentTarget, childTarget)))
-                    {
-                        childTargets.Add((parentTarget, childTarget));
-                    }
-                    else
-                    {
-                        context.ReportError(DiagnosticError.DuplicatedTarget,
-                           "Duplicate configuration target declared.",
-                           $"A configuration target from '{parentTarget}' to '{childTarget}' has already been declared for template interface '{ifaceSymbol.Name}'",
-                              targetAttr.ApplicationSyntaxReference?.GetSyntax().GetLocation() ?? Location.None, ref errorOccurred);
-                    }
-                }
-            }
-            return !errorOccurred;
-        }
-
-        private static bool VerifyCollectionProperty(GeneratorExecutionContext context, 
-            Compilation compilation, ConfigurationTypes types, ISymbol member, 
-            INamedTypeSymbol ifaceSymbol, 
-            in HashSet<string> seenProperties,
-            out IPropertySymbol? propertyResult)
-        {
-            bool errorOccurred = false;
-            // Ignore accessors, we only care about the property declaration.
-            if (member is IMethodSymbol accessor && accessor.AssociatedSymbol is IPropertySymbol)
-            {
-                propertyResult = null;
-                return false;
-            }
-       
-            // SFC003
-            if (member is not IPropertySymbol property)
-            {
-                context.ReportError(DiagnosticError.InvalidMembers, "Invalid members in template interface.",
-                   $"Template interface '{ifaceSymbol.Name}' must only declare property members. " +
-                   $"{member.Kind} '{ifaceSymbol.Name}.{member.Name}' is not a property.",
-                   member.Locations.First(), ref errorOccurred);
-                propertyResult = null;
-                return !errorOccurred;
-            }
-            var propertySyntax = (PropertyDeclarationSyntax)property.DeclaringSyntaxReferences.First().GetSyntax();
-            var propertyLocation = propertySyntax.GetLocation();
-
-            if (seenProperties.Contains(property.Name))
-            {
-                context.ReportError(DiagnosticError.DuplicateProperty,
-                   "Duplicate template property key.",
-                   $"A template property named '{property.Name}' already exists. Template interfaces are not allowed to hide or override inherited properties.",
-                   propertyLocation, ref errorOccurred);
-            }
-
-            if (property.Type.TypeKind != TypeKind.Interface)
-            {
-                context.ReportError(DiagnosticError.NotAConfigurationSection,
-                   "Configuration collection template members must be interfaces.",
-                   $"'{property.Name}' is not an interface.", propertyLocation, ref errorOccurred);
-            }
-
-            if (propertySyntax.AccessorList == null || !propertySyntax.AccessorList.Accessors.Any(x => x.IsKind(SyntaxKind.GetAccessorDeclaration)))
-            {
-                context.ReportError(DiagnosticError.MissingGetter, "Missing get accessor",
-               $"Property '{property.Name}' must declare a getter.",
-               propertyLocation, ref errorOccurred);
-            }
-
-            if (propertySyntax.AccessorList?.Accessors.Any(x => x.IsKind(SyntaxKind.SetAccessorDeclaration)) == true)
-            {
-                context.ReportError(DiagnosticError.UnexpectedSetter,
-                    "Unexpected setter in template property",
-                    $"Collection template property '{property.Name}' can not have a setter.", propertyLocation, ref errorOccurred);
-            }
-
-            if (!property.Type.DeclaringSyntaxReferences
-               .Select(s => s.GetSyntax())
-               .Where(s => s is InterfaceDeclarationSyntax)
-               .Cast<InterfaceDeclarationSyntax>()
-               .Select(i => compilation.GetSemanticModel(i.SyntaxTree).GetDeclaredSymbol(i))
-               .Where(i => i != null)
-               .SelectMany(i => i!.GetAttributes())
-               .Any(a => SymbolEqualityComparer.Default.Equals(a.AttributeClass, types.ConfigurationSectionAttribute)))
-            {
-                context.ReportError(DiagnosticError.NotAConfigurationSection,
-                   "Configuration collection template members must be marked with ConfigurationSectionAttribute.",
-                   $"Template type '{property.Type}' for property '{property.Name}' is not marked with ConfigurationSectionAttribute.", propertyLocation, ref errorOccurred);
-            }
-
-            if (!errorOccurred)
-                seenProperties.Add(property.Name);
-            propertyResult = property;
-            return !errorOccurred;
         }
 
         private string GenerateSource(INamedTypeSymbol declaringInterface,
--- src/Snowflake.Framework.Tests/Configuration/DoubleTargetConfigurationCollection.cs
+++ src/Snowflake.Framework.Tests/Configuration/DoubleTargetConfigurationCollection.cs
@@ -8,14 +8,14 @@ using Snowflake.Configuration.Serialization.Serializers.Implementations;
 
 namespace Snowflake.Configuration.Tests
 {
-    [ConfigurationTarget("#target")]
-    [ConfigurationTarget("#target")]
-    [ConfigurationTarget("#target2", "#target")]
-    [ConfigurationTarget("#target2", "#target")]
-    [ConfigurationCollection]
-    public partial interface DoubleTargetConfigurationCollection
-    {
-        [ConfigurationTargetMember("#target")]
-        ExampleConfigurationSection ExampleConfiguration { get; }
-    }
+    //[ConfigurationTarget("#target")]
+    //[ConfigurationTarget("#target")]
+    //[ConfigurationTarget("#target2", "#target")]
+    //[ConfigurationTarget("#target2", "#target")]
+    //[ConfigurationCollection]
+    //public partial interface DoubleTargetConfigurationCollection
+    //{
+    //    [ConfigurationTargetMember("#target")]
+    //    ExampleConfigurationSection ExampleConfiguration { get; }
+    //}
 }
