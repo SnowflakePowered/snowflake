20462a0cbed1979aee7d99a622460c2a656cdefb
chyyran <ronny6993@gmail.com>
2021-03-05 17:47:43 -0500

    tests(srcgen): move analyzer test framework into Snowflake.Framework.Tests

---
+++ src/Snowflake.Framework.Tests/Language/ConfigurationAnalyzerTests.cs
@@ -0,0 +1,45 @@
+﻿using Snowflake.Language.Analyzers.Configuration;
+using Snowflake.Language.Tests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Snowflake.Language.Tests
+{
+    public class ConfigurationAnalyzerTests
+    {
+        [Fact]
+        public async Task SFC022_GenericArgumentRequiresConfigurationSectionAnalyzer_Test()
+        {
+            string testCode = @"
+namespace Snowflake.Framework.Tests.Configuration
+{        
+        using Snowflake.Configuration.Internal;
+
+        public interface SomeInterface
+        {
+        }
+
+        public class TestFixture
+        {
+            [GenericTypeAcceptsConfigurationSection(0)]
+            public void Use<T>()
+                where T: class
+            {
+            }
+            public void Else()
+            {
+                this.Use<SomeInterface>();
+            }
+        }
+}
+";
+            var harness = LanguageTestUtilities.MakeAnalyzerTest<GenericArgumentRequiresConfigurationSectionAnalyzer>
+                (testCode, (19, 17), "SomeInterface");
+            await harness.RunAsync();
+        }
+    }
+}
---
+++ src/Snowflake.Framework.Tests/Language/LanguageTestUtilities.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.CodeAnalysis.Testing.Verifiers;
+using Snowflake.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snowflake.Language.Tests
+{
+    public static class LanguageTestUtilities
+    {
+        private static readonly MetadataReference SnowflakePrimitiveReference 
+            = MetadataReference.CreateFromFile(typeof(ConfigurationCollectionAttribute).Assembly.Location);
+        private static readonly MetadataReference SnowflakeFrameworkReference 
+            = MetadataReference.CreateFromFile(typeof(ConfigurationCollection<>).Assembly.Location);
+        private static readonly MetadataReference SnowflakeTestReference
+          = MetadataReference.CreateFromFile(typeof(LanguageTestUtilities).Assembly.Location);
+        private static Solution AddSnowflakeReferences(Solution solution, ProjectId projectId)
+        {
+            Project project = solution.GetProject(projectId)!;
+            var parseOptions = (CSharpParseOptions)project.ParseOptions!;
+            project = project.WithParseOptions(parseOptions.WithLanguageVersion(LanguageVersion.CSharp9));
+            project = project
+                .AddMetadataReference(SnowflakePrimitiveReference)
+                .AddMetadataReference(SnowflakeFrameworkReference)
+                .AddMetadataReference(SnowflakeTestReference);
+            return project.Solution;
+        }
+
+        public static CSharpAnalyzerTest<TAnalyzer, XUnitVerifier> 
+            MakeAnalyzerTest<TAnalyzer>(string testSource, (int line, int col) location, params object[] arguments)
+            where TAnalyzer : DiagnosticAnalyzer, new()
+        {
+            return new CSharpAnalyzerTest<TAnalyzer, XUnitVerifier>()
+            {
+                TestCode = testSource,
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net50,
+                SolutionTransforms =
+                {
+                    LanguageTestUtilities.AddSnowflakeReferences,
+                },
+                TestState =
+                {
+                    ExpectedDiagnostics = { 
+                        Microsoft.CodeAnalysis.CSharp.Testing.XUnit.AnalyzerVerifier<TAnalyzer>.Diagnostic()
+                        .WithArguments(arguments)
+                        .WithLocation(location.line, location.col)
+                    },
+                },
+            };
+        }
+    }
+}
