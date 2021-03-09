958b41a43a6f5794b2d8be06f1e7754ddaee602d
chyyran <ronny6993@gmail.com>
2021-03-09 00:08:56 -0500

    AbstractGenericTypeRequiresAttributeAnalyzer: Clean up analyzer logic a little

--- src/Snowflake.Framework.Language/Analyzers/AbstractGenericTypeRequiresAttributeAnalyzer.cs
+++ src/Snowflake.Framework.Language/Analyzers/AbstractGenericTypeRequiresAttributeAnalyzer.cs
@@ -32,84 +32,98 @@ namespace Snowflake.Language.Analyzers
 
         protected abstract (INamedTypeSymbol marker, INamedTypeSymbol target) GetAttributes(Compilation compilation);
 
+        private IEnumerable<Diagnostic> Analyze(INamedTypeSymbol marker, INamedTypeSymbol target, SemanticModel semanticModel, 
+            InvocationExpressionSyntax invocationSyntax, CancellationToken cancel)
+        {
+            if (semanticModel.GetSymbolInfo(invocationSyntax, cancel).Symbol is not IMethodSymbol method)
+                yield break;
+
+            if (!method.IsGenericMethod
+                || method.GetAttributes()
+                    .FirstOrDefault(a =>
+                        SymbolEqualityComparer
+                        .Default.Equals(a.AttributeClass, marker)) is not AttributeData genericTypeAttribute)
+                yield break;
+            if (genericTypeAttribute.ConstructorArguments.FirstOrDefault().Value is not int genericIndex)
+                yield break;
+            if (method.TypeArguments.ElementAtOrDefault(genericIndex) is not INamedTypeSymbol targetType)
+                yield break;
+            if (!targetType.GetAttributes().Any(a => SymbolEqualityComparer.Default.Equals(a.AttributeClass, target)))
+                yield return Diagnostic.Create(Rule, invocationSyntax.GetLocation(), targetType.Name);
+        }
+
+        private IEnumerable<Diagnostic> Analyze(INamedTypeSymbol marker, INamedTypeSymbol target, SemanticModel semanticModel,
+            BaseObjectCreationExpressionSyntax newObjSyntax)
+        {
+            // ImplicitObject has no type so instead we get the constructor, then the containing type of the constructor.
+            if (semanticModel.GetSymbolInfo(newObjSyntax).Symbol?.ContainingType is not INamedTypeSymbol type)
+                yield break;
+            if (!type.IsGenericType
+                || type.GetAttributes()
+                    .FirstOrDefault(a =>
+                        SymbolEqualityComparer
+                        .Default.Equals(a.AttributeClass, marker)) is not AttributeData genericTypeAttribute)
+                yield break;
+            if (genericTypeAttribute.ConstructorArguments.FirstOrDefault().Value is not int genericIndex)
+                yield break;
+            if (type.TypeArguments.ElementAtOrDefault(genericIndex) is not INamedTypeSymbol targetType)
+                yield break;
+            if (!targetType.GetAttributes().Any(a => SymbolEqualityComparer.Default.Equals(a.AttributeClass, target)))
+                yield return Diagnostic.Create(Rule, newObjSyntax.GetLocation(), targetType.Name);
+        }
+
+        private IEnumerable<Diagnostic> Analyze(INamedTypeSymbol marker, INamedTypeSymbol target, SemanticModel semanticModel,
+            VariableDeclarationSyntax varDeclSyntax)
+        {
+            if (semanticModel.GetSymbolInfo(varDeclSyntax.Type).Symbol is not INamedTypeSymbol type)
+                yield break;
+            if (!type.IsGenericType
+               || type.GetAttributes()
+                   .FirstOrDefault(a =>
+                       SymbolEqualityComparer
+                       .Default.Equals(a.AttributeClass, marker)) is not AttributeData genericTypeAttribute)
+                yield break;
+            if (genericTypeAttribute.ConstructorArguments.FirstOrDefault().Value is not int genericIndex)
+                yield break;
+            if (type.TypeArguments.ElementAtOrDefault(genericIndex) is not INamedTypeSymbol targetType)
+                yield break;
+            if (!targetType.GetAttributes().Any(a => SymbolEqualityComparer.Default.Equals(a.AttributeClass, target)))
+                yield return Diagnostic.Create(Rule, varDeclSyntax.GetLocation(), targetType.Name);
+        }
+
+        private IEnumerable<Diagnostic> Analyze(INamedTypeSymbol marker, INamedTypeSymbol target, SemanticModel semanticModel,
+            ParameterSyntax paramSyntax)
+        {
+            if (paramSyntax.Type == null)
+                yield break;
+            if (semanticModel.GetSymbolInfo(paramSyntax.Type).Symbol is not INamedTypeSymbol type)
+                yield break;
+            if (!type.IsGenericType
+               || type.GetAttributes()
+                   .FirstOrDefault(a =>
+                       SymbolEqualityComparer
+                       .Default.Equals(a.AttributeClass, marker)) is not AttributeData genericTypeAttribute)
+                yield break;
+            if (genericTypeAttribute.ConstructorArguments.FirstOrDefault().Value is not int genericIndex)
+                yield break;
+            if (type.TypeArguments.ElementAtOrDefault(genericIndex) is not INamedTypeSymbol targetType)
+                yield break;
+            if (!targetType.GetAttributes().Any(a => SymbolEqualityComparer.Default.Equals(a.AttributeClass, target)))
+                yield return Diagnostic.Create(Rule, paramSyntax.GetLocation(), targetType.Name);
+        }
+
         public sealed override IEnumerable<Diagnostic> Analyze(Compilation compilation, SemanticModel semanticModel, SyntaxNode node, CancellationToken cancel)
         {
             (var marker, var target) = this.GetAttributes(compilation);
-            if (node is InvocationExpressionSyntax invocationSyntax)
-            {
-                if (semanticModel.GetSymbolInfo(invocationSyntax, cancel).Symbol is not IMethodSymbol method)
-                    yield break;
 
-                if (!method.IsGenericMethod
-                    || method.GetAttributes()
-                        .FirstOrDefault(a =>
-                            SymbolEqualityComparer
-                            .Default.Equals(a.AttributeClass, marker)) is not AttributeData genericTypeAttribute)
-                    yield break;
-                if (genericTypeAttribute.ConstructorArguments.FirstOrDefault().Value is not int genericIndex)
-                    yield break;
-                if (method.TypeArguments.ElementAtOrDefault(genericIndex) is not INamedTypeSymbol targetType)
-                    yield break;
-                if (!targetType.GetAttributes().Any(a => SymbolEqualityComparer.Default.Equals(a.AttributeClass, target)))
-                    yield return Diagnostic.Create(Rule, invocationSyntax.GetLocation(), targetType.Name);
-            }
-
-            if (node is BaseObjectCreationExpressionSyntax newObjSyntax)
+            return node switch
             {
-                // ImplicitObject has no type so instead we get the constructor, then the containing type of the constructor.
-                if (semanticModel.GetSymbolInfo(newObjSyntax).Symbol?.ContainingType is not INamedTypeSymbol type)
-                    yield break;
-                if (!type.IsGenericType
-                    || type.GetAttributes()
-                        .FirstOrDefault(a =>
-                            SymbolEqualityComparer
-                            .Default.Equals(a.AttributeClass, marker)) is not AttributeData genericTypeAttribute)
-                    yield break;
-                if (genericTypeAttribute.ConstructorArguments.FirstOrDefault().Value is not int genericIndex)
-                    yield break;
-                if (type.TypeArguments.ElementAtOrDefault(genericIndex) is not INamedTypeSymbol targetType)
-                    yield break;
-                if (!targetType.GetAttributes().Any(a => SymbolEqualityComparer.Default.Equals(a.AttributeClass, target)))
-                    yield return Diagnostic.Create(Rule, newObjSyntax.GetLocation(), targetType.Name);
-            }
-
-            if (node is VariableDeclarationSyntax varDeclSyntax)
-            {
-                if (semanticModel.GetSymbolInfo(varDeclSyntax.Type).Symbol is not INamedTypeSymbol type)
-                    yield break;
-                if (!type.IsGenericType
-                   || type.GetAttributes()
-                       .FirstOrDefault(a =>
-                           SymbolEqualityComparer
-                           .Default.Equals(a.AttributeClass, marker)) is not AttributeData genericTypeAttribute)
-                    yield break;
-                if (genericTypeAttribute.ConstructorArguments.FirstOrDefault().Value is not int genericIndex)
-                        yield break;
-                if (type.TypeArguments.ElementAtOrDefault(genericIndex) is not INamedTypeSymbol targetType)
-                    yield break;
-                if (!targetType.GetAttributes().Any(a => SymbolEqualityComparer.Default.Equals(a.AttributeClass, target)))
-                    yield return Diagnostic.Create(Rule, varDeclSyntax.GetLocation(), targetType.Name);
-            }
-
-            if (node is ParameterSyntax paramSyntax)
-            {
-                if (paramSyntax.Type == null)
-                    yield break;
-                if (semanticModel.GetSymbolInfo(paramSyntax.Type).Symbol is not INamedTypeSymbol type)
-                    yield break;
-                if (!type.IsGenericType
-                   || type.GetAttributes()
-                       .FirstOrDefault(a =>
-                           SymbolEqualityComparer
-                           .Default.Equals(a.AttributeClass, marker)) is not AttributeData genericTypeAttribute)
-                    yield break;
-                if (genericTypeAttribute.ConstructorArguments.FirstOrDefault().Value is not int genericIndex)
-                    yield break;
-                if (type.TypeArguments.ElementAtOrDefault(genericIndex) is not INamedTypeSymbol targetType)
-                    yield break;
-                if (!targetType.GetAttributes().Any(a => SymbolEqualityComparer.Default.Equals(a.AttributeClass, target)))
-                    yield return Diagnostic.Create(Rule, paramSyntax.GetLocation(), targetType.Name);
-            }
+                InvocationExpressionSyntax invocationSyntax => this.Analyze(marker, target, semanticModel, invocationSyntax, cancel),
+                BaseObjectCreationExpressionSyntax newObjSyntax => this.Analyze(marker, target, semanticModel, newObjSyntax),
+                VariableDeclarationSyntax varDeclSyntax => this.Analyze(marker, target, semanticModel, varDeclSyntax),
+                ParameterSyntax paramSyntax => this.Analyze(marker, target, semanticModel, paramSyntax),
+                _ => Enumerable.Empty<Diagnostic>()
+            };
         }
     }
 }
